0a42e03ac241880100124c28feef6a80ebf63e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:04:21 +0000

    Add package metabolights-utils/1.4.6/metabolights_utils-1.4.6-py3-none-any.whl

---
+++ packages/metabolights-utils/metabolights_utils-1.4.6-py3-none-any.whl/metabolights_utils/models/metabolights/__init__.py
@@ -0,0 +1,19 @@
+import json
+import os
+from typing import Dict, Union
+
+from metabolights_utils.models.metabolights import metabolights_study, model
+
+
+def get_study_model_schema(schema_path: Union[str, None] = None) -> Dict:
+    if not schema_path:
+        schema_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "study_model_schema_v1.1.json",
+        )
+    with open(schema_path, "r", encoding="utf-8") as sf:
+        study_model_jsonschema = json.load(sf)
+    return study_model_jsonschema
+
+
+__all__ = ["metabolights_study", "model", "get_study_model_schema"]
