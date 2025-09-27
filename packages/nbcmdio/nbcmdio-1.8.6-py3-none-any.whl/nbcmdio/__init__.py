f498979ab815ddab006e8a676d5a0271dcfc54a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:04:31 +0000

    Add package nbcmdio/1.8.6/nbcmdio-1.8.6-py3-none-any.whl

---
+++ packages/nbcmdio/nbcmdio-1.8.6-py3-none-any.whl/nbcmdio/__init__.py
@@ -0,0 +1,8 @@
+from .style import Style, BOLD, FG_RED, FG_YELLOW, RESET, bg_hex, bg_rgb, fg_hex, fg_rgb
+from .output import Output, prt, NbCmdIO
+from .input import Input, inp
+from .utils import *
+
+__version__ = "1.8.6"
+
+__all__ = ['Output', 'Input', 'NbCmdIO', 'prt', 'inp', 'TIMER', 'Style', 'BOLD', 'FG_RED', 'FG_YELLOW', 'RESET', 'RGB', 'bg_hex', 'bg_rgb', 'fg_hex', 'fg_rgb']
