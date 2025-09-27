14637bbc4aad114eaf7e6095486b20962a245b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:04:15 +0000

    Add package lark-webhook-notify/0.1.1/lark_webhook_notify-0.1.1.tar.gz

---
+++ packages/lark-webhook-notify/lark_webhook_notify-0.1.1.tar.gz/lark_webhook_notify-0.1.1/src/lark_webhook_notify/__init__.py
@@ -0,0 +1,49 @@
+from .client import LarkWebhookNotifier
+from .config import LarkWebhookSettings, create_settings
+from .templates import (
+    LarkTemplate,
+    LegacyTaskTemplate,
+    StartTaskTemplate,
+    ReportTaskResultTemplate,
+    SimpleMessageTemplate,
+    AlertTemplate,
+    RawContentTemplate,
+    CardContent,
+    SeverityLevel,
+    ColorTheme,
+)
+from .convenience import (
+    send_task_notification,
+    send_alert,
+    send_simple_message,
+    send_task_start,
+    send_task_result,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # Client
+    "LarkWebhookNotifier",
+    # Configuration
+    "LarkWebhookSettings",
+    "create_settings",
+    # Templates
+    "LarkTemplate",
+    "LegacyTaskTemplate",
+    "StartTaskTemplate",
+    "ReportTaskResultTemplate",
+    "SimpleMessageTemplate",
+    "AlertTemplate",
+    "RawContentTemplate",
+    # Convenience functions
+    "send_task_notification",
+    "send_alert",
+    "send_simple_message",
+    "send_task_start",
+    "send_task_result",
+    # Types
+    "CardContent",
+    "SeverityLevel",
+    "ColorTheme",
+]
