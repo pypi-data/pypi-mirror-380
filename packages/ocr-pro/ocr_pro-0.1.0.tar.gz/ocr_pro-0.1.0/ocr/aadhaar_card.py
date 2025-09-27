0c70681d558f512b15c7f251f4dd77054099a3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:03:59 +0000

    Add package ocr-pro/0.1.0/ocr_pro-0.1.0.tar.gz

---
+++ packages/ocr-pro/ocr_pro-0.1.0.tar.gz/ocr_pro-0.1.0/ocr/aadhaar_card.py
@@ -0,0 +1,17 @@
+import re
+from .fetch_data import Extractdata
+class ExtractAadhaarData(Extractdata):
+    def __init__(self,imagepath, preprocess=False):
+        super().__init__(imagepath, preprocess=preprocess)
+    def get_aadhaar(self):
+        aadhaar = self.aadhaar_data()
+        aadhaar_no = re.search(r"\b\d{4}[ ]\d{4}[ ]\d{4}", aadhaar)
+        aadhaar_dob = re.search("[0-9]{2}[/][0-9]{2}[/]\d{4}", aadhaar)
+        aadhaar_gender = re.search("female|male", aadhaar, re.IGNORECASE)
+
+        result = [
+            aadhaar_no.group() if aadhaar_no else "Aadhaar Number Not Found", 
+            aadhaar_dob.group() if aadhaar_dob else "Aadhaar DOB Not Found",
+            aadhaar_gender.group() if aadhaar_gender else "Aadhaar Gender Not Found"
+        ]
+        return result
