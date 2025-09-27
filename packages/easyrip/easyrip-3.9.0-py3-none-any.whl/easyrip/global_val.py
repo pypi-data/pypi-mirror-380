cb1c4a71902b3f60501bf783e34cdf7f0dc114c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:03:58 +0000

    Add package easyrip/3.9.0/easyrip-3.9.0-py3-none-any.whl

---
+++ packages/easyrip/easyrip-3.9.0-py3-none-any.whl/easyrip/global_val.py
@@ -0,0 +1,6 @@
+class Global_val:
+    PROJECT_NAME = "Easy Rip"
+    PROJECT_VERSION = "3.9.0"
+    PROJECT_TITLE = f"{PROJECT_NAME} v{PROJECT_VERSION}"
+    PROJECT_URL = "https://github.com/op200/EasyRip"
+    PROJECT_RELEASE_API = "https://api.github.com/repos/op200/EasyRip/releases/latest"
---
+++ packages/easyrip/easyrip-3.9.0-py3-none-any.whl/easyrip/ripper/font_subset/__init__.py
@@ -0,0 +1,4 @@
+from .subset import subset
+from .ass import Ass
+
+__all__ = ["subset", "Ass"]
