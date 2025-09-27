26cb4b98f7af44313bf651c1f0ff182403fce6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:03:37 +0000

    Add package parsehub/1.4.3/parsehub-1.4.3-py3-none-any.whl

---
+++ packages/parsehub/parsehub-1.4.3-py3-none-any.whl/parsehub/utiles/img_host.py
@@ -0,0 +1,57 @@
+import os
+
+import httpx
+
+from tenacity import retry, stop_after_attempt
+
+
+class ImgHost:
+    def __init__(self, proxies: httpx.Proxy | str = None):
+        self.async_client = httpx.AsyncClient(proxy=proxies)
+
+    async def _to_file(self, filename_or_url):
+        if str(filename_or_url).startswith("http"):
+            response = await self.async_client.get(filename_or_url)
+            filename = filename_or_url.split("/")[-1]
+            with open(filename, "wb") as f:
+                f.write(response.content)
+        else:
+            filename = filename_or_url
+        return filename
+
+    @retry(stop=stop_after_attempt(5))
+    async def catbox(self, filename_or_url: str):
+        host_url = "https://catbox.moe/user/api.php"
+        filename = await self._to_file(filename_or_url)
+
+        file = open(filename, "rb")
+        try:
+            data = {
+                "reqtype": "fileupload",
+                "userhash": "",
+            }
+            response = await self.async_client.post(
+                host_url, data=data, files={"fileToUpload": file}
+            )
+            return response.text
+        finally:
+            file.close()
+            os.remove(filename)
+
+    @retry(stop=stop_after_attempt(5))
+    async def litterbox(self, filename_or_url: str):
+        host_url = "https://litterbox.catbox.moe/resources/internals/api.php"
+        filename = await self._to_file(filename_or_url)
+        file = open(filename, "rb")
+        try:
+            data = {
+                "reqtype": "fileupload",
+                "time": "1h",
+            }
+            response = await self.async_client.post(
+                host_url, data=data, files={"fileToUpload": file}
+            )
+            return response.text
+        finally:
+            file.close()
+            os.remove(filename)
