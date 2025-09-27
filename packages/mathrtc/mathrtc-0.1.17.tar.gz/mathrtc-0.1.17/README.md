ec3230a801b7c7cffd4085237182f9c45571e87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:04:06 +0000

    Add package mathrtc/0.1.17/mathrtc-0.1.17.tar.gz

---
+++ packages/mathrtc/mathrtc-0.1.17.tar.gz/mathrtc-0.1.17/README.md
@@ -0,0 +1,3 @@
+# My Flit Package
+
+A simple Python package published using Flit.
---
+++ packages/mathrtc/mathrtc-0.1.17.tar.gz/mathrtc-0.1.17/mathrtc/__init__.py
@@ -0,0 +1,4 @@
+from .main import fact
+from .fib import fib
+from .palindrome import palindrome
+from .add_or_even import add_or_even
