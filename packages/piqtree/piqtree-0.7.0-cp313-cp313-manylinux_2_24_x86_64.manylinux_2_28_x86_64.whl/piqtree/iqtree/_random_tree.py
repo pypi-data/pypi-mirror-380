7111f19801d5ab28c67ee52e373b78bd82a0939e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:03:51 +0000

    Add package piqtree/0.7.0/piqtree-0.7.0-cp313-cp313-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/piqtree/piqtree-0.7.0-cp313-cp313-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/piqtree/iqtree/_random_tree.py
@@ -0,0 +1,52 @@
+"""Python wrappers to random tree generation in the IQ-TREE library."""
+
+from enum import Enum, auto
+
+import cogent3
+from _piqtree import iq_random_tree
+
+from piqtree.iqtree._decorator import iqtree_func
+
+iq_random_tree = iqtree_func(iq_random_tree)
+
+
+class TreeGenMode(Enum):
+    """Setting under which to generate a random tree."""
+
+    YULE_HARDING = auto()
+    UNIFORM = auto()
+    CATERPILLAR = auto()
+    BALANCED = auto()
+    BIRTH_DEATH = auto()
+    STAR_TREE = auto()
+
+
+def random_tree(
+    num_taxa: int,
+    tree_mode: TreeGenMode,
+    rand_seed: int | None = None,
+) -> cogent3.PhyloNode:
+    """Generate a random phylogenetic tree.
+
+    Generates a random tree through IQ-TREE.
+
+    Parameters
+    ----------
+    num_taxa : int
+        The number of taxa per tree.
+    tree_mode : TreeGenMode
+        How the tree is generated.
+    rand_seed : int | None, optional
+        The random seed - 0 or None means no seed, by default None.
+
+    Returns
+    -------
+    cogent3.PhyloNode
+        A random phylogenetic tree.
+
+    """
+    if rand_seed is None:
+        rand_seed = 0  # The default rand_seed in IQ-TREE
+
+    newick = iq_random_tree(num_taxa, tree_mode.name, 1, rand_seed).strip()
+    return cogent3.make_tree(newick)
