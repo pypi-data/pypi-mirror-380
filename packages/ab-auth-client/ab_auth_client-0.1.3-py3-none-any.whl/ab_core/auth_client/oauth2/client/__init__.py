6b03667d4c4613b51e74d4ed831fff84892ec91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:04:40 +0000

    Add package ab-auth-client/0.1.3/ab_auth_client-0.1.3-py3-none-any.whl

---
+++ packages/ab-auth-client/ab_auth_client-0.1.3-py3-none-any.whl/ab_core/auth_client/oauth2/client/__init__.py
@@ -0,0 +1,14 @@
+from typing import Annotated, Union
+
+from pydantic import Discriminator
+
+from .pkce import PKCEOAuth2Client
+from .standard import StandardOAuth2Client
+
+OAuth2Client = Annotated[
+    Union[
+        StandardOAuth2Client,
+        PKCEOAuth2Client,
+    ],
+    Discriminator("type"),
+]
