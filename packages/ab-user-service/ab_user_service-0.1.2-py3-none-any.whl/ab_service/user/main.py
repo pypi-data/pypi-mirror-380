28fcc8b1616829a2bc968300729fad8685954b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:04:32 +0000

    Add package ab-user-service/0.1.2/ab_user_service-0.1.2-py3-none-any.whl

---
+++ packages/ab-user-service/ab_user_service-0.1.2-py3-none-any.whl/ab_service/user/main.py
@@ -0,0 +1,47 @@
+import logging
+import logging.config
+import os
+
+from fastapi import FastAPI
+
+from ab_service.user.routes.user import router as user_router
+
+ROOT_LEVEL = os.environ.get("PROD", "INFO")
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+    },
+    "handlers": {
+        "default": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",  # Default is stderr
+        },
+    },
+    "loggers": {
+        "": {  # root logger
+            "level": ROOT_LEVEL,  # "INFO",
+            "handlers": ["default"],
+            "propagate": False,
+        },
+        "uvicorn.error": {
+            "level": "DEBUG",
+            "handlers": ["default"],
+        },
+        "uvicorn.access": {
+            "level": "DEBUG",
+            "handlers": ["default"],
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+app.include_router(user_router)
