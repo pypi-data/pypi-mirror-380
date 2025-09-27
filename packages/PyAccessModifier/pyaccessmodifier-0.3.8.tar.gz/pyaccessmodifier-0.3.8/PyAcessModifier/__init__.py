d97c4e2228a59587083433ec892740e5e9f5fcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 15:04:06 +0000

    Add package PyAccessModifier/0.3.8/pyaccessmodifier-0.3.8.tar.gz

---
+++ packages/PyAccessModifier/pyaccessmodifier-0.3.8.tar.gz/pyaccessmodifier-0.3.8/PyAcessModifier/__init__.py
@@ -0,0 +1,16 @@
+from .modifier import (
+    Private,
+    Protected,
+    Internal,
+    Public,
+    private,
+    protected,
+    internal,
+    public,
+    privateinit
+)
+
+__all__ = [
+    "Private", "Protected", "Internal", "Public",
+    "private", "protected", "internal", "public", "privateinit"
+]
