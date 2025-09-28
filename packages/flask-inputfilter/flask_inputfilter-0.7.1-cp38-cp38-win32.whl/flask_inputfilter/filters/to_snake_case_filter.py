bf0334e1f66080c805e1240401b50022f26e48a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:05:00 +0000

    Add package flask-inputfilter/0.7.1/flask_inputfilter-0.7.1-cp38-cp38-win32.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.1-cp38-cp38-win32.whl/flask_inputfilter/filters/to_snake_case_filter.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import re
+from typing import Any, Union
+
+from flask_inputfilter.models import BaseFilter
+
+
+class ToSnakeCaseFilter(BaseFilter):
+    """
+    Converts a string to snake_case.
+
+    **Expected Behavior:**
+
+    - Inserts underscores before uppercase letters (except the first),
+      converts the string to lowercase, and replaces spaces or hyphens
+      with underscores.
+    - Non-string inputs are returned unchanged.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        class VariableFilter(InputFilter):
+            def __init__(self):
+                super().__init__()
+
+                self.add('variableName', filters=[
+                    ToSnakeCaseFilter()
+                ])
+    """
+
+    __slots__ = ()
+
+    def apply(self, value: Any) -> Union[str, Any]:
+        if not isinstance(value, str):
+            return value
+
+        value = re.sub(r"(?<!^)(?=[A-Z])", "_", value).lower()
+        return re.sub(r"[\s-]+", "_", value)
