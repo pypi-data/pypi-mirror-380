e50d15168b68ea98184a68a6d4d09f91f5e54e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:29 +0000

    Add package wechat-agent/0.0.3/wechat_agent-0.0.3.tar.gz

---
+++ packages/wechat-agent/wechat_agent-0.0.3.tar.gz/wechat_agent-0.0.3/wechat_agent/main.py
@@ -0,0 +1,92 @@
+from wechat_agent.conf import token_header, token_prefix, token_white_list, SECRET_KEY, server_host, server_port
+from flask import Flask, request, jsonify, session, g
+from wechat_agent.logger_config import get_logger
+import re
+from waitress import serve
+from wechat_agent.controller.service_error import ApiError
+from wechat_agent.domain.ajax_result import error
+from wechat_agent.service.jwt_util import verify_token
+from wechat_agent.controller.auth_controller import auth_bp
+from wechat_agent.controller.base_controller import base_bp
+from wechat_agent.controller.agent_controller import agent_bp
+from wechat_agent.controller.ai_role_controller import ai_role_bp
+from wechat_agent.controller.reply_controller import reply_bp
+from wechat_agent.controller.model_controller import model_bp
+from wechat_agent.controller.history_controller import history_bp
+
+app = Flask(__name__, static_url_path='/ui', static_folder='ui')
+app.secret_key = SECRET_KEY
+app.register_blueprint(auth_bp)
+app.register_blueprint(base_bp)
+app.register_blueprint(agent_bp)
+app.register_blueprint(ai_role_bp)
+app.register_blueprint(reply_bp)
+app.register_blueprint(model_bp)
+app.register_blueprint(history_bp)
+
+logger = get_logger()
+
+
+@app.before_request
+def auth():
+    if request.path in token_white_list:
+        logger.info(f"{request.path} match token white list, no need auth")
+        return None
+    for rule in token_white_list:
+        if bool(re.fullmatch(rule, request.path)):
+            logger.info(f"{request.path} match token white list, no need auth")
+            return None
+    headers = request.headers
+    bearer_token = headers.get(token_header)
+    if not bearer_token or not bearer_token.startswith(token_prefix):
+        return jsonify(error("invalid token")), 401
+
+    save_user_id = session.get(bearer_token)
+    if save_user_id is None:
+        session.clear()
+        return jsonify(error("invalid token")), 401
+
+    token = bearer_token[len(token_prefix):]
+    payload = verify_token(token)
+    if not payload["valid"]:
+        return jsonify(error(payload["error"])), 401
+    g.user_id = payload["payload"]["user_id"]
+
+    return None
+
+
+@app.after_request
+def resp(response):
+    global data
+    status = response.status
+    try:
+        data = response.get_json()
+    except Exception as e:
+        logger.error("cant not get json from response", e)
+
+    if data is not None:
+        code = data.get('code')
+        if code is None:
+            return jsonify(code=code, msg=data.get('msg'), data=data.get('data')), status
+
+    return response
+
+
+@app.errorhandler(ApiError)
+def error_handle(e):
+    logger.error(e)
+    return jsonify(error(e.message)), 200
+
+
+@app.errorhandler(Exception)
+def unknow_error_handle(e: Exception):
+    logger.error(e)
+    return jsonify(error("system is too busy")), 500
+
+
+def main():
+    # app.run(host=server_host, port=server_port)
+    serve(app, host=server_host, port=server_port)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/wechat-agent/wechat_agent-0.0.3.tar.gz/wechat_agent-0.0.3/wechat_agent/service/wxauto3_util.py
@@ -0,0 +1,61 @@
+from typing import Callable
+
+from wxauto import WeChat, Chat
+from wxauto.msgs.base import BaseMessage
+from wxauto.msgs.friend import FriendTextMessage
+
+from wechat_agent.SysEnum import WechatReplyType
+from wechat_agent.logger_config import get_logger
+from wechat_agent.controller.service_error import ApiError
+
+logger = get_logger(__name__)
+
+
+class WeXinAuto3Service:
+
+    def __init__(self):
+        self.msg_handlers = {}
+        self.wx = WeChat()
+
+    def listen(self, nickname: str, msg_handler: Callable):
+        if nickname in self.msg_handlers:
+            raise ApiError(f"已经监听了{nickname}的微信消息,请先关闭已开启的监听")
+        logger.info(f"开始监听{nickname}的微信消息")
+        self.msg_handlers[nickname] = msg_handler
+        return self.wx.AddListenChat(nickname=nickname, callback=self.on_message)
+
+    def remove_listen(self, nickname: str):
+        logger.info(f"停止监听{nickname}的微信消息")
+        if nickname in self.msg_handlers:
+            self.msg_handlers.pop(nickname)
+        return self.wx.RemoveListenChat(nickname=nickname)
+
+    def on_message(self, msg: BaseMessage, chat: Chat):
+        nickname = chat.ChatInfo()['chat_name']
+        content = msg.content
+        sender = msg.sender or msg.sender_remark
+        logger.info(f"收到来自{nickname}聊天窗口{sender}的消息: {content}")
+        if isinstance(msg, FriendTextMessage):
+            msg_handler = self.msg_handlers[nickname]
+            resp, resp_type = msg_handler(nickname, sender, content)
+            if resp is not None:
+                if resp_type == WechatReplyType.REPLY:
+                    msg.reply(resp)
+                elif resp_type == WechatReplyType.QUOTE:
+                    msg.quote(resp)
+                else:
+                    logger.warning(f'未知的回复类型: {resp_type}, 将回复为普通回复')
+                    msg.reply(resp)
+
+    def stop_listening(self):
+        return self.wx.StopListening()
+
+
+if __name__ == '__main__':
+    def handler(msg, nickname):
+        return "haha", WechatReplyType.REPLY
+
+
+    service = WeXinAuto3Service()
+    service.listen("假洒脱", handler)
+    service.wx.KeepRunning()
