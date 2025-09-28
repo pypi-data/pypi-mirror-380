6ae072073722fb4979e3bd49b07a5dbbb31136e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:03:48 +0000

    Add package tinycp/0.1.2/tinycp-0.1.2-py3-none-any.whl

---
+++ packages/tinycp/tinycp-0.1.2-py3-none-any.whl/tinycp/regressor/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024-2025 Lucas Leão
+# tinyCP - A small toolbox for conformal prediction
+# Licensed under the MIT License
+
+
+from .cqr import ConformalizedQuantileRegressor
+from .icp import ConformalizedRegressor
