4035ebd9533371f4cbead720b42575f35dfbd9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:45 +0000

    Add package ratemaking-tools/0.3.0/ratemaking_tools-0.3.0-py3-none-any.whl

---
+++ packages/ratemaking-tools/ratemaking_tools-0.3.0-py3-none-any.whl/ratemaking_tools/trending/__init__.py
@@ -0,0 +1,13 @@
+"""
+Trending analysis tools for P&C ratemaking
+
+This subpackage will provide tools for:
+- Loss cost trending
+- Frequency and severity trending
+- Economic and social inflation adjustments
+- Trend factor calculations
+
+TODO: Implementation coming in future releases
+"""
+
+__all__ = []
