c553c2a7c845c9f7e8449a5b23f6b8360cb17e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:05:00 +0000

    Add package flask-inputfilter/0.7.1/flask_inputfilter-0.7.1-cp38-cp38-win_amd64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.1-cp38-cp38-win_amd64.whl/flask_inputfilter/conditions/one_of_matches_condition.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from typing import Any
+
+from flask_inputfilter.models import BaseCondition
+
+
+class OneOfMatchesCondition(BaseCondition):
+    """
+    Ensures that at least one of the specified fields matches a given value.
+
+    **Parameters:**
+
+    - **fields** (*list[str]*): A list of fields to check.
+    - **value** (*Any*): The value to match against.
+
+    **Expected Behavior:**
+
+    Validates that at least one field from the specified list
+    has the given value.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        class OneMatchRequiredFilter(InputFilter):
+            def __init__(self):
+                super().__init__()
+
+                self.add(
+                    'option1'
+                )
+
+                self.add(
+                    'option2'
+                )
+
+                self.add_condition(
+                    OneOfMatchesCondition(
+                        fields=['option1', 'option2'],
+                        value='yes'
+                    )
+                )
+    """
+
+    __slots__ = ("fields", "value")
+
+    def __init__(self, fields: list[str], value: Any) -> None:
+        self.fields = fields
+        self.value = value
+
+    def check(self, data: dict[str, Any]) -> bool:
+        return any(data.get(field) == self.value for field in self.fields)
