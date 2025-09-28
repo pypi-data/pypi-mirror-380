a665edb1e37b3012306b4e082a58f0096d3afe1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:54 +0000

    Add package flask-inputfilter/0.7.1/flask_inputfilter-0.7.1-cp311-cp311-win32.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.1-cp311-cp311-win32.whl/flask_inputfilter/declarative/cimports.pxd
@@ -0,0 +1,2 @@
+from ._factory_functions cimport field
+from ._field_descriptor cimport FieldDescriptor
---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.1-cp311-cp311-win32.whl/flask_inputfilter/filters/array_explode_filter.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from typing import Any, Union
+
+from flask_inputfilter.models import BaseFilter
+
+
+class ArrayExplodeFilter(BaseFilter):
+    """
+    Splits a string into an array based on a specified delimiter.
+
+    **Parameters:**
+
+    - **delimiter** (*str*, default: ``","``): The delimiter used to split
+      the string.
+
+    **Expected Behavior:**
+
+    If the input value is a string, it returns a list of substrings. For
+    non-string values, it returns the value unchanged.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        class TagFilter(InputFilter):
+            def __init__(self):
+                super().__init__()
+
+                self.add('tags', filters=[
+                    ArrayExplodeFilter(delimiter=";")
+                ])
+    """
+
+    __slots__ = ("delimiter",)
+
+    def __init__(self, delimiter: str = ",") -> None:
+        self.delimiter = delimiter
+
+    def apply(self, value: Any) -> Union[list[str], Any]:
+        if not isinstance(value, str):
+            return value
+
+        return value.split(self.delimiter)
