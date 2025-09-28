827d22f212fc13a556837a24dd6e418530f4cc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:31 +0000

    Add package ratemaking/0.1.1/ratemaking-0.1.1-py3-none-any.whl

---
+++ packages/ratemaking/ratemaking-0.1.1-py3-none-any.whl/ratemaking/exposure/__init__.py
@@ -0,0 +1,13 @@
+"""
+Exposure calculation tools for P&C ratemaking
+
+This subpackage will provide tools for:
+- Exposure base calculations
+- Earned exposure adjustments
+- Policy term normalization
+- Territory and class plan exposure
+
+TODO: Implementation coming in future releases
+"""
+
+__all__ = []
