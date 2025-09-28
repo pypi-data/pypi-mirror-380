38490cb06ae75aea479cec3aec2e155d82c24d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:57 +0000

    Add package flask-inputfilter/0.7.1/flask_inputfilter-0.7.1-cp313-cp313-win_amd64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.1-cp313-cp313-win_amd64.whl/flask_inputfilter/validators/base_validator.py
@@ -0,0 +1,11 @@
+import warnings
+
+warnings.warn(
+    "Please use `flask_inputfilter.models` for importing BaseValidator "
+    "as `flask_inputfilter.validators` is deprecated and will be removed in "
+    "future versions.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+from flask_inputfilter.models import BaseValidator
