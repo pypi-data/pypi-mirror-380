02f7e00636bc141260ec387f4edd0915048b26e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:20:18 +0000

    Add package cli-command-parser/2025.9.27/cli_command_parser-2025.9.27-py3-none-any.whl

---
+++ packages/cli-command-parser/cli_command_parser-2025.9.27-py3-none-any.whl/cli_command_parser/inputs/__init__.py
@@ -0,0 +1,71 @@
+"""
+Custom input handlers for Parameters
+
+:author: Doug Skrypa
+"""
+
+from __future__ import annotations
+
+import typing as _t
+from enum import Enum as _Enum
+
+from ..exceptions import ParameterDefinitionError as _ParameterDefinitionError
+from .base import InputType
+from .choices import ChoiceMap, Choices, EnumChoices
+from .exceptions import InputValidationError, InvalidChoiceError
+from .files import File, Json, Path, Pickle, Serialized
+from .numeric import NumRange, Range
+from .patterns import Glob, Regex, RegexMode
+from .time import Date, DateTime, Day, DTFormatMode, Month, Time, TimeDelta
+from .utils import FileWrapper, StatMode
+
+if _t.TYPE_CHECKING:
+    from ..typing import ChoicesType, InputTypeFunc, TypeFunc
+
+# fmt: off
+__all__ = [
+    'StatMode', 'FileWrapper', 'Path', 'File', 'Serialized', 'Json', 'Pickle',
+    'Range', 'NumRange',
+    'Choices', 'ChoiceMap', 'EnumChoices',
+    'Regex', 'RegexMode', 'Glob',
+    'Day', 'Month', 'TimeDelta', 'DateTime', 'Date', 'Time', 'DTFormatMode',
+    'normalize_input_type',
+]
+# fmt: on
+
+_INVALID_CHOICES_TYPES = (_t.Pattern, InputType)
+_INVALID_TYPES_WITH_CHOICES = (Range, range, Regex, _t.Pattern, Glob)
+
+
+def normalize_input_type(type_func: InputTypeFunc, param_choices: ChoicesType) -> _t.Optional[TypeFunc]:
+    if choices_provided := param_choices is not None:
+        if not param_choices:
+            raise _ParameterDefinitionError(
+                f'Invalid choices={param_choices!r} - when specified, choices cannot be empty'
+            )
+        elif isinstance(param_choices, range):
+            return Range(param_choices, type_func)
+        elif isinstance(param_choices, _INVALID_CHOICES_TYPES):
+            raise _ParameterDefinitionError(f'Invalid choices={param_choices!r} - use type={param_choices!r} instead')
+        elif isinstance(type_func, _INVALID_TYPES_WITH_CHOICES):
+            raise _ParameterDefinitionError(f'Cannot combine type={type_func!r} with choices={param_choices!r}')
+
+    if type_func is None:
+        return Choices(param_choices) if choices_provided else type_func
+    elif isinstance(type_func, range):
+        return Range(type_func)
+    elif isinstance(type_func, _t.Pattern):
+        return Regex(type_func)
+
+    try:
+        is_enum = issubclass(type_func, _Enum)
+    except TypeError:
+        pass
+    else:
+        if is_enum:
+            enum_choices = EnumChoices(type_func)
+            if choices_provided:
+                return Choices(param_choices, enum_choices)
+            return enum_choices
+
+    return Choices(param_choices, type_func) if choices_provided else type_func
