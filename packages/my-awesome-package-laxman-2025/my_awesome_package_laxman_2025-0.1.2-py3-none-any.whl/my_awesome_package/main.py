e555cf3a432d1e3a7b11b95e3d582e131ad28e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:57 +0000

    Add package my-awesome-package-laxman-2025/0.1.2/my_awesome_package_laxman_2025-0.1.2-py3-none-any.whl

---
+++ packages/my-awesome-package-laxman-2025/my_awesome_package_laxman_2025-0.1.2-py3-none-any.whl/my_awesome_package/main.py
@@ -0,0 +1,9 @@
+# src/my_awesome_package/main.py
+
+def greet(name):
+    """Greets the user with their name."""
+    return f"Hello, {name}! Your awesome package is working."
+
+
+if __name__ == '__main__':
+    print(greet("PyPI User"))
