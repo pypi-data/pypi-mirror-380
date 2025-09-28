508fb887899f1eccd9b4fd3848804473c2e167a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 05:04:26 +0000

    Add package datahold/2.0.1/datahold-2.0.1-py3-none-any.whl

---
+++ packages/datahold/datahold-2.0.1-py3-none-any.whl/datahold/core/DataABC.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta, abstractmethod
+from typing import *
+
+import setdoc
+from unhash import unhash
+
+__all__ = ["DataABC"]
+
+
+class DataABC(metaclass=ABCMeta):
+    __slots__ = ()
+
+    data: Any
+
+    __hash__ = unhash
+
+    @abstractmethod
+    @setdoc.basic
+    def __init__(self: Self, *args: Any, **kwargs: Any) -> None: ...
+
+    @classmethod
+    def __subclasshook__(cls: type, other: type, /) -> bool:
+        "This magic classmethod can be overwritten for a custom subclass check."
+        return NotImplemented
+
+    @property
+    @abstractmethod
+    def data(self: Self) -> Any: ...
