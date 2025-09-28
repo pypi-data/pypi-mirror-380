786019a2ac74980dd0492f95642cb0c7b39ba9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:03 +0000

    Add package wechat-agent/0.0.5/wechat_agent-0.0.5-py3-none-any.whl

---
+++ packages/wechat-agent/wechat_agent-0.0.5-py3-none-any.whl/wechat_agent/domain/ajax_result.py
@@ -0,0 +1,14 @@
+def success(data=None):
+    return {"code": 0, "msg": "success", "data": data}
+
+
+def error(msg="system is too busy"):
+    return {"code": 1, "msg": msg, "data": None}
+
+
+def build(code, msg, data):
+    return {"code": code, "msg": msg, "data": data}
+
+
+def pageResp(rows, total):
+    return {"code": 0, "rows": rows, "total": total}
