1eb06229c3b0b5da4aa94b6f5ad1d17ebaecc77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:20:52 +0000

    Add package domru-intercom-api/1.1.4/domru_intercom_api-1.1.4.tar.gz

---
+++ packages/domru-intercom-api/domru_intercom_api-1.1.4.tar.gz/domru_intercom_api-1.1.4/domru_intercom_api/exceptions.py
@@ -0,0 +1,40 @@
+from typing import (Any,
+                    Optional)
+
+
+class DomruIntercomAPIError(Exception):
+    """"""
+
+
+class AuthDataRequiredDomruIntercomAPIError(DomruIntercomAPIError):
+    def __init__(self, message: Optional[Any] = None):
+        message = message or ('\n------------------------------------------------------------\n'
+                              'domru_api = DomruIntercomAPI(login=LOGIN, password=PASSWORD)\n'
+                              'OR\n'
+                              'domru_api = DomruIntercomAPI(refresh_token=REFRESH_TOKEN, operator_id=OPERATOR_ID)'
+                              '\n------------------------------------------------------------\n')
+        super().__init__(message)
+
+
+class DeviceUnavailableDomruIntercomAPIError(DomruIntercomAPIError):
+    """"""
+
+
+class TemporaryCodeFailedDomruIntercomAPIError(DomruIntercomAPIError):
+    """"""
+
+
+class ClientConnectorDomruIntercomAPIError(DomruIntercomAPIError):
+    """"""
+
+
+class UnauthorizedDomruIntercomAPIError(DomruIntercomAPIError):
+    """"""
+
+
+class TimeoutDomruIntercomAPIError(DomruIntercomAPIError):
+    """"""
+
+
+class UnknownDomruIntercomAPIError(DomruIntercomAPIError):
+    """"""
