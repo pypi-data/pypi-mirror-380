07fd33a7f617f17ccaff18e6f3f61782be1b218c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:20:53 +0000

    Add package wandelbots-nova/2.19.0/wandelbots_nova-2.19.0-py3-none-any.whl

---
+++ packages/wandelbots-nova/wandelbots_nova-2.19.0-py3-none-any.whl/nova/program/__init__.py
@@ -0,0 +1,4 @@
+from nova.program.function import Program, ProgramPreconditions, program
+from nova.program.runner import ProgramRunner
+
+__all__ = ["ProgramRunner", "program", "ProgramPreconditions", "Program"]
