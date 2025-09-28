b9d7acaa00c2c864757b01962f337662b2531fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 05:04:20 +0000

    Add package ubc-solar-physics/1.9.1/ubc_solar_physics-1.9.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ubc-solar-physics/ubc_solar_physics-1.9.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/physics/models/motor/__init__.py
@@ -0,0 +1,11 @@
+from .base_motor import BaseMotor
+from .basic_motor import BasicMotor
+from .advanced_motor import AdvancedMotor
+
+
+
+__all__ = [
+    "BaseMotor",
+    "BasicMotor",
+    "AdvancedMotor"
+]
