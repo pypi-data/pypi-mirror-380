9e4692bea2dd17324c494d48594c82c24f8d333f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:40 +0000

    Add package envon/0.1.2/envon-0.1.2-py3-none-any.whl

---
+++ packages/envon/envon-0.1.2-py3-none-any.whl/envon/bootstrap_sh.sh
@@ -0,0 +1,11 @@
+﻿envon() {
+  if [ "$#" -gt 0 ]; then
+    case "$1" in
+      help|-h|--help|--install) command envon "$@"; return $? ;;
+      -*) command envon "$@"; return $? ;;
+    esac
+  fi
+  cmd=$(command envon "$@"); ec=$?
+  if [ $ec -ne 0 ]; then printf %s\n "$cmd" >&2; return $ec; fi
+  eval "$cmd"
+}
