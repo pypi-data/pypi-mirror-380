6d1ffa44255eaab5f7f77f148edda917c476f520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 15:04:29 +0000

    Add package gencalc/1.0.3/gencalc-1.0.3-py3-none-any.whl

---
+++ packages/gencalc/gencalc-1.0.3-py3-none-any.whl/gencalc/__init__.py
@@ -0,0 +1,7 @@
+from .gencalc import oldgen_start, oldgen_last, newgen_start, newgen_last
+
+__version__ = "1.0.3"
+__author__ = "Niral Bhatt"
+__license__ = "MIT"
+
+__all__ = ["oldgen_start", "oldgen_last", "newgen_start", "newgen_last"]
---
+++ packages/gencalc/gencalc-1.0.3-py3-none-any.whl/gencalc/gencalc.py
@@ -0,0 +1,11 @@
+def oldgen_start(p):
+    return 1606 + 15*p
+
+def oldgen_last(p):
+    return 1620 + 15*p
+
+def newgen_start(p):
+    return 1996 + 15*p
+
+def newgen_last(p):
+    return 2010 + 15*p
