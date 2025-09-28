43ee8cd1aa9795451c1402d179aad575bbe85848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:20:57 +0000

    Add package QMVVM/0.0.4/qmvvm-0.0.4.tar.gz

---
+++ packages/QMVVM/qmvvm-0.0.4.tar.gz/qmvvm-0.0.4/qmvvm/ref.py
@@ -0,0 +1,53 @@
+# coding:utf-8
+import weakref
+import warnings
+
+from qtpy.QtCore import QObject, Signal, QMetaProperty
+from qtpy.QtWidgets import QWidget
+
+from .proxy import Proxy
+
+
+class Ref(Proxy):
+
+    def __init__(self, widget: QWidget, property: str):
+        super().__init__()
+        self.watchedProperty = property
+        self.widgetRef = weakref.ref(widget)
+        self._connectSignal(widget)
+
+    @property
+    def widget(self):
+        return self.widgetRef()
+
+    @property
+    def value(self):
+        if self.widget is None:
+            return None
+
+        return self.widget.property(self.watchedProperty)
+
+    @value.setter
+    def value(self, value):
+        if value == self.value:
+            return
+
+        self.widget.setProperty(self.watchedProperty, value)
+        self.valueChanged.emit()
+
+    def _connectSignal(self, widget):
+        metaObject = widget.metaObject()
+        property = self.watchedProperty
+        index = metaObject.indexOfProperty(property)
+
+        if index == -1:
+            return
+
+        metaProperty = metaObject.property(index)
+
+        if not metaProperty.hasNotifySignal():
+            return
+
+        notifySignal = metaProperty.notifySignal()
+        signal = getattr(widget, notifySignal.name().data().decode('utf-8'))
+        signal.connect(lambda: self.valueChanged.emit())
