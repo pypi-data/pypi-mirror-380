ce97c719f5d10efad66632e28b7032d4310abc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:24 +0000

    Add package Py-DOS-B1/1.0.4/py_dos_b1-1.0.4-py3-none-any.whl

---
+++ packages/Py-DOS-B1/py_dos_b1-1.0.4-py3-none-any.whl/main.py
@@ -0,0 +1,15 @@
+from utils import *
+
+if __name__ == "__main__":
+    clear_terminal()
+    print(PY_DOS)
+    print("PY DOS [Version 1.3]")
+    print("ENTER 'help' TO GET STARTED.")
+    setup_readline()  # Add this line
+    load_filesystem()
+    while True:
+        try:
+            print("\n")
+            process_commands()
+        except KeyboardInterrupt:
+            break
