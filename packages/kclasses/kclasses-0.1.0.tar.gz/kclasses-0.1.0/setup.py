3012be50ef70d9a26b089883d0cd02598109b2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 05:04:18 +0000

    Add package kclasses/0.1.0/kclasses-0.1.0.tar.gz

---
+++ packages/kclasses/kclasses-0.1.0.tar.gz/kclasses-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup()
+
+
+"""
+import setuptools
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kclasses",
+    version="0.0.1",
+    author="KeVeon White",
+    author_email="keveonwhite@gmail.com",
+    description="Extensions of built-in Python classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AMS0x2A/KClasses",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir= {"", "src"},
+    packages=setuptools.find_packages(where="src")
+)
+"""
