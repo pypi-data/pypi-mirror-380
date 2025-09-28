1e436dbb50df7bd65d93babc822e10beab1b974e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:51 +0000

    Add package anfispy/1.1.3/anfispy-1.1.3-py3-none-any.whl

---
+++ packages/anfispy/anfispy-1.1.3-py3-none-any.whl/ANFISpy/__init__.py
@@ -0,0 +1,6 @@
+from .utils import _print_rules, _plot_var, _plot_rules, _rule_activations
+from .mfs import GaussianMF, BellMF, SigmoidMF, TriangularMF
+from .layers import Antecedents, Consequents, Inference, RecurrentInference
+from .anfis import ANFIS, RANFIS, LSTMANFIS, GRUANFIS
+
+__version__ = "1.1.3"
