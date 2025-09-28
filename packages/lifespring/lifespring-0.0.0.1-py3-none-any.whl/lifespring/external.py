660858fbd230590d53537e969b111a15bba95580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:01 +0000

    Add package lifespring/0.0.0.1/lifespring-0.0.0.1-py3-none-any.whl

---
+++ packages/lifespring/lifespring-0.0.0.1-py3-none-any.whl/lifespring/external.py
@@ -0,0 +1,27 @@
+import os
+import re
+import ast
+import json
+import shutil
+import base64
+import random
+import tempfile
+from time import sleep
+from threading import Lock
+from threading import Lock
+from random import normalvariate
+
+
+__all__ = [
+    "os",
+    "re",
+    "ast",
+    "shutil",
+    "base64",
+    "random",
+    "tempfile",
+    "json",
+    "Lock",
+    "sleep",
+    "normalvariate",
+]
