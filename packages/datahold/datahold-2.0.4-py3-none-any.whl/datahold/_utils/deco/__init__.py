971ec5821c8c4f35517dee644283905ca1c67620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 05:04:24 +0000

    Add package datahold/2.0.4/datahold-2.0.4-py3-none-any.whl

---
+++ packages/datahold/datahold-2.0.4-py3-none-any.whl/datahold/_utils/deco/__init__.py
@@ -0,0 +1,3 @@
+from datahold._utils.deco.dataDeco import dataDeco
+from datahold._utils.deco.funcDeco import funcDeco
+from datahold._utils.deco.initDeco import initDeco
---
+++ packages/datahold/datahold-2.0.4-py3-none-any.whl/datahold/core/DataDict.py
@@ -0,0 +1,22 @@
+import collections
+from typing import *
+
+from frozendict import frozendict
+
+from datahold._utils import deco
+from datahold._utils.Cfg import Cfg
+from datahold.core.DataABC import DataABC
+
+
+@deco.funcDeco(
+    *Cfg.cfg.data["datafuncs"]["Dict"],
+    Frozen=frozendict,
+    NonFrozen=dict,
+)
+@deco.initDeco(
+    Frozen=frozendict,
+    NonFrozen=dict,
+)
+class DataDict(DataABC, collections.abc.MutableMapping):
+    __slots__ = ()
+    data: frozendict
