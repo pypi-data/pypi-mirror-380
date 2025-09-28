00b08624e683cfc6bf4b37cc535770e4a6130213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:03:58 +0000

    Add package waypaper/2.7/waypaper-2.7.tar.gz

---
+++ packages/waypaper/waypaper-2.7.tar.gz/waypaper-2.7/waypaper/__main__.py
@@ -0,0 +1,143 @@
+"""Main module that either runs cli commands or starts GUI"""
+
+import argparse
+import sys
+import time
+import json
+import pathlib
+import threading
+
+from waypaper.app import App
+from waypaper.changer import change_wallpaper
+from waypaper.common import get_random_file
+from waypaper.config import Config
+from waypaper.options import BACKEND_OPTIONS, FILL_OPTIONS, get_monitor_options
+from waypaper.translations import load_language
+
+
+__version__ = "2.7"
+
+# Get application settings and language package:
+cf = Config()
+cf.read()
+cf.read_state()
+txt = load_language(cf.lang)
+
+# Define command line argument parser and parse user arguments:
+parser = argparse.ArgumentParser(
+    prog="waypaper", description=txt.msg_desc, epilog=txt.msg_info
+)
+parser.add_argument("-v", "--version", help=txt.msg_arg_help, action="store_true")
+parser.add_argument("--restore", help=txt.msg_arg_rest, action="store_true")
+parser.add_argument("--random", help=txt.msg_arg_rand, action="store_true")
+parser.add_argument("--fill", help=txt.msg_arg_fill, choices=FILL_OPTIONS)
+parser.add_argument("--wallpaper", help=txt.msg_arg_wall)
+parser.add_argument("--folder", help=txt.msg_arg_folder, nargs="+", default = [])
+parser.add_argument("--state-file", help=txt.msg_arg_statefile)
+parser.add_argument("--backend", help=txt.msg_arg_back, choices=BACKEND_OPTIONS)
+parser.add_argument("--list", help=txt.msg_arg_list, action='store_true')
+parser.add_argument("--monitor", help=txt.msg_arg_monitor, choices=get_monitor_options(cf.backend))
+parser.add_argument("--no-post-command", help=txt.msg_arg_post, action='store_true')
+args = parser.parse_args()
+
+
+def run():
+    """Read user arguments and either run GUI app or perform requested action"""
+
+    # Read user arguments, and update things if alternative state file was provided:
+    cf.read_parameters_from_user_arguments(args)
+    if args.state_file:
+        cf.read_state()
+        cf.read_parameters_from_user_arguments(args)
+    cf.check_validity()
+
+    # Set monitor and wallpaper from user arguments:
+    if args.monitor:
+        monitor = args.monitor
+
+        # Set wallpaper from user arguments if provided:
+        if args.wallpaper:
+            wallpaper = pathlib.Path(args.wallpaper).expanduser()
+
+        # Otherwise set a random wallpaper:
+        else:
+            wallpaper_str = get_random_file(cf.backend, cf.image_folder_list, cf.include_subfolders,
+                                            cf.include_all_subfolders, cf.cache_dir, cf.show_hidden)
+            if wallpaper_str:
+                wallpaper = pathlib.Path(wallpaper_str)
+            else:
+                print("Could not get random wallpaper.")
+                sys.exit(0)
+
+        # Launch commands to change wallpaper in a separate thread:
+        threading.Thread(target=change_wallpaper, args=(wallpaper, cf, monitor)).start()
+        time.sleep(0.1)
+
+        # Save this wallpaper in config and quit:
+        cf.selected_wallpaper = wallpaper
+        cf.selected_monitor = monitor
+        cf.attribute_selected_wallpaper()
+        if cf.use_xdg_state:
+            cf.save_state_file()
+        else:
+            cf.save()
+        sys.exit(0)
+
+    # Set previous wallpapers or random wallpaper:
+    if args.restore or args.random:
+        for index, (wallpaper, monitor) in enumerate(zip(cf.wallpapers, cf.monitors)):
+            if args.random:
+                wallpaper_str = get_random_file(cf.backend, cf.image_folder_list, cf.include_subfolders,
+                                                cf.include_all_subfolders, cf.cache_dir, cf.show_hidden)
+                if wallpaper_str:
+                    wallpaper = pathlib.Path(wallpaper_str)
+                    cf.wallpapers[index] = wallpaper
+
+            if cf.wallpapers[index] is None:
+                continue
+
+            # Launch commands to change wallpaper in a separate thread:
+            threading.Thread(target=change_wallpaper, args=(wallpaper, cf, monitor)).start()
+            time.sleep(0.1)
+
+        # Save new wallpapers:
+        if cf.use_xdg_state:
+            cf.save_state_file()
+        else:
+            cf.save()
+        sys.exit(0)
+
+    # Set wallpaper from user arguments:
+    if args.wallpaper:
+        monitor = "All"
+        wallpaper = pathlib.Path(args.wallpaper).expanduser()
+        threading.Thread(target=change_wallpaper, args=(wallpaper, cf, monitor)).start()
+
+        # Save this wallpaper in config and quit:
+        cf.selected_wallpaper = wallpaper
+        cf.selected_monitor = monitor
+        cf.attribute_selected_wallpaper()
+        if cf.use_xdg_state:
+            cf.save_state_file()
+        else:
+            cf.save()
+        sys.exit(0)
+
+    # Output some information in json format:
+    if args.list:
+        info = list(map(lambda x: {"monitor": x[0], "wallpaper": str(x[1]), "backend": cf.backend}, zip(cf.monitors, cf.wallpapers)))
+        print(json.dumps(info))
+        sys.exit(0)
+
+    # Print the version and quit:
+    if args.version:
+        print(f"waypaper v.{__version__}")
+        sys.exit(0)
+
+    # Start GUI:
+    app = App(txt, cf)
+    app.run()
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/waypaper/waypaper-2.7.tar.gz/waypaper-2.7/waypaper/options.py
@@ -0,0 +1,80 @@
+"""Module that contains lists of possible options used in the application"""
+import json
+import subprocess
+
+from typing import List, Dict
+
+
+BACKEND_OPTIONS: List[str] = ["none", "swaybg", "swww", "feh", "wallutils", "hyprpaper", "mpvpaper"]
+FILL_OPTIONS: List[str] = ["fill", "stretch", "fit", "center", "tile"]
+SORT_OPTIONS: List[str] = ["name", "namerev", "date", "daterev", "random"]
+SORT_DISPLAYS: Dict[str, str] = {"name": "Name ↓", "namerev": "Name ↑", "date": "Date ↓", "daterev": "Date ↑", "random": "Random"}
+
+VIDEO_EXTENSIONS: List[str] = ['.webm', '.mkv', '.flv', '.vob', '.ogv', '.ogg', '.rrc', '.gifv', '.mng', '.mov',
+                               '.avi', '.qt', '.wmv', '.yuv', '.rm', '.asf', '.amv', '.mp4', '.m4p', '.m4v',
+                               '.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.m4v', '.svi', '.3gp', '.3g2', '.mxf',
+                               '.roq', '.nsv', '.flv', '.f4v', '.f4p', '.f4a', '.f4b', '.mod' ]
+
+IMAGE_EXTENSIONS: Dict[str, List[str]] = {
+        BACKEND_OPTIONS[0]: ['.gif', '.jpg', '.jpeg', '.png', '.webp', '.bmp', '.pnm', '.tiff'],
+        BACKEND_OPTIONS[1]: ['.gif', '.jpg', '.jpeg', '.png'],
+        BACKEND_OPTIONS[2]: ['.gif', '.jpg', '.jpeg', '.png', '.webp', '.bmp', '.pnm', '.tiff'],
+        BACKEND_OPTIONS[3]: ['.gif', '.jpg', '.jpeg', '.png', '.bmp', '.pnm', '.tiff'],
+        BACKEND_OPTIONS[4]: ['.gif', '.jpg', '.jpeg', '.png'],
+        BACKEND_OPTIONS[5]: ['.jpg', '.jpeg', '.png', '.webp'],
+        BACKEND_OPTIONS[6]: ['.gif', '.jpg', '.jpeg', '.png', '.webp', '.bmp', '.pnm', '.tiff', '.avif'] + VIDEO_EXTENSIONS,
+        }
+
+SWWW_TRANSITION_TYPES: List[str] = ["any", "none", "simple", "fade", "wipe",  "left", "right", "top",
+                                "bottom", "wave", "grow", "center", "outer", "random"]
+
+TIMERS: Dict[str, int] = {"30 sec": 30, "1 min": 60, "2 min": 120, "5 min": 300, "10 min": 600, "30 min": 1800, "1 hour": 3600,
+          "2 hours": 7200, "6 hours": 21600, "12 hours": 43200, "1 day": 86400, "1 week": 604800}
+
+
+def get_monitor_names_with_swww() -> List[str]:
+    """Obtain the list of plugged monitors using swww daemon"""
+    connected_monitors: List[str] = []
+    try:
+        # Check if swww-deamon is already running. If not, launch it:
+        try:
+            subprocess.check_output(["pgrep", "swww-daemon"], encoding='utf-8')
+        except subprocess.CalledProcessError:
+            subprocess.Popen(["swww-daemon"])
+            print("The swww-daemon launched.")
+        # Check available monitors:
+        monitors_info = str(subprocess.check_output(["swww", "query"], encoding='utf-8'))
+        monitors = monitors_info.split("\n")
+        for monitor in monitors[:-1]:
+            connected_monitors.append(monitor.split(':')[0])
+    except Exception as e:
+        print(f"Exception: {e}")
+    return connected_monitors
+
+
+def get_monitor_names_with_hyprctl() -> List[str]:
+    """Obtain the list of plugged monitors using hyprctl"""
+    monitors_info = subprocess.run(["hyprctl", "monitors", "-j"], capture_output=True, text=True, check=True)
+    return [monitor["name"] for monitor in json.loads(monitors_info.stdout)]
+
+
+def get_monitors(backend) -> List[str]:
+    """Get a list of monitor names by various means depending on the backend.
+    Returns a list of monitor names or an empty list if an error occurs."""
+    try:
+        if backend == "hyprpaper":
+            return get_monitor_names_with_hyprctl()
+        elif backend == "swww":
+            return get_monitor_names_with_swww()
+        else:
+            from screeninfo import get_monitors as _get_monitors
+            return [m.name for m in _get_monitors()]
+    except Exception as e:
+        print(f"Error fetching monitors: {e}. Falling back to 'All'.")
+        return []
+
+
+def get_monitor_options(backend) -> List[str]:
+    """Get a list of available monitors for the CLI."""
+    mons = get_monitors(backend)
+    return ["All"] + mons if mons else ["All"]
