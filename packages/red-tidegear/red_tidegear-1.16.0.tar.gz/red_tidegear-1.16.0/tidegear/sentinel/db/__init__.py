fa448f944278837c98b529ed30afd7554c4ee090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:10 +0000

    Add package red-tidegear/1.16.0/red_tidegear-1.16.0.tar.gz

---
+++ packages/red-tidegear/red_tidegear-1.16.0.tar.gz/red_tidegear-1.16.0/tidegear/sentinel/db/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2025 cswimr <copyright@csw.im>
+# SPDX-License-Identifier: MPL-2.0
+
+from .tables import AbstractPartial, Change, Moderation, PartialChannel, PartialGuild, PartialRole, PartialTargetable, PartialUser
+
+__all__ = [
+    "AbstractPartial",
+    "Change",
+    "Moderation",
+    "PartialChannel",
+    "PartialRole",
+    "PartialGuild",
+    "PartialUser",
+    "PartialTargetable",
+    "PartialGuild",
+    "PartialRole",
+]
