5dad30d92538641a8509eb2a1f0017afe57f3a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:03:54 +0000

    Add package torchcurves/0.1.1/torchcurves-0.1.1.tar.gz

---
+++ packages/torchcurves/torchcurves-0.1.1.tar.gz/torchcurves-0.1.1/src/torchcurves/modules/__init__.py
@@ -0,0 +1,5 @@
+from ._bspline import BSplineCurve, BSplineEmbeddings
+from ._kan_tools import Sum
+from ._legendre import LegendreCurve
+
+__all__ = ["BSplineEmbeddings", "BSplineCurve", "LegendreCurve", "Sum"]
