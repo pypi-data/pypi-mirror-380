1cd5119c17402baf56441962a479abbd0951904a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 05:04:14 +0000

    Add package ubc-solar-physics/1.9.1/ubc_solar_physics-1.9.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/ubc-solar-physics/ubc_solar_physics-1.9.1-cp39-cp39-macosx_11_0_arm64.whl/physics/models/__init__.py
@@ -0,0 +1,15 @@
+from .arrays import BasicArray
+from .battery import BasicBattery
+from .lvs import BasicLVS
+from .motor import BasicMotor
+from .regen import BasicRegen
+from .aeroshell import Aeroshell
+
+__all__ = [
+    "BasicArray",
+    "BasicBattery",
+    "BasicLVS",
+    "BasicMotor",
+    "BasicRegen",
+    "Aeroshell"
+]
