34754d58c0e3e60eee080b93629a5cc5b4621e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:03:58 +0000

    Add package waypaper/2.7/waypaper-2.7-py3-none-any.whl

---
+++ packages/waypaper/waypaper-2.7-py3-none-any.whl/waypaper/changer.py
@@ -0,0 +1,258 @@
+"""Module that runs the system processes to change the wallpaper"""
+
+import subprocess
+import time
+from typing import Optional
+from pathlib import Path
+
+from waypaper.config import Config
+from waypaper.options import get_monitor_names_with_hyprctl
+
+
+def find_process_pid(command: str) -> Optional[int]:
+    """Find the PID of the process matching the exact command"""
+    try:
+        result = subprocess.run(['ps', 'aux'], stdout=subprocess.PIPE, text=True)
+        processes = result.stdout.splitlines()
+        for process in processes:
+            if command in process:
+                # Extract PID (second column after splitting):
+                return int(process.split()[1])
+        return None
+    except Exception:
+        return None
+
+
+def seek_and_destroy(process: str, monitor: str = "All"):
+    """Find if a backend is already running somewhere and kill it"""
+
+    # Kill all process instances if we want to set for all monitors:
+    if monitor == "All":
+        try:
+            subprocess.check_output(["pgrep", f"{process}"], encoding='utf-8')
+            subprocess.Popen(["killall", f"{process}"])
+            time.sleep(0.1)
+            print(f"Killed all previous instances of {process}")
+        except subprocess.CalledProcessError:
+            pass
+
+    # Otherwise, find PID of the process for certain monitor and kill it:
+    else:
+        if process == "mpvpaper":
+            pid = find_process_pid(f"mpvpaper -f socket-{monitor}")
+        elif process == "swaybg":
+            pid = find_process_pid(f"swaybg -o {monitor}")
+        else:
+            return
+        try:
+            subprocess.run(['kill', '-9', str(pid)], check=True)
+            print(f"Detected {process} on {monitor} and killed it")
+        except Exception as e:
+            pass
+
+
+def change_with_swaybg(image_path: Path, cf: Config, monitor: str):
+    """Change wallpaper with swaybg backend"""
+
+    # Check pid of current swaybg process:
+    if monitor == "All":
+        pid = find_process_pid(f"swaybg")
+    else:
+        pid = find_process_pid(f"swaybg -o {monitor}")
+
+    # Launch a new swaybg process:
+    fill = cf.fill_option.lower()
+    command = ["swaybg"]
+    if monitor != "All":
+        command.extend(["-o", monitor])
+    command.extend(["-i", str(image_path)])
+    command.extend(["-m", fill, "-c", cf.color])
+    subprocess.Popen(command)
+
+    # Kill previous swaybg process once new wallpaper is set:
+    if pid:
+        time.sleep(0.2)
+        subprocess.run(['kill', '-9', str(pid)], check=True)
+
+
+def change_with_mpvpaper(image_path: Path, cf: Config, monitor: str):
+    """Change wallpaper with mpvpaper backend"""
+
+    fill_types = {
+            "fill": "panscan=1.0",
+            "fit": "panscan=0.0",
+            "center": "",
+            "stretch": "--keepaspect=no",
+            "tile": "",
+            }
+    fill = fill_types[cf.fill_option.lower()]
+
+    # If mpvpaper is already active on given monitor, try to call that process in that socket:
+    try:
+        subprocess.check_output(["pgrep", "-f", f"socket-{monitor}"], encoding='utf-8')
+        time.sleep(0.2)
+        print(f"Detected running mpvpaper on {monitor}, now trying to call mpvpaper socket")
+        subprocess.Popen(f"echo 'loadfile \"{image_path}\"' | socat - /tmp/mpv-socket-{monitor}", shell=True)
+
+    # If mpvpaper is not running, create a new process in a new socket:
+    except subprocess.CalledProcessError:
+        print("Detected no running mpvpaper, starting new mpvpaper process")
+        command = ["mpvpaper", "--fork"]
+        if cf.mpvpaper_sound:
+            command.extend(["-o", f"input-ipc-server=/tmp/mpv-socket-{monitor} {cf.mpvpaper_options} loop {fill} --background-color='{cf.color}'"])
+        else:
+            command.extend(["-o", f"input-ipc-server=/tmp/mpv-socket-{monitor} {cf.mpvpaper_options} loop {fill} --mute=yes --background-color='{cf.color}'"])
+
+        # Specify the monitor:
+        if monitor == "All":
+            command.extend('*')
+        else:
+            command.extend([monitor])
+
+        command.extend([image_path])
+
+        print(f"{command=}")
+        subprocess.Popen(command)
+
+
+def change_with_swww(image_path: Path, cf: Config, monitor: str):
+    """Change wallpaper with swww backend"""
+
+    # Because swaybg and hyprpaper are known to conflict with swww, kill them:
+    seek_and_destroy("swaybg")
+    seek_and_destroy("hyprpaper")
+
+    fill_types = {
+            "fill": "crop",
+            "fit": "fit",
+            "center": "no",
+            "stretch": "crop",
+            "tile": "no",
+            }
+    fill = fill_types[cf.fill_option.lower()]
+
+    # Check if swww-daemon is already running. If not, launch it:
+    try:
+        subprocess.check_output(["pgrep", "swww-daemon"], encoding='utf-8')
+    except subprocess.CalledProcessError:
+        subprocess.Popen(["swww-daemon"])
+        print("Launched swww-daemon")
+
+    version_p = subprocess.run(["swww", "-V"], capture_output=True, text=True)
+    swww_version = [int(x) for x in version_p.stdout.strip().split(" ")[1].split(".")]
+
+    command = ["swww", "img", image_path]
+    command.extend(["--resize", fill])
+    if swww_version >= [0, 11, 0]:
+        command.extend(["--fill-color", cf.color.lstrip("#")])
+    else:
+        command.extend(["--fill-color", cf.color])
+    command.extend(["--transition-type", cf.swww_transition_type])
+    command.extend(["--transition-step", str(cf.swww_transition_step)])
+    command.extend(["--transition-angle", str(cf.swww_transition_angle)])
+    command.extend(["--transition-duration", str(cf.swww_transition_duration)])
+    command.extend(["--transition-fps", str(cf.swww_transition_fps)])
+    if monitor != "All":
+        command.extend(["--outputs", monitor])
+    subprocess.Popen(command)
+
+
+def change_with_feh(image_path: Path, cf: Config, monitor: str):
+    """Change wallpaper with feh backend"""
+
+    fill_types = {
+            "fill": "--bg-fill",
+            "fit": "--bg-max",
+            "center": "--bg-center",
+            "stretch": "--bg-scale",
+            "tile": "--bg-tile",
+            }
+    fill = fill_types[cf.fill_option.lower()]
+    command = ["feh", fill, "--image-bg", cf.color]
+    command.extend([str(image_path)])
+    subprocess.Popen(command)
+
+
+def change_with_wallutils(image_path: Path, cf: Config, monitor: str):
+    """Change wallpaper with wallutils backend"""
+    fill_types = {
+            "fill": "scale",
+            "fit": "scale",
+            "center": "center",
+            "stretch": "stretch",
+            "tile": "tile",
+            }
+    fill = fill_types[cf.fill_option.lower()]
+    subprocess.Popen(["setwallpaper", "--mode", fill, image_path])
+
+
+def change_with_hyprpaper(image_path: Path, cf: Config, monitor: str):
+    """Change wallpaper with hyprpaper backend"""
+
+    # Check if hyprpaper is already running, otherwise start it, and preload the wallpaper:
+    try:
+        subprocess.check_output(["pgrep", "hyprpaper"], encoding='utf-8')
+    except subprocess.CalledProcessError:
+        subprocess.Popen(["hyprpaper"])
+        time.sleep(1)
+    preload_command = ["hyprctl", "hyprpaper", "preload", image_path]
+
+    # Decide which monitors are affected:
+    if monitor == "All":
+        # monitors = [m.name for m in screeninfo.get_monitors()]
+        # monitor_info = subprocess.run(["hyprctl", "monitors", "-j"], capture_output=True, text=True, check=True)
+        # monitors = [m["name"] for m in json.loads(monitor_info.stdout)]
+        monitors = get_monitor_names_with_hyprctl()
+    else:
+        monitors: list = [monitor]
+
+    # Change the wallpaper one by one for each affected monitor:
+    for m in monitors:
+        wallpaper_command = ["hyprctl", "hyprpaper", "wallpaper", f"{m},{image_path}"]
+        unload_command = ["hyprctl", "hyprpaper", "unload", "all"]
+        result: str = ""
+        retry_counter: int = 0
+
+        # Since sometimes hyprpaper fails to change the wallpaper, we try until success:
+        while result != "ok" and retry_counter < 10:
+            try:
+                subprocess.check_output(unload_command, encoding="utf-8").strip()
+                subprocess.check_output(preload_command, encoding="utf-8").strip()
+                result = subprocess.check_output(wallpaper_command, encoding="utf-8").strip()
+                time.sleep(0.1)
+            except Exception:
+                retry_counter += 1
+
+
+def change_wallpaper(image_path: Path, cf: Config, monitor: str):
+    """Run system commands to change the wallpaper depending on the backend"""
+
+    print(f"Selected file: {image_path}")
+
+    try:
+        if cf.backend == "swaybg":
+            change_with_swaybg(image_path, cf, monitor)
+        if cf.backend == "mpvpaper":
+            change_with_mpvpaper(image_path, cf, monitor)
+        if cf.backend == "swww":
+            change_with_swww(image_path, cf, monitor)
+        if cf.backend == "feh":
+            change_with_feh(image_path, cf, monitor)
+        if cf.backend == "wallutils":
+            change_with_wallutils(image_path, cf, monitor)
+        if cf.backend == "hyprpaper":
+            change_with_hyprpaper(image_path, cf, monitor)
+        if cf.backend != "none":
+            filename = Path(image_path).resolve().name
+            print(f"Sent {cf.backend} command to set {filename} on {monitor} display\n")
+
+        # Run a post command:
+        if cf.post_command and cf.use_post_command:
+            modified_image_path = str(image_path).replace(" ", "\\ ")
+            post_command = cf.post_command.replace("$wallpaper", modified_image_path)
+            post_command = post_command.replace("$monitor", monitor)
+            subprocess.Popen(post_command, shell=True)
+            print(f'Executed "{post_command}" post-command\n')
+
+    except Exception as e:
+        print(f"Error occured while changing wallpaper: \n{e}")
---
+++ packages/waypaper/waypaper-2.7-py3-none-any.whl/waypaper/config.py
@@ -0,0 +1,276 @@
+"""Module responsible for reading and saving the configuration and state files"""
+
+import configparser
+import pathlib
+from argparse import Namespace
+from typing import List
+from platformdirs import user_config_path, user_pictures_path, user_cache_path, user_state_path
+
+from waypaper.options import FILL_OPTIONS, SORT_OPTIONS, SWWW_TRANSITION_TYPES, BACKEND_OPTIONS
+from waypaper.common import check_installed_backends
+
+
+class Config:
+    """User configuration loaded from the config.ini file"""
+    def __init__(self):
+        # All paths (folders or wallpapers) are Path objects
+        self.name = "waypaper"
+        self.home_path = pathlib.Path.home()
+        self.image_folder_list: list[pathlib.Path] = []
+        self.image_folder_fallback: str = str(user_pictures_path())
+        self.installed_backends = check_installed_backends()
+        self.selected_wallpaper = None
+        self.selected_monitor = "All"
+        self.fill_option = FILL_OPTIONS[0]
+        self.sort_option = SORT_OPTIONS[0]
+        self.backend = self.installed_backends[-1]
+        self.color = "#ffffff"
+        self.number_of_columns = 3
+        self.swww_transition_type = SWWW_TRANSITION_TYPES[0]
+        self.swww_transition_step = 63
+        self.swww_transition_angle = 0
+        self.swww_transition_duration = 2
+        self.swww_transition_fps = 60
+        self.mpvpaper_sound = False
+        self.mpvpaper_options = ""
+        self.lang = "en"
+        self.monitors = [self.selected_monitor]
+        self.wallpapers = []
+        self.post_command = ""
+        self.include_subfolders = False
+        self.include_all_subfolders = False
+        self.show_hidden = False
+        self.show_gifs_only = False
+        self.zen_mode = False
+        self.cache_dir = user_cache_path(self.name)
+        self.config_dir = user_config_path(self.name)
+        self.config_file = self.config_dir / "config.ini"
+        self.state_dir = user_state_path(self.name)
+        self.state_file = self.state_dir / "state.ini"
+        self.style_file = self.config_dir / "style.css"
+        self.use_xdg_state = False
+        self.use_post_command = True
+        self.show_path_in_tooltip = True
+
+        # Create config and cache folders:
+        self.config_dir.mkdir(parents=True, exist_ok=True)
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+        self.state_dir.mkdir(parents=True, exist_ok=True)
+
+
+    def select_wallpaper(self, path_str: str) -> None:
+        self.selected_wallpaper = pathlib.Path(path_str)
+
+
+    def shorten_path(self, path: pathlib.Path) -> str:
+        """Replace home part of paths with tilde"""
+        if path.is_relative_to(self.home_path):
+            return str(path).replace(str(self.home_path), "~", 1)
+        elif path:
+            return str(path)
+        else:
+            return ''
+
+
+    def get_image_folder_list(self, section: str, config) -> list[pathlib.Path]:
+        image_folders_str: list[str] =  config.get(section, "folder", fallback = self.image_folder_fallback).split("\n")
+        image_folder_list = [pathlib.Path(path_str).expanduser() for path_str in image_folders_str]
+        return image_folder_list
+
+
+    def read(self) -> None:
+        """Load data from the config.ini or use default if it does not exists"""
+        config = configparser.ConfigParser()
+        config.read(self.config_file, 'utf-8')
+
+        # Read basic parameters:
+        self.fill_option = config.get("Settings", "fill", fallback=self.fill_option)
+        self.sort_option = config.get("Settings", "sort", fallback=self.sort_option)
+        self.backend = config.get("Settings", "backend", fallback=self.backend)
+        self.color = config.get("Settings", "color", fallback=self.color)
+        self.post_command = config.get("Settings", "post_command", fallback=self.post_command)
+        self.swww_transition_type = config.get("Settings", "swww_transition_type", fallback=self.swww_transition_type)
+        self.swww_transition_step = config.get("Settings", "swww_transition_step", fallback=self.swww_transition_step)
+        self.swww_transition_angle = config.get("Settings", "swww_transition_angle", fallback=self.swww_transition_angle)
+        self.swww_transition_duration = config.get("Settings", "swww_transition_duration", fallback=self.swww_transition_duration)
+        self.swww_transition_fps = config.get("Settings", "swww_transition_fps", fallback=self.swww_transition_fps)
+        self.mpvpaper_sound = config.getboolean("Settings", "mpvpaper_sound", fallback=self.mpvpaper_sound)
+        self.mpvpaper_options = config.get("Settings", "mpvpaper_options", fallback=self.mpvpaper_options)
+        self.number_of_columns = int(config.get("Settings", "number_of_columns", fallback=self.number_of_columns))
+        self.lang = config.get("Settings", "language", fallback=self.lang)
+        self.include_subfolders = config.getboolean("Settings", "subfolders", fallback=self.include_subfolders)
+        self.include_all_subfolders = config.getboolean("Settings", "all_subfolders", fallback=self.include_all_subfolders)
+        self.show_hidden = config.getboolean("Settings", "show_hidden", fallback=self.show_hidden)
+        self.show_gifs_only = config.getboolean("Settings", "show_gifs_only", fallback=self.show_gifs_only)
+        self.zen_mode = config.getboolean("Settings", "zen_mode", fallback=self.zen_mode)
+        self.use_xdg_state = config.getboolean("Settings", "use_xdg_state", fallback=self.use_xdg_state)
+        self.show_path_in_tooltip = config.getboolean("Settings", "show_path_in_tooltip", fallback=self.show_path_in_tooltip)
+        self.style_file = config.get("Settings", "stylesheet", fallback=self.style_file)
+
+        # Read and convert strings representing lists and paths:
+        monitors_str = config.get("Settings", "monitors", fallback=self.selected_monitor, raw=True)
+        wallpapers_str = config.get("Settings", "wallpaper", fallback="", raw=True)
+        self.image_folder_list = self.get_image_folder_list("Settings", config)
+        if monitors_str:
+            self.monitors = [str(monitor) for monitor in monitors_str.split("\n")]
+        if wallpapers_str:
+            self.wallpapers = [pathlib.Path(paper).expanduser() for paper in wallpapers_str.split("\n")]
+
+
+    def read_state(self) -> None:
+        """Load data from the state.ini file"""
+        if not self.use_xdg_state:
+            return
+
+        state = configparser.ConfigParser()
+        state.read(self.state_file, 'utf-8')
+
+        # Read and convert strings representing lists and paths:
+        monitors_str = state.get("State", "monitors", fallback=self.selected_monitor, raw=True)
+        wallpapers_str = state.get("State", "wallpaper", fallback="", raw=True)
+        self.image_folder_list = self.get_image_folder_list("State", state)
+        if monitors_str:
+            self.monitors = [str(monitor) for monitor in monitors_str.split("\n")]
+            self.selected_monitor = self.monitors[0]
+        if wallpapers_str:
+            self.wallpapers = [pathlib.Path(paper).expanduser() for paper in wallpapers_str.split("\n")]
+
+
+    def check_validity(self) -> None:
+        """Check if the config parameters are valid and correct them if needed"""
+
+        # Check validity of general options:
+        if self.backend not in BACKEND_OPTIONS:
+            self.backend = self.installed_backends[-1]
+        if self.sort_option not in SORT_OPTIONS:
+            self.sort_option = SORT_OPTIONS[0]
+        if self.fill_option not in FILL_OPTIONS:
+            self.fill_option = FILL_OPTIONS[0]
+        if self.swww_transition_type not in SWWW_TRANSITION_TYPES:
+            self.swww_transition_type = "any"
+        if self.number_of_columns <= 0:
+            self.number_of_columns = 1
+
+        # Check validity of other swww options:
+        if 0 > int(self.swww_transition_angle) > 180:
+            self.swww_transition_angle = 0
+        if 0 > int(self.swww_transition_step) > 255:
+            self.swww_transition_step = 90
+        if 0 > float(self.swww_transition_duration):
+            self.swww_transition_duration = 2
+        if 0 > int(self.swww_transition_fps):
+            self.swww_transition_fps = 60
+
+
+    def attribute_selected_wallpaper(self) -> None:
+        """
+        This function is used before saving config to attribute a wallpaper to a monitor.
+        For example, if only certain monitor was affected,
+        change only corresponding wallpaper in the wallpapers list.
+        Otherwise, reset the list and set one wallpaper for all monitors.
+        """
+        if not self.selected_wallpaper:
+            return
+        if self.selected_monitor == "All":
+            self.monitors = [self.selected_monitor]
+            self.wallpapers = [self.selected_wallpaper]
+        elif self.selected_monitor in self.monitors:
+            index = self.monitors.index(self.selected_monitor)
+            self.wallpapers[index] = self.selected_wallpaper
+        else:
+            self.monitors.append(self.selected_monitor)
+            self.wallpapers.append(self.selected_wallpaper)
+
+
+    def save_state_file(self) -> None:
+        """Save some parameters to the state file"""
+        state = configparser.ConfigParser()
+        state.read(self.state_file)
+        if not state.has_section("State"):
+            state.add_section("State")
+        self.write_folder_list_to_config("State", state)
+        state.set("State", "monitors", "\n".join(self.monitors))
+        state.set("State", "wallpaper", '\n'.join(self.shorten_path(p) for p in self.wallpapers))
+        try:
+            with open(self.state_file, "w") as statefile:
+                state.write(statefile)
+        except PermissionError:
+            print("Could not save state file due to permission error.")
+
+
+    def write_folder_list_to_config(self, section: str, config):
+        config_save_folder_str = ""
+        for i, folder in enumerate(self.image_folder_list):
+            if i == 0:
+                config_save_folder_str += self.shorten_path(folder)
+            else:
+                config_save_folder_str += "\n     " + self.shorten_path(folder)
+        config.set(section, "folder", config_save_folder_str)
+
+
+    def save(self, only_state=False) -> None:
+        """Save current parameters to the configuration file"""
+
+        # Write configuration to the file:
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+        if not config.has_section("Settings"):
+            config.add_section("Settings")
+        config.set("Settings", "language", self.lang)
+
+        # If state file is used, some parameters are not save into config:
+        if not self.use_xdg_state:
+            self.write_folder_list_to_config("Settings", config)
+            config.set("Settings", "monitors", "\n".join(self.monitors))
+            config.set("Settings", "wallpaper", '\n'.join(self.shorten_path(p) for p in self.wallpapers))
+
+        # Save the parameters into config:
+        config.set("Settings", "show_path_in_tooltip", str(self.show_path_in_tooltip))
+        config.set("Settings", "backend", self.backend)
+        config.set("Settings", "fill", self.fill_option)
+        config.set("Settings", "sort", self.sort_option)
+        config.set("Settings", "color", self.color)
+        config.set("Settings", "subfolders", str(self.include_subfolders))
+        config.set("Settings", "all_subfolders", str(self.include_all_subfolders))
+        config.set("Settings", "show_hidden", str(self.show_hidden))
+        config.set("Settings", "show_gifs_only", str(self.show_gifs_only))
+        config.set("Settings", "zen_mode", str(self.zen_mode))
+        config.set("Settings", "post_command", self.post_command)
+        config.set("Settings", "number_of_columns", str(self.number_of_columns))
+        config.set("Settings", "swww_transition_type", str(self.swww_transition_type))
+        config.set("Settings", "swww_transition_step", str(self.swww_transition_step))
+        config.set("Settings", "swww_transition_angle", str(self.swww_transition_angle))
+        config.set("Settings", "swww_transition_duration", str(self.swww_transition_duration))
+        config.set("Settings", "swww_transition_fps", str(self.swww_transition_fps))
+        config.set("Settings", "mpvpaper_sound", str(self.mpvpaper_sound))
+        config.set("Settings", "mpvpaper_options", str(self.mpvpaper_options))
+        config.set("Settings", "use_xdg_state", str(self.use_xdg_state))
+        config.set("Settings", "stylesheet", str(self.style_file))
+
+        try:
+            with open(self.config_file, "w") as configfile:
+                config.write(configfile)
+        except PermissionError:
+            print("Could not save config file due to permission error.")
+
+        # If requested, save the state file:
+        if self.use_xdg_state:
+            self.save_state_file()
+
+
+    def read_parameters_from_user_arguments(self, args: Namespace) -> None:
+        """
+        Read user arguments provided at the run. These values take priority over config.ini
+        :param args Output of the argparse parser.parse_args() method. Should be only used in __main__.py.
+        """
+        if args.backend:
+            self.backend = args.backend
+        if args.fill:
+            self.fill_option = args.fill
+        if args.folder:
+            self.image_folder_list = [pathlib.Path(path_str) for path_str in args.folder]
+        if args.state_file:
+            self.use_xdg_state = True # Use of a custom state file implies state is in a separate file, requires use_xdg_state
+            self.state_file = pathlib.Path(args.state_file).expanduser()
+        if args.no_post_command:
+            self.use_post_command = False
