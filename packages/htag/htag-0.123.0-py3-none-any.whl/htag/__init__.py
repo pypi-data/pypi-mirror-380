e5ed9f788345384310f3b56ce632dcc916c9256d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:05 +0000

    Add package htag/0.123.0/htag-0.123.0-py3-none-any.whl

---
+++ packages/htag/htag-0.123.0-py3-none-any.whl/htag/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2022 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+
+__version__ = "0.123.0" # auto-updated
+
+from .tag import Tag,HTagException,expose
+from .runners import Runner
+
+__all__= ["Tag","HTagException","expose","Runner"]
+
