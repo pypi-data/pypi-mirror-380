51a4a0179e286c32268ddab913d5cd450fdf0ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:31 +0000

    Add package ratemaking/0.1.1/ratemaking-0.1.1.tar.gz

---
+++ packages/ratemaking/ratemaking-0.1.1.tar.gz/ratemaking-0.1.1/ratemaking/trending/trend_analysis.py
@@ -0,0 +1,22 @@
+"""
+Trend analysis functions for loss costs, frequency, and severity
+
+This module will contain:
+- Linear and exponential trend fitting
+- Trend factor calculations  
+- Economic indicator trending
+- Inflation adjustments
+
+TODO: Implementation coming in future releases
+"""
+
+def calculate_trend_factor():
+    """
+    Calculate trend factors for loss costs or frequency/severity.
+    
+    TODO: Implementation coming in future releases
+    """
+    raise NotImplementedError("Trending tools not yet implemented")
+
+
+__all__ = ['calculate_trend_factor']
