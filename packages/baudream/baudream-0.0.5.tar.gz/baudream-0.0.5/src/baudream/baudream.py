c5eb79dac55036c61bfe8b593b52fbfe7e31ca7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:05:04 +0000

    Add package baudream/0.0.5/baudream-0.0.5.tar.gz

---
+++ packages/baudream/baudream-0.0.5.tar.gz/baudream-0.0.5/src/baudream/baudream.py
@@ -0,0 +1,21 @@
+import subprocess, sys
+from asyncio.log import logger
+import importlib.resources as pkg_resources
+
+def open_file(file_path):
+    try:
+        if sys.platform == 'linux':
+            subprocess.run(['xdg-open', file_path])
+        elif sys.platform == 'darwin':
+            subprocess.run(['open', file_path])
+        else:
+            logger.error('Unsupported OS')
+    except Exception as e:
+        raise RuntimeError(f"Failed to open file: {e}")
+
+
+
+def main():
+    file = 'presentation_ogiv.pptx'
+    with pkg_resources.path('baudream.ressources', file) as file_path:
+        open_file(file_path)
