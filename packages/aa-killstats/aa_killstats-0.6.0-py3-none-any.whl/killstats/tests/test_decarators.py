ef23bccb908cf2160ae9ba9e27193ef6f7ec3b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:20:55 +0000

    Add package aa-killstats/0.6.0/aa_killstats-0.6.0-py3-none-any.whl

---
+++ packages/aa-killstats/aa_killstats-0.6.0-py3-none-any.whl/killstats/tests/test_decarators.py
@@ -0,0 +1,74 @@
+# Standard Library
+from unittest.mock import patch
+
+# Django
+from django.test import TestCase
+
+# Alliance Auth
+from allianceauth.services.hooks import get_extension_logger
+
+# Alliance Auth (External Libs)
+from app_utils.esi import EsiDailyDowntime
+from app_utils.logging import LoggerAddTag
+
+# AA Killstats
+from killstats import __title__
+from killstats.decorators import when_esi_is_available
+
+
+class TestDecorators(TestCase):
+    @patch("killstats.decorators.fetch_esi_status")
+    @patch("killstats.decorators.IS_TESTING", new=False)
+    def test_when_esi_is_available(self, mock_fetch_esi_status):
+        # given
+        @when_esi_is_available
+        def trigger_esi_deco():
+            return "Esi is Available"
+
+        # when
+        result = trigger_esi_deco()
+        # then
+        mock_fetch_esi_status.assert_called_once()
+        self.assertEqual(result, "Esi is Available")
+
+    @patch("killstats.decorators.fetch_esi_status", side_effect=EsiDailyDowntime)
+    @patch("killstats.decorators.IS_TESTING", new=False)
+    def test_when_esi_is_available_downtime(self, mock_fetch_esi_status):
+        # given
+        @when_esi_is_available
+        def trigger_esi_deco():
+            return "Daily Downtime detected. Aborting."
+
+        # when
+        result = trigger_esi_deco()
+        # then
+        self.assertIsNone(result)
+
+    @patch("killstats.decorators.fetch_esi_status")
+    @patch("killstats.decorators.IS_TESTING", new=True)
+    def test_when_esi_is_available_is_test(self, mock_fetch_esi_status):
+        # given
+        @when_esi_is_available
+        def trigger_esi_deco():
+            return "Teesting Mode."
+
+        # when
+        result = trigger_esi_deco()
+        # then
+        self.assertEqual(result, "Teesting Mode.")
+
+    def test_log_timing(self):
+        # given
+        # AA Killstats
+        from killstats.decorators import log_timing
+
+        logger = LoggerAddTag(get_extension_logger(__name__), __title__)
+
+        @log_timing(logger)
+        def trigger_log_timing():
+            return "Log Timing"
+
+        # when
+        result = trigger_log_timing()
+        # then
+        self.assertEqual(result, "Log Timing")
