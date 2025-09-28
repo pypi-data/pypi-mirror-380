6c7d62dfa6ccd81e75cf144eac22605b070cedc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:20:55 +0000

    Add package simple-math-moh/1.0/simple_math_moh-1.0.tar.gz

---
+++ packages/simple-math-moh/simple_math_moh-1.0.tar.gz/simple_math_moh-1.0/README.md
@@ -0,0 +1,7 @@
+# SimpleMath
+
+A beginner-friendly Python package that can **add, multiply, and divide** numbers.
+
+## Installation
+```bash
+pip install simplemath
---
+++ packages/simple-math-moh/simple_math_moh-1.0.tar.gz/simple_math_moh-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="simple_math_moh",  # package name (unique on PyPI)
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[],  # no external dependencies
+    description="A simple math package for beginners (add, multiply, divide)",
+    author="Mohamed Adam",
+    author_email="mohan223@gmail.com",
+    # url="https://github.com/yourname/simplemath",  # optional
+)
