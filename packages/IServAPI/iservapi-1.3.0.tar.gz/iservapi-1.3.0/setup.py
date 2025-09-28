4146b80e213203125a02c57a51635c81e612baee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:05:12 +0000

    Add package IServAPI/1.3.0/iservapi-1.3.0.tar.gz

---
+++ packages/IServAPI/iservapi-1.3.0.tar.gz/iservapi-1.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="IServAPI",
+    version="1.3.0",
+    author="Leo Aqua",
+    author_email="contact@leoaqua.de",
+    description="Unofficial API for IServ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Leo-Aqua/IServAPI",
+    keywords=["IServ", "IServAPI", "iserv api", "iserv-api", "API", "Leo-Aqua"],
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "lxml",
+        "webdavclient",
+        "pandas",
+        "html5lib",
+        "dateutil",
+        "typing",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
