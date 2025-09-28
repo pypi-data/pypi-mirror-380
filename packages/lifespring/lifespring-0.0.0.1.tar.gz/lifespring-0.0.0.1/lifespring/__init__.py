759f80ad8da6a9471c3afda5d4e7295e8d0e518e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 14:04:01 +0000

    Add package lifespring/0.0.0.1/lifespring-0.0.0.1.tar.gz

---
+++ packages/lifespring/lifespring-0.0.0.1.tar.gz/lifespring-0.0.0.1/lifespring/__init__.py
@@ -0,0 +1,8 @@
+from .i18n import set_language
+from .i18n import init_language
+
+
+__all__ = [
+    "set_language",
+    "init_language",
+]
---
+++ packages/lifespring/lifespring-0.0.0.1.tar.gz/lifespring-0.0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name = "lifespring",
+    version = "0.0.0.1",
+    packages = find_packages(),
+    description = "A personal account keeper.",
+    author = "parkcai",
+    author_email = "sun_retailer@163.com",
+    url = "https://github.com/parkcai/lifespring",
+    include_package_data = True,
+    package_data = {
+        "pywheels": ["locales/**/LC_MESSAGES/*.mo"],
+    },
+    python_requires = ">=3.8",
+    install_requires = [
+        "numpy>=1.21.0",
+    ],
+)
