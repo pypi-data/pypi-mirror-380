bd8408458f7f3fa881283fbe99208e2e67ae72e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 03:21:01 +0000

    Add package Instaweb/1.4/instaweb-1.4-py3-none-any.whl

---
+++ packages/Instaweb/instaweb-1.4-py3-none-any.whl/Instaweb/__init__.py
@@ -0,0 +1,14 @@
+from .client import InstaClient, FileError, UnknowError, FailError, connectionError, PasswordError, loginRequired, challengeRequired, Twofacteur, MdpIncorrect
+
+__all__ = [
+  "InstaClient",
+  "FileError",
+  "UnknowError",
+  "FailError",
+  "connectionError",
+  "PasswordError",
+  "loginRequired",
+  "challengeRequired"
+  "Twofacteur",
+  "MdpIncorrect"
+]
