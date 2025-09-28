fbe35b39c95253b8ef0c3f942bb3cb634eac177c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:58 +0000

    Add package cleandb/5.0.2.1/cleandb-5.0.2.1.tar.gz

---
+++ packages/cleandb/cleandb-5.0.2.1.tar.gz/cleandb-5.0.2.1/cleandb/__init__.py
@@ -0,0 +1,14 @@
+# mdb/__init__.py
+
+from .main import (
+    w,
+    r,
+    a,
+    d,
+    backup,
+    snapshot,
+    debug,
+    help
+)
+
+__all__ = ["w", "r", "a", "d", "backup", "snapshot", "debug", "help"]
