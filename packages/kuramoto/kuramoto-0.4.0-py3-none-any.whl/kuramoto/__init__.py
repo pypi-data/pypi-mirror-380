8cfc090bc3e0f982eed7e13ac777845f3aef98b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-28 04:04:01 +0000

    Add package kuramoto/0.4.0/kuramoto-0.4.0-py3-none-any.whl

---
+++ packages/kuramoto/kuramoto-0.4.0-py3-none-any.whl/kuramoto/__init__.py
@@ -0,0 +1,10 @@
+from .kuramoto import Kuramoto
+from .plotting import plot_activity, plot_phase_coherence
+
+__version__ = "0.4.0"
+
+__all__ = [
+    "Kuramoto",
+    "plot_activity",
+    "plot_phase_coherence",
+]
