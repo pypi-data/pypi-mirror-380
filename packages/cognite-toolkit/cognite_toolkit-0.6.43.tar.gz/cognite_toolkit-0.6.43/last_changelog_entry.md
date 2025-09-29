1f7304a7f052e0b9adb19deefe4102fc726b7394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:06:02 +0000

    Add package cognite-toolkit/0.6.43/cognite_toolkit-0.6.43.tar.gz

---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.43.tar.gz/cognite_toolkit-0.6.43/last_changelog_entry.md
@@ -0,0 +1,14 @@
+## cdf 
+
+### Improved
+
+- Improved guidance when starting toolkit. 
+
+### Added
+
+-  Deprecation warning on cdf init command
+
+
+## templates
+
+No changes.
---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.43.tar.gz/cognite_toolkit-0.6.43/tests/test_unit/test_cdf_tk/test_utils/test_file.py
@@ -0,0 +1,98 @@
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import pytest
+
+from cognite_toolkit._cdf_tk.exceptions import ToolkitFileNotFoundError
+from cognite_toolkit._cdf_tk.utils.file import find_adjacent_files, sanitize_filename
+
+
+class TestFindAdjacentFiles:
+    @pytest.mark.parametrize(
+        "datafile, suffix, available_files, expected_files",
+        [
+            pytest.param(
+                "my_table-part0001.RawRows.ndjson",
+                ".Table.yaml",
+                ["my_table.Table.yaml", "my_table.Table.json"],
+                ["my_table.Table.yaml"],
+                id="single matching file",
+            ),
+            pytest.param(
+                "my_table-part0001.RawRows.ndjson",
+                ".Table.yaml",
+                ["other_file.Table.yaml", "my_table.Table.json"],
+                [],
+                id="no matching file",
+            ),
+            pytest.param(
+                "my_table-part0001.RawRows.ndjson",
+                ".Table.yaml",
+                ["my_table.Table.yaml", "my_table.Table.yaml", "my_table.Table.json"],
+                ["my_table.Table.yaml", "my_table.Table.yaml"],
+                id="multiple matching files",
+            ),
+            pytest.param(
+                "my_table-part0001.RawRows.ndjson",
+                ".Table.yaml",
+                ["not_my_table.Table.yaml", "my_table2.Table.yaml"],
+                [],
+                id="similar names but no match",
+            ),
+            pytest.param(
+                "my_table-part0001.RawRows.ndjson",
+                ".DoesNotExist",
+                ["my_table.Table.yaml", "my_table.Table.json"],
+                [],
+                id="no matching suffix",
+            ),
+        ],
+    )
+    def test_find_adjacent_files(
+        self, datafile: str, suffix: str, available_files: list[str], expected_files: list[str]
+    ) -> None:
+        filepath = MagicMock(spec=Path)
+        filepath.is_file.return_value = True
+        filepath.name = datafile
+        directory = MagicMock(spec=Path)
+        filepath.parent = directory
+        directory_files: list[Path] = []
+        for file in available_files:
+            mock_file = MagicMock(spec=Path)
+            mock_file.name = file
+            directory_files.append(mock_file)
+        directory.glob.return_value = directory_files
+
+        result = find_adjacent_files(filepath, suffix=suffix)
+
+        actual = [file.name for file in result]
+        assert actual == expected_files
+
+    def test_find_adjacent_files_not_a_file(self):
+        filepath = MagicMock(spec=Path)
+        filepath.is_file.return_value = False
+        filepath.name = "my_table-part0001.RawRows.ndjson"
+        with pytest.raises(ToolkitFileNotFoundError):
+            find_adjacent_files(filepath, suffix=".Table.yaml")
+
+
+class TestSanitizeFilename:
+    @pytest.mark.parametrize(
+        "filename, expected",
+        [
+            ("valid_filename.yaml", "valid_filename.yaml"),
+            ("another-valid_filename123.json", "another-valid_filename123.json"),
+            ("invalid/filename.yaml", "invalid_filename.yaml"),
+            ("invalid\\filename.json", "invalid_filename.json"),
+            ("invalid:filename.yaml", "invalid_filename.yaml"),
+            ("invalid*filename.json", "invalid_filename.json"),
+            ("invalid?filename.yaml", "invalid_filename.yaml"),
+            ('invalid"filename.json', "invalid_filename.json"),
+            ("invalid<filename.yaml", "invalid_filename.yaml"),
+            ("invalid>filename.json", "invalid_filename.json"),
+            ("invalid|filename.yaml", "invalid_filename.yaml"),
+            ("inva|lid:fi*le?name<.json", "inva_lid_fi_le_name_.json"),
+        ],
+    )
+    def test_sanitize_filename(self, filename: str, expected: str) -> None:
+        assert sanitize_filename(filename) == expected
