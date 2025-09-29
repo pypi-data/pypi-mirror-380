bb9b98e9d17db1ceaecbbd17ef4f5225c626f9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 17:04:54 +0000

    Add package sphinx-notionbuilder/2025.9.29/sphinx_notionbuilder-2025.9.29.tar.gz

---
+++ packages/sphinx-notionbuilder/sphinx_notionbuilder-2025.9.29.tar.gz/sphinx_notionbuilder-2025.9.29/sample/conf.py
@@ -0,0 +1,12 @@
+"""
+Configuration for Sphinx.
+"""
+
+extensions = [
+    "sphinxcontrib.video",
+    "sphinxnotes.strike",
+    "sphinx_notion",
+    "sphinx_simplepdf",
+    "sphinx_toolbox.collapse",
+    "atsphinx.audioplayer",
+]
