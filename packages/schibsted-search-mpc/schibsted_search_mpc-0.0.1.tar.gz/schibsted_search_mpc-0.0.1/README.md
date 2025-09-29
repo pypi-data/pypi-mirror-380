03699fd25878f9f50cc21eef15063cc74997b74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:05:19 +0000

    Add package schibsted-search-mpc/0.0.1/schibsted_search_mpc-0.0.1.tar.gz

---
+++ packages/schibsted-search-mpc/schibsted_search_mpc-0.0.1.tar.gz/schibsted_search_mpc-0.0.1/README.md
@@ -0,0 +1,5 @@
+# schibsted-search-mpc
+
+This is a package owned by Schibsted Appication Security Team.
+
+**Dependency Confusion Prevention.**
---
+++ packages/schibsted-search-mpc/schibsted_search_mpc-0.0.1.tar.gz/schibsted_search_mpc-0.0.1/schibsted_search_mpc/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is a placeholder package intentionally published to PyPI to prevent
+dependency confusion attacks. It contains no functional code.
+"""
+
+__version__ = "0.0.1"
