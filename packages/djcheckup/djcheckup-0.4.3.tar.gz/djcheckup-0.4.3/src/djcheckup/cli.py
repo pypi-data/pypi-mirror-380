3dd1dcb588eaed2722e971eb6e458f3f0057f2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 14:05:16 +0000

    Add package djcheckup/0.4.3/djcheckup-0.4.3.tar.gz

---
+++ packages/djcheckup/djcheckup-0.4.3.tar.gz/djcheckup-0.4.3/src/djcheckup/cli.py
@@ -0,0 +1,31 @@
+"""Command line interface."""
+
+from typing import Annotated
+
+import rich
+import typer
+
+from djcheckup.check_defs import all_checks
+from djcheckup.checks import SiteChecker
+from djcheckup.outputs import rich_output, sitecheck_as_json
+
+app = typer.Typer()
+
+
+@app.command()
+def run_checks(
+    url: Annotated[str, typer.Argument(help="The URL to check.")],
+    *,
+    output_json: Annotated[
+        bool,
+        typer.Option(help="Prints the results in JSON format."),
+    ] = False,
+) -> None:
+    """Run the DJ Checkup tool against a specific URL."""
+    checker = SiteChecker(url=url)
+    results = checker.run_checks(all_checks)
+
+    if output_json:
+        rich.print_json(sitecheck_as_json(results))
+    else:
+        rich_output(results)
