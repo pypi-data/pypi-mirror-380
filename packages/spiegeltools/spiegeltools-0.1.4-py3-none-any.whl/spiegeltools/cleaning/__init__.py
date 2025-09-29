5afb0eda1065973bbc746762b72b6d24c2e7134f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:15 +0000

    Add package spiegeltools/0.1.4/spiegeltools-0.1.4-py3-none-any.whl

---
+++ packages/spiegeltools/spiegeltools-0.1.4-py3-none-any.whl/spiegeltools/cleaning/__init__.py
@@ -0,0 +1,3 @@
+from .ukdata_cleaning import compare_dataframes, merge_csv_files, set_location, split_month_year, compute_crime_stats, prepare_lsoa_crime_data, get_lsoa_boundaries
+
+__all__ = ["compare_dataframes", "merge_csv_files", "set_location", "split_month_year", "compute_crime_stats", "prepare_lsoa_crime_data", "get_lsoa_boundaries"]
