18f876daf5df054106cf36be806dd8dc25081a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 14:05:05 +0000

    Add package machineconfig/4.9/machineconfig-4.9.tar.gz

---
+++ packages/machineconfig/machineconfig-4.9.tar.gz/machineconfig-4.9/src/machineconfig/jobs/python/python_ve_symlink.py
@@ -0,0 +1,29 @@
+"""Symlinks"""
+
+from machineconfig.utils.path_extended import PathExtended as PathExtended
+# from machineconfig.utils.utils import choose_from_options
+
+
+def main():
+    print(f"""
+{"=" * 150}
+🔗 SYMLINK CREATOR | Create symlinks for virtual environments
+{"=" * 150}
+""")
+    target = PathExtended(input("🎯 Symlink to which target? ")).expanduser().absolute()
+    source = input(f"📍 Symlink from which source? [default to: CWD/{target.name}] ") or PathExtended.cwd().joinpath(target.name)
+    if isinstance(source, str):
+        source = PathExtended(source).expanduser().absolute()
+    source.symlink_to(target, overwrite=True)
+    print(f"""
+{"=" * 150}
+✅ SUCCESS | Symlink created successfully
+📍 Source: {source}
+🎯 Target: {target}
+{"=" * 150}
+""")
+    print("🔗 Finished creating symlink.")
+
+
+if __name__ == "__main__":
+    pass
