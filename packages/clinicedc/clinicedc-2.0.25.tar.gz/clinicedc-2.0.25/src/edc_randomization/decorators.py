9fae2e9d05e11cd52e51660be4f6664cb86213f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:44 +0000

    Add package clinicedc/2.0.25/clinicedc-2.0.25.tar.gz

---
+++ packages/clinicedc/clinicedc-2.0.25.tar.gz/clinicedc-2.0.25/src/edc_randomization/decorators.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+from .randomizer import Randomizer
+from .site_randomizers import site_randomizers
+
+
+class RegisterRandomizerError(Exception):
+    pass
+
+
+def register(site=None, **kwargs) -> Any:
+    """Registers a randomizer class."""
+    site = site or site_randomizers
+
+    def _register_randomizer_cls_wrapper(randomizer_cls: Any) -> Any:
+        if not issubclass(randomizer_cls, (Randomizer,)):
+            raise RegisterRandomizerError(
+                f"Wrapped class must a Randomizer class. Got {randomizer_cls}"
+            )
+        site.register(randomizer_cls)
+        return randomizer_cls
+
+    return _register_randomizer_cls_wrapper
