ce6896e7e5ed0be8e7436d246f93277eee145de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:35 +0000

    Add package hypofuzz/25.9.2/hypofuzz-25.9.2.tar.gz

---
+++ packages/hypofuzz/hypofuzz-25.9.2.tar.gz/hypofuzz-25.9.2/src/hypofuzz/__init__.py
@@ -0,0 +1,6 @@
+"""Adaptive fuzzing for property-based tests using Hypothesis."""
+
+from hypofuzz.detection import in_hypofuzz_run
+
+__version__ = "25.09.02"
+__all__: list[str] = ["in_hypofuzz_run"]
