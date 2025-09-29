af62200b65341414119d74b5ca4eb8c14f25c170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:42 +0000

    Add package theMostPowerfulCatEver/0.4/themostpowerfulcatever-0.4.tar.gz

---
+++ packages/theMostPowerfulCatEver/themostpowerfulcatever-0.4.tar.gz/themostpowerfulcatever-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="theMostPowerfulCatEver",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "meow=theMostPowerfulCatEver:meow",
+        ]
+    }
+)
