7e733df6f0210ef141a639fcbe35ccbfacfc8c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 03:20:15 +0000

    Add package pythonic-fp/3.1.1/pythonic_fp-3.1.1.tar.gz

---
+++ packages/pythonic-fp/pythonic_fp-3.1.1.tar.gz/pythonic_fp-3.1.1/src/pythonic_fp/homepage/intro.py
@@ -0,0 +1,29 @@
+"""
+Pythonic FP Projects
+====================
+
+Collection of Functional Programming (FP) PyPI projects.
+
+The overall project's name is **Pythonic FP** and consists of PyPI
+projects with PyPI and GitHub names beginning `python-fp`. All project
+libraries install under the Python ``pythonic_fp`` namespace.
+
+Goals
+-----
+
+- assist in a functional style of programming
+- support stateful as well as functional paradigms
+- be extensible, type safe, and efficient
+- threadsafe when practical, thread tolerant when efficiency important
+- have good documentation
+- fulling support typing tools like `mypy <https://mypy-lang.org/>`_
+- be pythonic
+
+Non-Goals
+---------
+
+- try and make Python a pure functional language like Haskell
+- implement a functional domain specific language in Python
+- force users to adopt unpythonic cryptic coding styles
+
+"""
