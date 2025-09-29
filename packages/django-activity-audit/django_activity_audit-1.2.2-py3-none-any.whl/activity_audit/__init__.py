87b00aef0a0f183148a95fc926002815b5acb132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:04:35 +0000

    Add package django-activity-audit/1.2.2/django_activity_audit-1.2.2-py3-none-any.whl

---
+++ packages/django-activity-audit/django_activity_audit-1.2.2-py3-none-any.whl/activity_audit/__init__.py
@@ -0,0 +1,19 @@
+default_app_config = "activity_audit.apps.AuditLoggingConfig"
+
+from activity_audit.utils import (
+    get_api_handler,
+    get_audit_handler,
+    get_console_formatter,
+    get_json_formatter,
+    get_json_handler,
+    get_login_handler,
+)
+
+__all__ = [
+    "get_console_formatter",
+    "get_json_formatter",
+    "get_json_handler",
+    "get_api_handler",
+    "get_audit_handler",
+    "get_login_handler",
+]
