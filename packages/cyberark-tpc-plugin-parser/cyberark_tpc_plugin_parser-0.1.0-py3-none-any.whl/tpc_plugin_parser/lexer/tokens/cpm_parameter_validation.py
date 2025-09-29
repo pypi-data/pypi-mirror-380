6a24478c22fb98b115aa743b1bfa8c000a963beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 05:05:41 +0000

    Add package cyberark-tpc-plugin-parser/0.1.0/cyberark_tpc_plugin_parser-0.1.0-py3-none-any.whl

---
+++ packages/cyberark-tpc-plugin-parser/cyberark_tpc_plugin_parser-0.1.0-py3-none-any.whl/tpc_plugin_parser/lexer/tokens/cpm_parameter_validation.py
@@ -0,0 +1,17 @@
+"""Class to hold the parameter validation token."""
+
+from dataclasses import dataclass
+
+from tpc_plugin_parser.lexer.utilities.token_name import TokenName
+
+
+@dataclass(frozen=True)
+class CPMParameterValidation(object):
+    """Dataclass to hold variable cpm parameter validation details."""
+
+    line_number: int
+    name: str
+    source: str
+    mandatory: str
+    allow_characters: str | None = None
+    token_name: str = TokenName.CPM_PARAMETER_VALIDATION.value
