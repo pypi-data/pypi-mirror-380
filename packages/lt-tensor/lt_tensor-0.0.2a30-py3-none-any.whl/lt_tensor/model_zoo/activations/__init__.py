5a14f1ee4218b5a0ec20b27239381b614c85b2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:45 +0000

    Add package lt-tensor/0.0.2a30/lt_tensor-0.0.2a30-py3-none-any.whl

---
+++ packages/lt-tensor/lt_tensor-0.0.2a30-py3-none-any.whl/lt_tensor/model_zoo/activations/__init__.py
@@ -0,0 +1,44 @@
+from .alias_free import Activation1d as Alias1d, Activation2d as Alias2d
+from .snake import Snake, SnakeBeta
+from .base import ScaleMinMax, SoftCELU
+from .oscillator import (
+    Oscillator,
+    OscillatorMul,
+    OscillatorSum,
+    SquaredOscillatorMul,
+    SquaredOscillatorSum,
+)
+from .trop_set import (
+    TropicalPoly,
+    TropicalPolyMul,
+    TropicalPolySum,
+    Hermite,
+    HermiteMul,
+    HermiteSum,
+    Fourier,
+    FourierMul,
+    FourierSum,
+)
+
+__all__ = [
+    "Alias1d",
+    "Alias2d",
+    "SoftCELU",
+    "SnakeBeta",
+    "Snake",
+    "ScaleMinMax",
+    "TropicalPoly",
+    "TropicalPolyMul",
+    "TropicalPolySum",
+    "Hermite",
+    "HermiteMul",
+    "HermiteSum",
+    "Fourier",
+    "FourierMul",
+    "FourierSum",
+    "Oscillator",
+    "OscillatorMul",
+    "OscillatorSum",
+    "SquaredOscillatorMul",
+    "SquaredOscillatorSum",
+]
