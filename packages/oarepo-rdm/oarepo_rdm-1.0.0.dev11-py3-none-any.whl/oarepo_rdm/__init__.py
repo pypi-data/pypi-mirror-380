8fd7b5565300c97215c8894fc0e4e69a88cf460d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:21 +0000

    Add package oarepo-rdm/1.0.0.dev11/oarepo_rdm-1.0.0.dev11-py3-none-any.whl

---
+++ packages/oarepo-rdm/oarepo_rdm-1.0.0.dev11-py3-none-any.whl/oarepo_rdm/__init__.py
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-rdm (see https://github.com/oarepo/oarepo-rdm).
+#
+# oarepo-rdm is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""OARepo extensions for invenio-rdm-records and invenio-app-rdm."""
+
+from __future__ import annotations
+
+from .model.presets import rdm_preset
+
+__version__ = "1.0.0dev11"
+"""Version of the library."""
+
+__all__ = ("__version__", "rdm_preset")
