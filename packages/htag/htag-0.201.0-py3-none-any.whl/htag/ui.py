b2f5fc91ee79a255000475dc763d3d924b28d0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 03:20:20 +0000

    Add package htag/0.201.0/htag-0.201.0-py3-none-any.whl

---
+++ packages/htag/htag-0.201.0-py3-none-any.whl/htag/ui.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# # #############################################################################
+# Copyright (C) 2024 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+try:
+  from htagui.basics import *  # htagui>=0.3
+except ImportError:
+  from htagui import *  # htagui<0.3
