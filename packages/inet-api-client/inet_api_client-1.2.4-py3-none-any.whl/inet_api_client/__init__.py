0764a53ae18bb459bd9470c57daf5e8309a9cfba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 14:05:13 +0000

    Add package inet-api-client/1.2.4/inet_api_client-1.2.4-py3-none-any.whl

---
+++ packages/inet-api-client/inet_api_client-1.2.4-py3-none-any.whl/inet_api_client/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Inet API Client
+独立的PyPI包，用于访问Sky Cloud平台API
+"""
+
+from .client import ApiClient
+from .exceptions import SkyApiException, AuthenticationError, APIRequestError
+
+__version__ = "1.2.4"
+__author__ = "Bobby Sheng <Bobby@sky-cloud.net>"
+
+__all__ = [
+    "ApiClient",
+    "SkyApiException", 
+    "AuthenticationError",
+    "APIRequestError"
+]
