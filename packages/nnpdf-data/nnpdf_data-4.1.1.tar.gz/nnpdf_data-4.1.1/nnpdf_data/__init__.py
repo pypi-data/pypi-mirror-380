c79be5882223d663eb166d8793bfe5279e5d961c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 03:20:41 +0000

    Add package nnpdf-data/4.1.1/nnpdf_data-4.1.1.tar.gz

---
+++ packages/nnpdf-data/nnpdf_data-4.1.1.tar.gz/nnpdf_data-4.1.1/nnpdf_data/__init__.py
@@ -0,0 +1,21 @@
+import pathlib
+
+from ._version import __version__
+from .commondataparser import parse_new_metadata
+from .validphys_compatibility import legacy_to_new_map
+
+path_vpdata = pathlib.Path(__file__).parent
+path_commondata = path_vpdata / "commondata"
+theory_cards = path_vpdata / "theory_cards"
+
+
+def load_dataset_metadata(dataset_name, variant=None):
+    """Given a dataset name, return the metadata"""
+    # Compatibility with old nnpdf names, these two lines
+    # might disappear at any given point
+    if variant is None:
+        dataset_name, variant = legacy_to_new_map(dataset_name)
+
+    setname, observable = dataset_name.rsplit("_", 1)
+    metadata_file = path_commondata / setname / "metadata.yaml"
+    return parse_new_metadata(metadata_file, observable, variant=variant)
