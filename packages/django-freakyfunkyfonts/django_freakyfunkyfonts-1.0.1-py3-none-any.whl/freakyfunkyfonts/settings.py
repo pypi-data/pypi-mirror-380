d006d80a1283cbd21a7a6d9506db1f684f58396b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 05:05:54 +0000

    Add package django-freakyfunkyfonts/1.0.1/django_freakyfunkyfonts-1.0.1-py3-none-any.whl

---
+++ packages/django-freakyfunkyfonts/django_freakyfunkyfonts-1.0.1-py3-none-any.whl/freakyfunkyfonts/settings.py
@@ -0,0 +1,25 @@
+import os
+import tomllib  # Python 3.11+; for older use `tomli`
+
+DEFAULTS = {
+    "fonts": {
+        "pool": ["Arial", "Courier New", "Times New Roman", "Verdana", "Comic Sans MS"],
+    },
+    "inject": {
+        "google_fonts_link": None,
+    },
+    "behaviour": {
+        "skip_tags": ["script", "style", "noscript", "title"],
+    },
+}
+
+def load_config():
+    path = os.path.join(os.getcwd(), "funkyfonts.toml")
+    config = DEFAULTS.copy()
+    if os.path.exists(path):
+        with open(path, "rb") as f:
+            user_conf = tomllib.load(f)
+            # deep merge
+            for section, values in user_conf.items():
+                config[section].update(values)
+    return config
