423442527e2ed9783684605364f0ca0b5706ad56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 03:20:09 +0000

    Add package lattica/1.0.0/lattica-1.0.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/lattica/lattica-1.0.0-cp311-cp311-macosx_11_0_arm64.whl/lattica/__init__.py
@@ -0,0 +1,15 @@
+from .client import Lattica
+from .connection_handler import (
+    ConnectionHandler
+)
+
+from lattica_python_core import rpc_method, rpc_stream
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Lattica",
+    "rpc_method",
+    "rpc_stream",
+    "ConnectionHandler",
+]
