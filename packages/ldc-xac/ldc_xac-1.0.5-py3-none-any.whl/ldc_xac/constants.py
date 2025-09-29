231b1c631d9148b9ada680fed42b8ad4deff279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 17:04:46 +0000

    Add package ldc-xac/1.0.5/ldc_xac-1.0.5-py3-none-any.whl

---
+++ packages/ldc-xac/ldc_xac-1.0.5-py3-none-any.whl/ldc_xac/constants.py
@@ -0,0 +1,13 @@
+"""
+Constants for external API logging.
+"""
+
+
+class ExternalAPILoggingConstants:
+    """Constants used for external API logging."""
+    
+    EXTERNAL_REQUEST = "External API Request"
+    EXTERNAL_RESPONSE = "External API Response"
+    EXTERNAL_ERROR = "External API Error"
+    EXTERNAL_SYSTEM = "EXTERNAL"
+    API_CALL = "API_CALL"
