1bcaff8e9126917d5eab50640134dd48413f331a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:52 +0000

    Add package ais-dagger-agents-config/0.1.23/ais_dagger_agents_config-0.1.23-py3-none-any.whl

---
+++ packages/ais-dagger-agents-config/ais_dagger_agents_config-0.1.23-py3-none-any.whl/ais_dagger_agents_config/__init__.py
@@ -0,0 +1,42 @@
+"""Shared configuration models for Dagger Agents."""
+
+from .models import (
+    YAMLConfig,
+    ContainerConfig,
+    ConcurrencyConfig,
+    GitConfig,
+    IndexingConfig,
+    TestGenerationConfig,
+    ReporterConfig,
+    CoreAPIConfig,
+    LLMCredentials,
+    SymbolProperties,
+    Neo4jConfig,
+    SmellConfig,
+    SmellThresholdsConfig,
+    SmellDetectorsConfig,
+    CodeMapConfig,
+    TestingConfig,
+)
+
+
+__version__ = "0.1.23"
+
+__all__ = [
+    "YAMLConfig",
+    "ContainerConfig",
+    "LLMCredentials",
+    "SymbolProperties",
+    "ConcurrencyConfig",
+    "GitConfig",
+    "IndexingConfig",
+    "TestGenerationConfig",
+    "ReporterConfig",
+    "CoreAPIConfig",
+    "Neo4jConfig",
+    "SmellConfig",
+    "SmellThresholdsConfig",
+    "SmellDetectorsConfig",
+    "CodeMapConfig",
+    "TestingConfig",
+]
