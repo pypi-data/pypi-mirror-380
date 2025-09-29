90db0b9452a63e4db3474e7132c8649bc898c4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 05:05:52 +0000

    Add package PyMultiHelper/1.1.20/pymultihelper-1.1.20.tar.gz

---
+++ packages/PyMultiHelper/pymultihelper-1.1.20.tar.gz/pymultihelper-1.1.20/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PyMultiHelper',
+    version='1.1.20',
+    packages=find_packages(),
+    description='Several common helpers for Python coding',
+    author='Anderson',
+    author_email='anderbytes@gmail.com',
+    url='https://github.com/anderbytes/PyMultiHelper',
+
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+
+    install_requires=[
+        "requests>=2.32.3",
+        "pytz>=2024.2",
+        "pandas>=2.3.2",
+        "speedtest-cli>=2.1.3",
+        "beautifulsoup4>=4.12.3"
+    ]
+)
