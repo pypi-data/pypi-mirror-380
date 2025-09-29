bab8070c9ff6515d751382d6400dd8afdce3a405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 17:04:55 +0000

    Add package designer-dna/0.0.1/designer_dna-0.0.1-cp314-cp314-win_amd64.whl

---
+++ packages/designer-dna/designer_dna-0.0.1-cp314-cp314-win_amd64.whl/designer_dna/_oligonucleotides.pyi
@@ -0,0 +1,45 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2025, Spill-Tea
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Oligonucleotide functions with the help of C++."""
+
+def manacher(sequence: str, dna: bool = ...) -> str:
+    """Find the longest palindromic substring within a nucleotide sequence.
+
+    Args:
+        sequence (str): Nucleotide sequence string.
+        dna (bool): Sequence is DNA, else RNA.
+
+    Returns:
+        (str) Longest palindromic substring within a sequence.
+
+    Notes:
+        * This is a cython/c++ implementation of the O(n) Manacher's algorithm.
+
+    """
