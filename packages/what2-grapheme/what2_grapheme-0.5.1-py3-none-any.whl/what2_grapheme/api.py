61ea8523506181561c39b2c2e6048f730a7d4f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 05:05:38 +0000

    Add package what2-grapheme/0.5.1/what2_grapheme-0.5.1-py3-none-any.whl

---
+++ packages/what2-grapheme/what2_grapheme-0.5.1-py3-none-any.whl/what2_grapheme/api.py
@@ -0,0 +1,11 @@
+
+from what2_grapheme.fast_re.api import contains as contains
+from what2_grapheme.fast_re.api import grapheme_sizes as grapheme_sizes
+from what2_grapheme.fast_re.api import graphemes as graphemes
+from what2_grapheme.fast_re.api import is_safe as is_safe
+from what2_grapheme.fast_re.api import iter_grapheme_sizes as iter_grapheme_sizes
+from what2_grapheme.fast_re.api import iter_graphemes as iter_graphemes
+from what2_grapheme.fast_re.api import length as length
+from what2_grapheme.fast_re.api import strslice as strslice
+from what2_grapheme.grapheme_data.load import utf_license as utf_license
+from what2_grapheme.grapheme_data.load import utf_version as utf_version
