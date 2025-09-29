bc56b3a07167db38c5083be015a2cf07abfec41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:20 +0000

    Add package mcpcat/0.1.7rc2/mcpcat-0.1.7rc2.tar.gz

---
+++ packages/mcpcat/mcpcat-0.1.7rc2.tar.gz/mcpcat-0.1.7rc2/src/mcpcat/modules/constants.py
@@ -0,0 +1,5 @@
+INACTIVITY_TIMEOUT_IN_MINUTES = 30
+LOG_PATH = "mcpcat.log"  # Default log file path
+SESSION_ID_PREFIX = "ses"
+EVENT_ID_PREFIX = "evt"
+MCPCAT_API_URL = "https://api.mcpcat.io"  # Default API URL for MCPCat events
