42f28802993ab0055a4c7721f7a46802e5cc7f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:41 +0000

    Add package Aminoapps/0.1.0/aminoapps-0.1.0.tar.gz

---
+++ packages/Aminoapps/aminoapps-0.1.0.tar.gz/aminoapps-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Aminoapps",
+    version="0.1.0",
+    packages=find_packages(),
+    description="The best",
+    author="ZOOM",
+    license="MIT",
+)
