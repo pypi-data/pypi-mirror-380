41eecd49153906fa9565674e4daa130c10bfd917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:04:33 +0000

    Add package badauth/0.1.2/badauth-0.1.2-py3-none-any.whl

---
+++ packages/badauth/badauth-0.1.2-py3-none-any.whl/badauth/protocols/kerberos/__init__.py
@@ -0,0 +1,4 @@
+import logging
+
+logger = logging.getLogger('badauth.kerberos')
+logger.propagate = True
---
+++ packages/badauth/badauth-0.1.2-py3-none-any.whl/badauth/protocols/ntlm/__init__.py
@@ -0,0 +1,4 @@
+import logging
+
+logger = logging.getLogger('badauth.ntlm')
+logger.propagate = True
