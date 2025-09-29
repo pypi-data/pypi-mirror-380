e5ac54666a2b45f7f146f44eb0a940df766c1c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 17:04:49 +0000

    Add package virtual-camera/1.0.3/virtual_camera-1.0.3.tar.gz

---
+++ packages/virtual-camera/virtual_camera-1.0.3.tar.gz/virtual_camera-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='virtual_camera',
+    version='1.0.3',
+    description='Virtual Camera',
+    author='AlphaLFC',
+    author_email='alphali@motovis.com',
+    url='https://github.com/AlphaLFC/virtual_camera',
+    long_description=open('README_pypi.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        '': ['data/*']
+    },
+    install_requires=[
+        'numpy',
+        'scipy'
+    ]
+)
