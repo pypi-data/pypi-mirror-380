427f7cd30d5784846b192cf660be92082f34b593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 14:05:21 +0000

    Add package lt-tensor/0.0.2a22/lt_tensor-0.0.2a22.tar.gz

---
+++ packages/lt-tensor/lt_tensor-0.0.2a22.tar.gz/lt_tensor-0.0.2a22/lt_tensor/model_zoo/activations/trop_set.py
@@ -0,0 +1,129 @@
+import math
+import torch
+
+from torch import nn, Tensor
+from .basic_utils import _ActivationBase
+from typing import Union, Sequence, Callable
+from torch.nn import functional as F
+
+
+class Hermite(_ActivationBase):
+    def __init__(self, degree: int, requires_grad: bool = True):
+        super().__init__()
+        degree = int(degree)
+        assert (
+            degree >= 1
+        ), f"degree must be equal or larger than 1, received instead: {degree}"
+        self.degree = degree
+        # Learnable coefficients for each polynomial degree
+        self.coeffs = nn.Parameter(torch.ones(degree + 1), requires_grad=requires_grad)
+        self.step_ld = 1.0 / (math.sqrt(degree) * math.pi)
+
+        self.reset_parameters()
+
+    def forward(self, x):
+        H = [torch.ones_like(x), 2 * x]  # H0 and H1
+
+        for n in range(2, self.degree + 1):
+            Hn = 2 * x * H[-1] - 2 * (n - 1) * H[-2]
+            H.append(Hn * self.step_ld)
+        H_stack = torch.stack(H[: self.degree + 1], dim=-1)
+        return (H_stack * self.coeffs).sum(-1)
+
+
+class TropicalPoly(_ActivationBase):
+    def __init__(
+        self,
+        degree: int,
+        dim: Union[Sequence[int], int] = -1,
+        keepdim: bool = False,
+        requires_grad: bool = True,
+    ):
+        super().__init__()
+        self.degree = degree
+        self.a = nn.Parameter(torch.ones(degree + 1), requires_grad=requires_grad)
+        self.keep_dim = keepdim
+        self.dim = dim
+        self.reset_parameters()
+
+    def forward(self, x: Tensor):
+        k = torch.arange(self.degree + 1, device=x.device, dtype=x.dtype)
+        xk = x.unsqueeze(-1) * k  # broadcast over degree
+        return torch.amax(self.a * xk, dim=self.dim)
+
+
+class Fourier(_ActivationBase):
+    def __init__(self, max_freq: int, requires_grad: bool = True):
+        super().__init__()
+        self.max_freq = max_freq
+        self.a_sin = nn.Parameter(torch.ones(max_freq), requires_grad=requires_grad)
+        self.a_cos = nn.Parameter(torch.ones(max_freq), requires_grad=requires_grad)
+        self.reset_parameters()
+
+    def forward(self, x):
+        k = torch.arange(1, self.max_freq + 1, device=x.device, dtype=x.dtype)
+        # Expand x: (..., 1) to broadcast with k: (max_freq,)
+        xk = x.unsqueeze(-1) * k
+        sin_terms = torch.sin(xk) * self.a_sin
+        cos_terms = torch.cos(xk) * self.a_cos
+        # Sum over frequencies
+        return sin_terms.sum(-1) + cos_terms.sum(-1)
+
+
+class HermiteSum(Hermite):
+    def forward(self, x: Tensor):
+        return x + super().forward(x)
+
+
+class HermiteMul(Hermite):
+    def __init__(
+        self,
+        degree: int,
+        requires_grad: bool = True,
+        gated_activ: Callable[[Tensor], Tensor] = F.tanh,
+    ):
+        super().__init__(degree, requires_grad)
+        self.fc = gated_activ
+
+    def forward(self, x: Tensor):
+        return x * (1.0 + self.fc(super().forward(x)))
+
+
+class TropicalPolySum(TropicalPoly):
+    def forward(self, x: Tensor):
+        return x + super().forward(x)
+
+
+class TropicalPolyMul(TropicalPoly):
+    def __init__(
+        self,
+        degree: int,
+        dim: Union[Sequence[int], int] = -1,
+        keepdim: bool = False,
+        requires_grad: bool = True,
+        gated_activ: Callable[[Tensor], Tensor] = F.tanh,
+    ):
+        super().__init__(degree, dim, keepdim, requires_grad)
+        self.fc = gated_activ
+
+    def forward(self, x: Tensor):
+        return x * (1 + self.fc(super().forward(x)))
+
+
+class FourierSum(Fourier):
+    def forward(self, x: Tensor):
+        return x + super().forward(x)
+
+
+class FourierMul(Fourier):
+    def __init__(
+        self,
+        max_freq: int,
+        requires_grad: bool = True,
+        gated_activ: Callable[[Tensor], Tensor] = F.tanh,
+    ):
+        super().__init__(max_freq, requires_grad)
+        self.fc = gated_activ
+
+    def forward(self, x: Tensor):
+        return x * (1.0 + self.fc(super().forward(x)))
