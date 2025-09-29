96dda2679a4d7149038e4077284854c3e5dfc50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:36 +0000

    Add package grain-nightly/0.2.13.dev20250929/grain_nightly-0.2.13.dev20250929-cp312-cp312-win_amd64.whl

---
+++ packages/grain-nightly/grain_nightly-0.2.13.dev20250929-cp312-cp312-win_amd64.whl/grain/_src/python/dataset/transformations/repeat_test.py
@@ -0,0 +1,106 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for repeat transformation."""
+import sys
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from grain._src.python.dataset import dataset
+from grain._src.python.dataset.transformations import repeat
+from typing_extensions import override
+
+
+class EmptyMapDataset(dataset.MapDataset[int]):
+
+  def __init__(self):
+    super().__init__(parents=[])
+
+  @override
+  def __len__(self) -> int:
+    return 0
+
+  @override
+  def __getitem__(self, index):
+    raise IndexError("Index out of range")
+
+
+class RepeatMapDatasetTest(parameterized.TestCase):
+
+  def test_finite_num_epochs_changes_length(self):
+    ds = dataset.MapDataset.range(6)
+    self.assertLen(ds, 6)
+    ds = repeat.RepeatMapDataset(ds, num_epochs=3)
+    self.assertLen(ds, 18)
+
+  def test_finite_num_epochs_produces_expected_elements_when_iterated(self):
+    ds = dataset.MapDataset.range(4)
+    ds = repeat.RepeatMapDataset(ds, num_epochs=3)
+    self.assertSequenceEqual(list(ds), [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
+
+  def test_infinite_epochs_sets_length_to_maxsize(self):
+    ds = dataset.MapDataset.range(6)
+    ds = repeat.RepeatMapDataset(ds, num_epochs=None)
+    self.assertLen(ds, sys.maxsize)
+
+  def test_repeat_after_setting_infinite_epochs_raises_value_error(self):
+    ds = dataset.MapDataset.range(6)
+    ds = repeat.RepeatMapDataset(ds, num_epochs=None)
+    with self.assertRaises(ValueError):
+      repeat.RepeatMapDataset(ds, num_epochs=2)
+
+  def test_setting_zero_epochs_raises_value_error(self):
+    ds = dataset.MapDataset.range(6)
+    with self.assertRaises(ValueError):
+      repeat.RepeatMapDataset(ds, num_epochs=0)
+
+  def test_setting_negative_epochs_raises_value_error(self):
+    ds = dataset.MapDataset.range(6)
+    with self.assertRaises(ValueError):
+      repeat.RepeatMapDataset(ds, num_epochs=-1)
+
+  def test_infinite_epochs_of_empty_dataset_keeps_length_zero(self):
+    ds = EmptyMapDataset()
+    ds = repeat.RepeatMapDataset(ds, num_epochs=None)
+    self.assertEmpty(ds)
+
+  @parameterized.product(
+      num_epochs=[2, None],
+      reseed_each_epoch=[True, False],
+      explicit_seed=[True, False],
+  )
+  def test_random_transforms_across_epochs(
+      self, num_epochs: int | None, reseed_each_epoch: bool, explicit_seed: bool
+  ):
+    num_examples = 1000
+    ds = dataset.MapDataset.range(num_examples)
+    if explicit_seed:
+      ds = ds.shuffle(seed=42).random_map(
+          lambda x, rng: x + rng.integers(10), seed=43
+      )
+    else:
+      ds = ds.seed(42).shuffle().random_map(lambda x, rng: x + rng.integers(10))
+    ds = repeat.RepeatMapDataset(
+        ds, num_epochs=num_epochs, reseed_each_epoch=reseed_each_epoch
+    )
+    first_epoch = list(ds[:num_examples])
+    second_epoch = list(ds[num_examples : num_examples * 2])
+    if reseed_each_epoch:
+      self.assertNotEqual(first_epoch, second_epoch)
+      self.assertNotEqual(set(first_epoch), set(second_epoch))
+    else:
+      self.assertEqual(first_epoch, second_epoch)
+
+
+if __name__ == "__main__":
+  absltest.main()
