16a2f524353564c384e1af075db9ed6d4458dae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 17:04:48 +0000

    Add package CodeEntropy/1.0.3/codeentropy-1.0.3-py3-none-any.whl

---
+++ packages/CodeEntropy/codeentropy-1.0.3-py3-none-any.whl/CodeEntropy/__init__.py
@@ -0,0 +1,9 @@
+"""
+CodeEntropy
+
+CodeEntropy tool with POSEIDON code integrated to form a complete and generally
+applicable set of tools for computing entropy of macromolecular systems from the
+forces sampled in a MD simulation.
+"""
+
+__version__ = "1.0.3"
