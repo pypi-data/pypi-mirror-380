af2e727794ef2f388988a126f7865a4324dd9f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:04:51 +0000

    Add package models-lib/0.1.0/models_lib-0.1.0-py3-none-any.whl

---
+++ packages/models-lib/models_lib-0.1.0-py3-none-any.whl/models/__init__.py
@@ -0,0 +1,4 @@
+from .models import db
+from .models import ClUser, Person, Customer, PersonDtl, PersonBankAccount
+
+__all__ = ["db", "ClUser", "Person", "Customer", "PersonDtl", "PersonBankAccount"]
