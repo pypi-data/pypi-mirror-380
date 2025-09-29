35b3ac720493377a3588baf02bb850bfd4e85c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 03:20:05 +0000

    Add package spreadsheet-use/0.1.1a7/spreadsheet_use-0.1.1a7-py3-none-any.whl

---
+++ packages/spreadsheet-use/spreadsheet_use-0.1.1a7-py3-none-any.whl/spreadsheet_use/__init__.py
@@ -0,0 +1,7 @@
+# spreadsheet-use: Alias package for univer-use
+# This package provides the same functionality as univer-use
+from univer_use.spreadsheet_act import spreadsheet_agent
+from univer_use.graph import build_graph
+from univer_use import *
+
+__all__ = ["spreadsheet_agent", "build_graph"]
