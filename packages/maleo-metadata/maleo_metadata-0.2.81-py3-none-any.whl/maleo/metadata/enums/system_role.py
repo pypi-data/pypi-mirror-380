e2f831285af6cd840191550fa3af97d5de8742a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:04:44 +0000

    Add package maleo-metadata/0.2.81/maleo_metadata-0.2.81-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.2.81-py3-none-any.whl/maleo/metadata/enums/system_role.py
@@ -0,0 +1,27 @@
+from enum import StrEnum
+from maleo.enums.system_role import Key as BaseKey
+from maleo.types.string import ListOfStrings
+
+
+class Granularity(StrEnum):
+    BASIC = "basic"
+    STANDARD = "standard"
+    FULL = "full"
+
+    @classmethod
+    def choices(cls) -> ListOfStrings:
+        return [e.value for e in cls]
+
+
+class IdentifierType(StrEnum):
+    ID = "id"
+    UUID = "uuid"
+    KEY = "key"
+    NAME = "name"
+
+    @classmethod
+    def choices(cls) -> ListOfStrings:
+        return [e.value for e in cls]
+
+
+Key = BaseKey
