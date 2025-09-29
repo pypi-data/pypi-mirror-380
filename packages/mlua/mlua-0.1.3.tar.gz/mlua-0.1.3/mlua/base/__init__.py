a9bb20c9b5d81ce69c54361c2081a95c4538c937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:15 +0000

    Add package mlua/0.1.3/mlua-0.1.3.tar.gz

---
+++ packages/mlua/mlua-0.1.3.tar.gz/mlua-0.1.3/mlua/base/__init__.py
@@ -0,0 +1,1 @@
+from .mluaroot import *
---
+++ packages/mlua/mlua-0.1.3.tar.gz/mlua-0.1.3/mlua/manager/mluaenv.py
@@ -0,0 +1,34 @@
+from os import mkdir
+from pathlib import Path
+from json import loads, dumps
+from .mluacore import MLuaModule
+from ..base import MLuaBase
+
+__all__ = ["MLuaModulesManager"]
+
+class MLuaModulesManager(MLuaBase):
+
+    @staticmethod
+    def save(*modules: MLuaModule, directory="./modules") -> None:
+        try:
+            mkdir(directory)
+        except FileExistsError:
+            pass
+
+        configuration = {}
+        for module in modules:
+            configuration[module.name()] = module.path()
+
+        Path(directory, "index.json").write_text(dumps(configuration))
+
+    @staticmethod
+    def load(directory="./modules") -> list[MLuaModule]:
+        configuration = loads(Path(directory, "index.json").read_text())
+        temp_modules = []
+        for module_name, module_path in configuration.items():
+            temp_modules.append(MLuaModule(module_path))
+
+        return temp_modules
+
+    def __str__(self):
+        return f"{type(self).__name__}()"
