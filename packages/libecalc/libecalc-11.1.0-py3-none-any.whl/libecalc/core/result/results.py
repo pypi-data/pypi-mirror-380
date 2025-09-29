3de47ae099f451f5dc7284b3817a32a52f900bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:04:46 +0000

    Add package libecalc/11.1.0/libecalc-11.1.0-py3-none-any.whl

---
+++ packages/libecalc/libecalc-11.1.0-py3-none-any.whl/libecalc/core/result/results.py
@@ -0,0 +1,261 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Literal, Self
+
+from libecalc.common.component_type import ComponentType
+from libecalc.common.time_utils import Periods
+from libecalc.common.utils.rates import (
+    TimeSeriesBoolean,
+    TimeSeriesFloat,
+    TimeSeriesInt,
+    TimeSeriesStreamDayRate,
+)
+from libecalc.core.result.base import EcalcResultBaseModel
+from libecalc.domain.process.core.results import CompressorStreamCondition, TurbineResult
+from libecalc.domain.process.core.results.compressor import (
+    CompressorStageResult,
+    CompressorTrainCommonShaftFailureStatus,
+)
+
+
+class CommonResultBase(EcalcResultBaseModel):
+    """
+    Base component for all results: Model, Installation, GenSet, Consumer System, Consumer, etc.
+
+    We need both energy usage and power rate since we sometimes want both fuel and power usage.
+    """
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power)
+        self.periods = periods
+        self.is_valid = is_valid
+        self.energy_usage = energy_usage
+        self.power = power
+
+
+class GenericComponentResult(CommonResultBase):
+    typ: Literal["generc"] = "generc"
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        id: str,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power)
+        self.id = id
+
+
+class GeneratorSetResult(GenericComponentResult):
+    """The Generator set result component."""
+
+    typ: Literal["genset"] = "genset"
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        id: str,
+        power_capacity_margin: TimeSeriesStreamDayRate,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power, id=id)
+        self.power_capacity_margin = power_capacity_margin
+
+
+class ConsumerSystemResult(GenericComponentResult):
+    typ: Literal["system"] = "system"
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        id: str,
+        operational_settings_used: TimeSeriesInt,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power, id=id)
+        self.operational_settings_used = operational_settings_used
+
+
+class CompressorResult(GenericComponentResult):
+    typ: Literal["comp"] = "comp"
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        id: str,
+        recirculation_loss: TimeSeriesStreamDayRate,
+        rate_exceeds_maximum: TimeSeriesBoolean,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power, id=id)
+        self.recirculation_loss = recirculation_loss
+        self.rate_exceeds_maximum = rate_exceeds_maximum
+
+    def get_subset(self, indices: list[int]) -> Self:
+        return self.__class__(
+            id=self.id,
+            periods=Periods([self.periods.periods[index] for index in indices]),
+            energy_usage=self.energy_usage[indices],
+            is_valid=self.is_valid[indices],
+            power=self.power[indices] if self.power is not None else None,
+            recirculation_loss=self.recirculation_loss[indices],
+            rate_exceeds_maximum=self.rate_exceeds_maximum[indices],
+        )
+
+
+class PumpResult(GenericComponentResult):
+    typ: Literal["pmp"] = "pmp"
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        id: str,
+        inlet_liquid_rate_m3_per_day: TimeSeriesStreamDayRate,
+        inlet_pressure_bar: TimeSeriesFloat,
+        outlet_pressure_bar: TimeSeriesFloat,
+        operational_head: TimeSeriesFloat,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power, id=id)
+        self.inlet_liquid_rate_m3_per_day = inlet_liquid_rate_m3_per_day
+        self.inlet_pressure_bar = inlet_pressure_bar
+        self.outlet_pressure_bar = outlet_pressure_bar
+        self.operational_head = operational_head
+
+    def get_subset(self, indices: list[int]) -> Self:
+        return self.__class__(
+            id=self.id,
+            periods=Periods([self.periods.periods[index] for index in indices]),
+            energy_usage=self.energy_usage[indices],
+            is_valid=self.is_valid[indices],
+            power=self.power[indices] if self.power is not None else None,
+            inlet_liquid_rate_m3_per_day=self.inlet_liquid_rate_m3_per_day[indices],
+            inlet_pressure_bar=self.inlet_pressure_bar[indices],
+            outlet_pressure_bar=self.outlet_pressure_bar[indices],
+            operational_head=self.operational_head[indices],
+        )
+
+
+class ConsumerModelResultBase(ABC, CommonResultBase):
+    """The Consumer base result component."""
+
+    @property
+    @abstractmethod
+    def component_type(self): ...
+
+    name: str
+
+
+class PumpModelResult(ConsumerModelResultBase):
+    """The Pump result component."""
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        name: str,
+        inlet_liquid_rate_m3_per_day: list[float],
+        inlet_pressure_bar: list[float],
+        outlet_pressure_bar: list[float],
+        operational_head: list[float],
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power)
+        self.name = name
+        self.inlet_liquid_rate_m3_per_day = inlet_liquid_rate_m3_per_day
+        self.inlet_pressure_bar = inlet_pressure_bar
+        self.outlet_pressure_bar = outlet_pressure_bar
+        self.operational_head = operational_head
+
+    @property
+    def component_type(self):
+        return ComponentType.PUMP
+
+
+class CompressorModelResult(ConsumerModelResultBase):
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        name: str,
+        rate_sm3_day: list[float] | list[list[float]],
+        max_standard_rate: list[float] | list[list[float]] | None,
+        stage_results: list[CompressorStageResult],
+        failure_status: list[CompressorTrainCommonShaftFailureStatus | None],
+        turbine_result: TurbineResult | None,
+        inlet_stream_condition: CompressorStreamCondition,
+        outlet_stream_condition: CompressorStreamCondition,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power)
+        self.name = name
+        self.rate_sm3_day = rate_sm3_day
+        self.max_standard_rate = max_standard_rate
+        self.stage_results = stage_results
+        self.failure_status = failure_status
+        self.turbine_result = turbine_result
+        self.inlet_stream_condition = inlet_stream_condition
+        self.outlet_stream_condition = outlet_stream_condition
+
+    @property
+    def component_type(self):
+        return ComponentType.COMPRESSOR
+
+
+class GenericModelResult(ConsumerModelResultBase):
+    """Generic consumer result component."""
+
+    def __init__(
+        self,
+        periods: Periods,
+        is_valid: TimeSeriesBoolean,
+        energy_usage: TimeSeriesStreamDayRate,
+        power: TimeSeriesStreamDayRate | None,
+        name: str,
+    ):
+        super().__init__(periods=periods, is_valid=is_valid, energy_usage=energy_usage, power=power)
+        self.name = name
+
+    @property
+    def component_type(self):
+        return ComponentType.GENERIC
+
+
+# Consumer model result is referred to as ENERGY_USAGE_MODEL in the input YAML
+ConsumerModelResult = CompressorModelResult | PumpModelResult | GenericModelResult
+
+ComponentResult = GeneratorSetResult | ConsumerSystemResult | CompressorResult | PumpResult | GenericComponentResult
+
+
+class EcalcModelResult(EcalcResultBaseModel):
+    """Result object holding one component for each part of the eCalc model run."""
+
+    def __init__(
+        self,
+        component_result: ComponentResult,
+        sub_components: list[ComponentResult],
+        models: list[ConsumerModelResult],
+    ):
+        super().__init__(component_result=component_result, sub_components=sub_components, models=models)
+        self.component_result = component_result
+        self.sub_components = sub_components
+        self.models = models
---
+++ packages/libecalc/libecalc-11.1.0-py3-none-any.whl/libecalc/domain/infrastructure/energy_components/legacy_consumer/system/results.py
@@ -0,0 +1,175 @@
+from __future__ import annotations
+
+from enum import Enum
+
+import numpy as np
+from numpy.typing import NDArray
+
+from libecalc.common.logger import logger
+from libecalc.common.time_utils import Periods
+from libecalc.core.result.results import ConsumerModelResult
+from libecalc.domain.component_validation_error import ComponentValidationException
+from libecalc.domain.infrastructure.energy_components.legacy_consumer.consumer_function.results import (
+    ConsumerFunctionResultBase,
+)
+from libecalc.domain.infrastructure.energy_components.legacy_consumer.consumer_function.types import (
+    ConsumerFunctionType,
+)
+from libecalc.domain.infrastructure.energy_components.legacy_consumer.system.operational_setting import (
+    ConsumerSystemOperationalSetting,
+)
+from libecalc.domain.process.core.results import CompressorTrainResult, EnergyFunctionResult, PumpModelResult
+
+
+class ConsumerSystemComponentResult:
+    def __init__(self, name: str, consumer_model_result: PumpModelResult | CompressorTrainResult):
+        self.name = name
+        self.consumer_model_result = consumer_model_result
+
+    @property
+    def energy_usage(self) -> list[float]:
+        return self.consumer_model_result.energy_usage
+
+    @property
+    def power(self) -> NDArray:
+        if self.consumer_model_result.power is not None:
+            return np.asarray(self.consumer_model_result.power)
+        else:
+            return np.zeros_like(self.consumer_model_result.energy_usage)
+
+    @property
+    def rate(self) -> list[float]:
+        return self.consumer_model_result.rate
+
+
+class PumpResult(ConsumerSystemComponentResult):
+    def __init__(self, name: str, consumer_model_result: PumpModelResult):
+        super().__init__(name, consumer_model_result)
+
+    @property
+    def fluid_density(self):
+        return self.consumer_model_result.fluid_density
+
+
+class CompressorResult(ConsumerSystemComponentResult):
+    def __init__(self, name: str, consumer_model_result: ConsumerModelResult | CompressorTrainResult):
+        super().__init__(name, consumer_model_result)  # type: ignore[arg-type]
+
+
+class ConsumerSystemOperationalSettingResult:
+    def __init__(self, consumer_results: list[ConsumerSystemComponentResult]):
+        self.consumer_results = consumer_results
+
+    @property
+    def total_energy_usage(self) -> NDArray:
+        total_energy_usage = np.sum(
+            [np.asarray(result.energy_usage) for result in self.consumer_results],
+            axis=0,
+        )
+        return np.array(total_energy_usage)
+
+    @property
+    def total_power(self) -> NDArray:
+        total_power = np.sum(
+            [np.asarray(result.power) for result in self.consumer_results],
+            axis=0,
+        )
+        return np.array(total_power)
+
+    @property
+    def indices_outside_capacity(self) -> NDArray:
+        invalid_indices = np.full_like(self.total_energy_usage, fill_value=0)
+
+        for result in self.consumer_results:
+            energy_function_result = result.consumer_model_result
+            if isinstance(energy_function_result, CompressorTrainResult | PumpModelResult):
+                invalid_indices = np.add(
+                    invalid_indices,
+                    np.array([0 if x else 1 for x in energy_function_result.is_valid]),
+                )
+            else:
+                raise NotImplementedError(
+                    f"Consumer system result assembly has not been implemented for {type(energy_function_result)}."
+                    f" Please contact eCalc support."
+                )
+        return np.argwhere(np.where(np.logical_or(invalid_indices > 0, np.isnan(self.total_energy_usage)), 1, 0))[:, 0]
+
+    @property
+    def indices_within_capacity(self) -> NDArray[np.float64]:
+        return np.setdiff1d(np.arange(self.total_energy_usage.shape[0]), self.indices_outside_capacity)
+
+
+class ConsumerSystemConsumerFunctionResult(ConsumerFunctionResultBase):
+    """Class for holding results for a consumer system evaluation.
+
+    operational_setting_used: an array - one for each time step evaluated - for which of
+    the operational settings where used (in obtaining the final energy usage result)
+    operational_settings: a list for each operational setting. Each element contains data
+    of the operational setting, i.e. what rates/pressures/fluid densities for which consumer in
+    the system
+    operational_settings_results: a list for each operational setting. Each element contain
+    data for the energy usage of each consumer in the system in this operational setting.
+    """
+
+    def __init__(
+        self,
+        operational_setting_used: NDArray,
+        operational_settings: list[list[ConsumerSystemOperationalSetting]],
+        operational_settings_results: list[list[ConsumerSystemOperationalSettingResult]],
+        consumer_results: list[list[ConsumerSystemComponentResult]],
+        cross_over_used: NDArray | None = None,
+        # 0 or 1 whether cross over is used for this result (1=True, 0=False)
+        periods: Periods = None,
+        is_valid: NDArray = None,
+        energy_usage: NDArray = None,
+        energy_usage_before_power_loss_factor: NDArray | None = None,
+        power_loss_factor: NDArray | None = None,
+        energy_function_result: EnergyFunctionResult | list[EnergyFunctionResult] | None = None,
+        power: NDArray | None = None,
+    ):
+        super().__init__(
+            typ=ConsumerFunctionType.SYSTEM,
+            periods=periods,
+            is_valid=is_valid,
+            energy_usage=energy_usage,
+            energy_usage_before_power_loss_factor=energy_usage_before_power_loss_factor,
+            power_loss_factor=power_loss_factor,
+            energy_function_result=energy_function_result,
+            power=power,
+        )
+        self.operational_setting_used = operational_setting_used
+        self.operational_settings = operational_settings
+        self.operational_settings_results = operational_settings_results
+        self.consumer_results = consumer_results
+        self.cross_over_used = cross_over_used
+
+    def extend(self, other) -> ConsumerSystemConsumerFunctionResult:
+        if not isinstance(self, type(other)):
+            msg = "Mixing CONSUMER_SYSTEM with non-CONSUMER_SYSTEM is no longer supported."
+            logger.warning(msg)
+            raise ComponentValidationException(
+                message=msg,
+            )
+
+        for attribute, values in self.__dict__.items():
+            other_values = other.__getattribute__(attribute)
+
+            if values is None or other_values is None or isinstance(values, Enum):
+                continue
+            elif isinstance(values, np.ndarray):
+                self.__setattr__(attribute, np.append(values, other_values))
+            elif isinstance(values, list):
+                if isinstance(other_values, list):
+                    values.extend(other_values)
+                else:
+                    values.append(other_values)
+            elif isinstance(values, Periods):
+                self.__setattr__(attribute, values + other_values)
+            else:
+                msg = (
+                    f"{self.__repr_name__()} attribute {attribute} does not have an append method."
+                    f" You should not land here. Please contact the eCalc Support."
+                )
+                logger.warning(msg)
+                raise NotImplementedError(msg)
+        return self
