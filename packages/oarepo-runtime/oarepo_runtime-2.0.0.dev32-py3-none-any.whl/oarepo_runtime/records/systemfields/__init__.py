03ec460f558517a5f7d784762db0d497b34403d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:11 +0000

    Add package oarepo-runtime/2.0.0.dev32/oarepo_runtime-2.0.0.dev32-py3-none-any.whl

---
+++ packages/oarepo-runtime/oarepo_runtime-2.0.0.dev32-py3-none-any.whl/oarepo_runtime/records/systemfields/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-runtime (see http://github.com/oarepo/oarepo-runtime).
+#
+# oarepo-runtime is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Records system fields."""
+
+from __future__ import annotations
+
+from .mapping import MappingSystemFieldMixin
+from .publication_status import PublicationStatusSystemField
+
+__all__ = ("MappingSystemFieldMixin", "PublicationStatusSystemField")
