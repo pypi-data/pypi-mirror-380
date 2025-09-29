ac5e649011338e0239d1f0d163ddddb9be34874b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 16:04:34 +0000

    Add package optima35/1.1.4/optima35-1.1.4-py3-none-any.whl

---
+++ packages/optima35/optima35-1.1.4-py3-none-any.whl/optima35/__main__.py
@@ -0,0 +1,9 @@
+from . import __version__
+# From ChatGPT
+def main():
+    print("(C) 2024-2025 Mr. Finchum aka CodeByMrFinchum")
+    print(f"optima35 (v{__version__}) is a core library and not intended to be run directly.")
+    print("Please use OptimaLab35 for a UI, run pip install OptimaLab35 and start with OptimaLab35.")
+
+if __name__ == "__main__":
+    main()
