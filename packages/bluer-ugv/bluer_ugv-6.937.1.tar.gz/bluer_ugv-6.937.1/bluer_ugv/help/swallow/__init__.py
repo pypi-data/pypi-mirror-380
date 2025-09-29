9ce6ae482c684dc59b289d0622e679bf4fb94890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 05:05:25 +0000

    Add package bluer-ugv/6.937.1/bluer_ugv-6.937.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.937.1.tar.gz/bluer_ugv-6.937.1/bluer_ugv/help/swallow/__init__.py
@@ -0,0 +1,11 @@
+from bluer_ugv.help.swallow.dataset import help_functions as help_dataset
+from bluer_ugv.help.swallow.debug import help_debug
+from bluer_ugv.help.swallow.env import help_functions as help_env
+from bluer_ugv.help.swallow.select_target import help_select_target
+
+help_functions = {
+    "dataset": help_dataset,
+    "debug": help_debug,
+    "env": help_env,
+    "select_target": help_select_target,
+}
