51beb981de6679a9bfdba476452c00208ff6983c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:21 +0000

    Add package leverage/2.1.2rc2/leverage-2.1.2rc2-py3-none-any.whl

---
+++ packages/leverage/leverage-2.1.2rc2-py3-none-any.whl/leverage/leverage.py
@@ -0,0 +1,68 @@
+"""
+    Binbash Leverage Command-line tool.
+"""
+
+import rich
+from packaging.version import Version
+
+import click
+
+from leverage import __version__, conf, MINIMUM_VERSIONS
+from leverage._internals import pass_state
+from leverage.modules.aws import aws
+from leverage.modules.credentials import credentials
+from leverage.modules import run, project, tofu, terraform, tfautomv, kubectl, shell
+from leverage.path import NotARepositoryError
+
+
+@click.group(invoke_without_command=True)
+@click.option("-v", "--verbose", is_flag=True, help="Increase output verbosity.")
+@click.version_option(version=__version__)
+@pass_state
+@click.pass_context
+def leverage(context, state, verbose):
+    """Leverage Reference Architecture projects command-line tool."""
+    # --verbose | -v
+    state.verbosity = verbose
+    if context.invoked_subcommand is None:
+        # leverage called with no subcommand
+        click.echo(context.get_help())
+
+    # if there is a version restriction set, make sure we satisfy it
+    try:
+        config = conf.load()
+    except NotARepositoryError:
+        # restrictions are only verified within a leverage project
+        return
+
+    # check if the current versions are lower than the minimum required
+    if not (image_tag := config.get("TF_IMAGE_TAG", config.get("TERRAFORM_IMAGE_TAG"))):
+        # at some points of the project (the init), the config file is not created yet
+        return
+
+    # validate both TOOLBOX and TF versions
+    image_versions = image_tag.split("-")
+    if "tofu" not in image_versions:
+        versions = zip(MINIMUM_VERSIONS, image_versions)
+    else:
+        versions = {"TOOLBOX": image_versions[-1]}.items()
+
+    for key, current in versions:
+        if Version(current) < Version(MINIMUM_VERSIONS[key]):
+            rich.print(
+                f"[red]WARNING[/red]\tYour current {key} version ({current}) is lower than the required minimum ({MINIMUM_VERSIONS[key]})."
+            )
+
+
+# Add modules to leverage
+leverage.add_command(run)
+leverage.add_command(project)
+leverage.add_command(tofu)
+leverage.add_command(tofu, name="tf")
+leverage.add_command(terraform)
+leverage.add_command(credentials)
+leverage.add_command(aws)
+leverage.add_command(tfautomv)
+leverage.add_command(kubectl)
+leverage.add_command(kubectl, name="kc")
+leverage.add_command(shell)
