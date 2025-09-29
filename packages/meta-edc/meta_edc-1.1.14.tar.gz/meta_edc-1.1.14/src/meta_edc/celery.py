a0b87e2c628143a5c5bfc48e8ebbab6d17878798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 15:05:37 +0000

    Add package meta-edc/1.1.14/meta_edc-1.1.14.tar.gz

---
+++ packages/meta-edc/meta_edc-1.1.14.tar.gz/meta_edc-1.1.14/src/meta_edc/celery.py
@@ -0,0 +1,16 @@
+import os
+
+from celery import Celery
+
+# load settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meta_edc.settings.debug")
+
+# config celery
+app = Celery("meta_edc")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
