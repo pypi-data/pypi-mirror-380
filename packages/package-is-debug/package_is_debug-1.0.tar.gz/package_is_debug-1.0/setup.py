ededbe8faeff6cc1a33ea3629ab026e3c4bd841a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:15 +0000

    Add package package-is-debug/1.0/package_is_debug-1.0.tar.gz

---
+++ packages/package-is-debug/package_is_debug-1.0.tar.gz/package_is_debug-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package_is_debug',
+    version='1.0',
+    author='Маг Ильяс DOMA (MagIlyas_DOMA)',
+    license='MIT License',
+    url='https://github.com/MagIlyas-DOMA/PackageIsDebug',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
