5abac6b89d622f8249d15d836fff3c3e39cdc4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 04:04:52 +0000

    Add package installer-ml/0.1.1/installer_ml-0.1.1.tar.gz

---
+++ packages/installer-ml/installer_ml-0.1.1.tar.gz/installer_ml-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="installer-ml",
+    version="0.1.1",
+    description="Auto install and import ML/DL libraries in Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Nguyen Tan Dat",
+    author_email="tandatnguyen89@gmail.com",
+    url="https://github.com/tandatnguyen89",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
