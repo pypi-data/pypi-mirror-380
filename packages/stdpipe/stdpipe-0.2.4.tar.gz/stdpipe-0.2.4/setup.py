a17c35ee2d5f744ce1f103331961f1417c29dc36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-29 03:20:28 +0000

    Add package stdpipe/0.2.4/stdpipe-0.2.4.tar.gz

---
+++ packages/stdpipe/stdpipe-0.2.4.tar.gz/stdpipe-0.2.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__version__ = '0.2.4'
+
+requirements = [
+    'numpy',
+    'scipy',
+    'astropy>=4.1',
+    'matplotlib',
+    'astroquery>0.4.1',
+    'sep',
+    'astroscrappy',
+    'photutils',
+    'statsmodels',
+    'tqdm',
+    'regions',
+    'python-dateutil',
+    'requests',
+    'sip_tpv',
+    'pyfftw',
+]
+
+setup(
+    name='stdpipe',
+    version=__version__,
+    description='Simple Transient Detection Pipeline',
+    author='Sergey Karpov',
+    author_email='karpov.sv@gmail.com',
+    url='https://github.com/karpov-sv/stdpipe',
+    install_requires=requirements,
+    packages=['stdpipe'],
+    package_data={'stdpipe':['data/*']},
+    include_package_data=True,
+)
