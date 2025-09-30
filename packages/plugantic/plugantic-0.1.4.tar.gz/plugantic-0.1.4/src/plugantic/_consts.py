4256b0ba2531b32c0606f6b9ddb78c1c972ce2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:36 +0000

    Add package plugantic/0.1.4/plugantic-0.1.4.tar.gz

---
+++ packages/plugantic/plugantic-0.1.4.tar.gz/plugantic-0.1.4/src/plugantic/_consts.py
@@ -0,0 +1,2 @@
+# Single source of truth for information needed at runtime and compile-time (i.e. version)
+version = "0.1.4"
---
+++ packages/plugantic/plugantic-0.1.4.tar.gz/plugantic-0.1.4/test/test_classvar.py
@@ -0,0 +1,40 @@
+from typing_extensions import Literal, ClassVar
+from plugantic import PluginModel, PluginDowncastHandler, PluginFeature
+from pydantic import BaseModel, Field
+
+def test_classvar():
+    Feature = PluginFeature["feature1"]
+
+    def enable_feature(handler: PluginDowncastHandler):
+        handler.enable_feature(Feature)
+        handler.set_class_var("test", 2)
+
+    class Base(PluginModel):
+        type: Literal["feature"]
+        a: int|None = Field(default=3)
+
+        test: ClassVar[int] = 1
+
+        plugantic_config = {"auto_downcasts": (enable_feature,)}
+        
+    
+    class SomeConfig1(BaseModel):
+        config: Base
+
+    v = SomeConfig1.model_validate({"config": {
+        "type": "feature",
+        "a": 1
+    }})
+
+    assert v.config.test == 1
+
+
+    class SomeConfig2(BaseModel):
+        config: Base[Feature]
+
+    v = SomeConfig2.model_validate({"config": {
+        "type": "feature",
+        "a": 1
+    }})
+
+    assert v.config.test == 2
