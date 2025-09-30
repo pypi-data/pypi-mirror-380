eb339f4e0785fe4228fc29bf04f2c2ad2d025136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:05 +0000

    Add package snowflake-ml-python/1.15.0/snowflake_ml_python-1.15.0-py3-none-any.whl

---
+++ packages/snowflake-ml-python/snowflake_ml_python-1.15.0-py3-none-any.whl/snowflake/ml/model/_client/model/batch_inference_specs.py
@@ -0,0 +1,21 @@
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+
+class OutputSpec(BaseModel):
+    stage_location: str
+
+
+class JobSpec(BaseModel):
+    image_repo: Optional[str] = None
+    job_name: Optional[str] = None
+    num_workers: Optional[int] = None
+    function_name: Optional[str] = None
+    gpu: Optional[Union[str, int]] = None
+    force_rebuild: bool = False
+    max_batch_rows: int = 1024
+    warehouse: Optional[str] = None
+    cpu_requests: Optional[str] = None
+    memory_requests: Optional[str] = None
+    replicas: Optional[int] = None
