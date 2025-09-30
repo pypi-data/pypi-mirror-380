e586508da09597f4a57d8e6711d5fc3819f7fd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:39 +0000

    Add package muna/0.0.61/muna-0.0.61-py3-none-any.whl

---
+++ packages/muna/muna-0.0.61-py3-none-any.whl/muna/beta/openai/openai.py
@@ -0,0 +1,29 @@
+# 
+#   Muna
+#   Copyright © 2025 NatML Inc. All Rights Reserved.
+#
+
+from ...services import PredictorService, PredictionService
+from ..remote.remote import RemotePredictionService
+from .chat import ChatService
+from .embeddings import EmbeddingsService
+
+class OpenAIClient:
+    """
+    Experimental client mimicking the official OpenAI client.
+
+    Members:
+        chat (ChatService): Chat service.
+        embeddings (EmbeddingsService): Embeddings service.
+    """
+    chat: ChatService
+    embeddings: EmbeddingsService
+
+    def __init__(
+        self,
+        predictors: PredictorService,
+        predictions: PredictionService,
+        remote_predictions: RemotePredictionService
+    ):
+        self.chat = ChatService(predictions, remote_predictions)
+        self.embeddings = EmbeddingsService(predictors, predictions, remote_predictions)
