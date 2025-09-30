f96d9afecbee034b7254484cad2ea953d5c9014d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:06 +0000

    Add package regeorged-py-demo-lib/0.2.0/regeorged_py_demo_lib-0.2.0.tar.gz

---
+++ packages/regeorged-py-demo-lib/regeorged_py_demo_lib-0.2.0.tar.gz/regeorged_py_demo_lib-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="regeorged_py_demo_lib",
+    version="0.2.0",
+    description="A sample base case example",
+    author="Giorgi B",
+    author_email="giorgi.bazierashvili.1@iliauni.edu.ge",
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
