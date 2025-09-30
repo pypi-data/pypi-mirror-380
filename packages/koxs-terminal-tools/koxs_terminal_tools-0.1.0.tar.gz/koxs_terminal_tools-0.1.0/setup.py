443ca5b51afa41f783cc411f61d2ddea30f196a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:16:02 +0000

    Add package koxs-terminal-tools/0.1.0/koxs_terminal_tools-0.1.0.tar.gz

---
+++ packages/koxs-terminal-tools/koxs_terminal_tools-0.1.0.tar.gz/koxs_terminal_tools-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="koxs-terminal-tools",
+    version="0.1.0",
+    author="koxs",
+    author_email="koxs@example.com",
+    description="强大的终端工具集，包含文件编辑器和颜色输出功能",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],  # 如果有依赖包，在这里添加
+)
