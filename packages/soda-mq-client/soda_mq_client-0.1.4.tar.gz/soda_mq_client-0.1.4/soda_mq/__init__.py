a7945bab168daf8b80c8b4210a2dafe48404d887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 15:05:18 +0000

    Add package soda-mq-client/0.1.4/soda_mq_client-0.1.4.tar.gz

---
+++ packages/soda-mq-client/soda_mq_client-0.1.4.tar.gz/soda_mq_client-0.1.4/soda_mq/__init__.py
@@ -0,0 +1,6 @@
+from .soda_client import SodaClient
+from .soda_producer import SodaProducer
+from .soda_consumer import SodaConsumer
+from .client_factory import ClientFactory
+
+__all__ = ["SodaClient", "SodaProducer", "SodaConsumer", "ClientFactory"]
---
+++ packages/soda-mq-client/soda_mq_client-0.1.4.tar.gz/soda_mq_client-0.1.4/soda_mq/soda_client.py
@@ -0,0 +1,55 @@
+﻿import asyncio
+import logging
+from typing import AsyncIterable
+
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+from signalrcore.protocol.messagepack_protocol import MessagePackHubProtocol
+
+
+
+class SodaClient:
+
+    def __init__(self, server_url: str):
+        self._server_url = server_url
+        self._connected = False
+        self.hub_connection = HubConnectionBuilder() \
+            .with_url(hub_url=server_url) \
+            .with_hub_protocol(MessagePackHubProtocol()) \
+            .configure_logging(logging.DEBUG) \
+            .build()
+
+    @property
+    def is_connected(self) -> bool:
+        return self.hub_connection.transport and self.hub_connection.transport.is_running
+
+    def connect(self):
+        return self.hub_connection.start()
+
+    def disconnect(self):
+        return self.hub_connection.stop()
+
+    def send_message(self, topic: str, message: dict) -> None:
+        self.hub_connection.send("AppendOnTopic", [topic, message])
+
+    def subscribe(self, topic: str, on_message):
+        # self.hub_connection.on("ListenToTopic", on_message)
+        x = self.hub_connection.stream(
+            "ListenToTopic",
+            [topic],
+        )
+
+        def next_call_back(message):
+            # print("next_call_back", message)
+            on_message(message)
+
+        def error_call_back(error):
+            print("error_call_back", error)
+
+        def complete_call_back():
+            print("complete_call_back")
+
+        x.subscribe({
+            "next": next_call_back,
+            "error": error_call_back,
+            "complete": complete_call_back
+        })
