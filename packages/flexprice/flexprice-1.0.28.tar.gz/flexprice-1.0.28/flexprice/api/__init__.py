cbd0a810b1417fff6fe6fba8164215ebbf04d9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 16:07:07 +0000

    Add package flexprice/1.0.28/flexprice-1.0.28.tar.gz

---
+++ packages/flexprice/flexprice-1.0.28.tar.gz/flexprice-1.0.28/flexprice/api/__init__.py
@@ -0,0 +1,33 @@
+# flake8: noqa
+
+# import apis into api package
+from flexprice.api.addons_api import AddonsApi
+from flexprice.api.auth_api import AuthApi
+from flexprice.api.connections_api import ConnectionsApi
+from flexprice.api.cost_sheets_api import CostSheetsApi
+from flexprice.api.coupons_api import CouponsApi
+from flexprice.api.credit_grants_api import CreditGrantsApi
+from flexprice.api.credit_notes_api import CreditNotesApi
+from flexprice.api.customers_api import CustomersApi
+from flexprice.api.entitlements_api import EntitlementsApi
+from flexprice.api.entity_integration_mappings_api import EntityIntegrationMappingsApi
+from flexprice.api.environments_api import EnvironmentsApi
+from flexprice.api.events_api import EventsApi
+from flexprice.api.features_api import FeaturesApi
+from flexprice.api.integration_api import IntegrationApi
+from flexprice.api.integrations_api import IntegrationsApi
+from flexprice.api.invoices_api import InvoicesApi
+from flexprice.api.payments_api import PaymentsApi
+from flexprice.api.plans_api import PlansApi
+from flexprice.api.price_units_api import PriceUnitsApi
+from flexprice.api.prices_api import PricesApi
+from flexprice.api.subscriptions_api import SubscriptionsApi
+from flexprice.api.tasks_api import TasksApi
+from flexprice.api.tax_associations_api import TaxAssociationsApi
+from flexprice.api.tax_rates_api import TaxRatesApi
+from flexprice.api.tenants_api import TenantsApi
+from flexprice.api.users_api import UsersApi
+from flexprice.api.wallets_api import WalletsApi
+from flexprice.api.webhooks_api import WebhooksApi
+from flexprice.api.secrets_api import SecretsApi
+
