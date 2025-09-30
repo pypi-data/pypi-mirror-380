6de12e6032b63096e40d91b3c889a1c6d6b56eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:15:48 +0000

    Add package him-lychee/0.2.0/him_lychee-0.2.0.tar.gz

---
+++ packages/him-lychee/him_lychee-0.2.0.tar.gz/him_lychee-0.2.0/setup.py
@@ -0,0 +1,6 @@
+import setuptools
+
+# This setup.py file is only here to maintain compatibility with older
+# packaging tools. All project configuration is handled in setup.cfg.
+if __name__ == '__main__':
+    setuptools.setup()
