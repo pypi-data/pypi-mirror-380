2f65ceb0e17452a8570e50492e5402bd6258ddfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:06 +0000

    Add package sa-tenders/0.0.8/sa_tenders-0.0.8.tar.gz

---
+++ packages/sa-tenders/sa_tenders-0.0.8.tar.gz/sa_tenders-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+#processing the markdown file
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name= "sa_tenders",
+    version= "0.0.8",
+    author="Ntuthuko Hlela",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy", "flatdict", "openpyxl"],
+    description="This library enables developers to download and process data from the South African etenders platform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT License",
+
+)
