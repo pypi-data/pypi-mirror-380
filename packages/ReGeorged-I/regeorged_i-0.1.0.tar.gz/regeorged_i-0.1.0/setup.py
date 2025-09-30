5737c380afd1befa5ba3d12a5c8959afc7f77961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:14:03 +0000

    Add package ReGeorged-I/0.1.0/regeorged_i-0.1.0.tar.gz

---
+++ packages/ReGeorged-I/regeorged_i-0.1.0.tar.gz/regeorged_i-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ReGeorged_I",
+    version="0.1.0",
+    description="A sample base case example",
+    author="Giorgi B",
+    author_email="giorgi.bazierashvili.1@iliauni.edu.ge",
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
