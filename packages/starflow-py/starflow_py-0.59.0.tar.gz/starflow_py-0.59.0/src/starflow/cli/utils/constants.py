a0006a1b6cc2a0179e2fc7439bbee81d5f38fec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:14:09 +0000

    Add package starflow-py/0.59.0/starflow_py-0.59.0.tar.gz

---
+++ packages/starflow-py/starflow_py-0.59.0.tar.gz/starflow_py-0.59.0/src/starflow/cli/utils/constants.py
@@ -0,0 +1,9 @@
+COLOR_PALETTE = {
+    "success": "rgb(113,176,109)",
+    "warning": "rgb(176,169,109)",
+    "error": "rgb(176,109,109)"
+}
+
+STARFLOW_HELM_PATH = 'starflow/starflow'
+STARFLOW_HELM_VERSION = '0.1.6'
+STARFLOW_HELM_REPOSITORY = 'https://prochain-star-atlas.github.io/starflow/'
