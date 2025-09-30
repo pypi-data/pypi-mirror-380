9f47db27ddb20775dfe1d5278438599d205d6bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:06:26 +0000

    Add package b24api/0.7.4/b24api-0.7.4-py3-none-any.whl

---
+++ packages/b24api/b24api-0.7.4-py3-none-any.whl/b24api/query.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from urllib.parse import quote_plus
+
+from b24api.type import ApiTypes
+
+
+def build_query(parameters: dict[int | str, ApiTypes], path: str = "%s") -> str:
+    query: list[str] = []
+
+    for key, value in parameters.items():
+        if value is None:
+            continue
+
+        value_ = value
+
+        if isinstance(value_, list | tuple):
+            value_ = dict(enumerate(value))
+
+        if isinstance(value_, dict):
+            subquery = build_query(value_, path % key + "[%s]")
+        else:
+            key_ = quote_plus(path % key)
+
+            if isinstance(value_, datetime):
+                value_ = value.astimezone().isoformat()
+            value_ = quote_plus(str(value_))
+
+            subquery = f"{key_}={value_}"
+
+        if subquery:
+            query.append(subquery)
+
+    return "&".join(query)
