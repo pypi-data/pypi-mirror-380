fe03262d536e3e697a9a0ce8ca2467e22cdbe7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:17 +0000

    Add package Zino/2.1.1/zino-2.1.1.tar.gz

---
+++ packages/Zino/zino-2.1.1.tar.gz/zino-2.1.1/src/zino/tasks/task.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from zino.config.models import PollDevice
+from zino.snmp import get_snmp_session
+
+if TYPE_CHECKING:
+    from zino.state import ZinoState
+
+from zino.statemodels import DeviceState
+
+
+class Task(ABC):
+    def __init__(self, device: PollDevice, state: ZinoState):
+        self.device = device
+        self.state = state
+        self.snmp = get_snmp_session(device=device)
+
+    @abstractmethod
+    async def run(self):
+        """Runs job asynchronously"""
+
+    async def _get_uptime(self) -> int:
+        """Polls and returns the device sysuptime value"""
+        response = await self.snmp.get("SNMPv2-MIB", "sysUpTime", 0)
+        uptime = response.value
+        return uptime
+
+    @property
+    def device_state(self) -> DeviceState:
+        return self.state.devices.get(self.device.name)
