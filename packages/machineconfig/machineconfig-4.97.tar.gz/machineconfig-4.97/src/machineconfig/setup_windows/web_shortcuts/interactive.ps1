003e4f91478fe66acaa64108a3d916ef0795507d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:55 +0000

    Add package machineconfig/4.97/machineconfig-4.97.tar.gz

---
+++ packages/machineconfig/machineconfig-4.97.tar.gz/machineconfig-4.97/src/machineconfig/setup_windows/web_shortcuts/interactive.ps1
@@ -0,0 +1,12 @@
+Write-Host "
+🚀 ===========================================
+📦 Machine Configuration Installation Script
+============================================="
+
+Write-Host "ℹ️  If you have execution policy issues, run: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/thisismygitrepo/machineconfig/main/src/machineconfig/setup_windows/ve.ps1" -OutFile "ve.ps1"
+.\ve.ps1
+rm ve.ps1
+
+uv run --python 3.13 --with machineconfig devops interactive
