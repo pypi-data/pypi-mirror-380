b1f98ada460ac6f3d084c72b04f914eacba01a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:45 +0000

    Add package lmitf/0.1.13/lmitf-0.1.13.tar.gz

---
+++ packages/lmitf/lmitf-0.1.13.tar.gz/lmitf-0.1.13/lmitf/__init__.py
@@ -0,0 +1,14 @@
+"""Large Model Interface - A flexible interface for interacting with large language models and vision models."""
+from __future__ import annotations
+
+import os
+import os.path as op
+
+from .base_llm import BaseLLM
+from .base_lvm import BaseLVM
+from .datasets import manager as prompts
+from .templete_llm import TemplateLLM
+from .utils import print_conversation as print_turn
+
+__version__ = '0.1.13'
+__description__ = 'Large Model Interface - A flexible interface for interacting with large language models and vision models.'
