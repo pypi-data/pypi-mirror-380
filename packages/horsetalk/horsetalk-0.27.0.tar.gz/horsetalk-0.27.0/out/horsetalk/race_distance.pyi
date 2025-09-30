aab4c7d7302aac70a8d15a0a218c2ade7c7571dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:15:12 +0000

    Add package horsetalk/0.27.0/horsetalk-0.27.0.tar.gz

---
+++ packages/horsetalk/horsetalk-0.27.0.tar.gz/horsetalk-0.27.0/out/horsetalk/race_distance.pyi
@@ -0,0 +1,6 @@
+from _typeshed import Incomplete
+from horsetalk.quantity import HorsetalkQuantity as HorsetalkQuantity
+
+class RaceDistance(HorsetalkQuantity):
+    POSSIBLE_DECIMAL: str
+    REGEX: Incomplete
