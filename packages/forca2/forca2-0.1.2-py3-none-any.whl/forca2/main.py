8043f6a1ff7464ae8585a85c6749dcc02139bd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:13:40 +0000

    Add package forca2/0.1.2/forca2-0.1.2-py3-none-any.whl

---
+++ packages/forca2/forca2-0.1.2-py3-none-any.whl/forca2/main.py
@@ -0,0 +1,12 @@
+from forca2.titulo.titulo import mostra_titulo
+from forca2.menu.menu import menu
+
+
+def main():
+    mostra_titulo()
+    while True:
+        menu()
+
+if __name__ == "__main__":
+    main()
+
