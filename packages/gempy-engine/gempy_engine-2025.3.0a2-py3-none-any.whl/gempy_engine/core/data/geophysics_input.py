68b02e899767394d26e40e2b788cb37d05ac5237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:26 +0000

    Add package gempy-engine/2025.3.0a2/gempy_engine-2025.3.0a2-py3-none-any.whl

---
+++ packages/gempy-engine/gempy_engine-2025.3.0a2-py3-none-any.whl/gempy_engine/core/data/geophysics_input.py
@@ -0,0 +1,12 @@
+﻿from dataclasses import dataclass
+from typing import Annotated
+
+import numpy as np
+
+from .encoders.converters import numpy_array_short_validator
+
+
+@dataclass
+class GeophysicsInput:
+    tz: Annotated[np.ndarray,  numpy_array_short_validator]
+    densities: Annotated[np.ndarray,  numpy_array_short_validator]
