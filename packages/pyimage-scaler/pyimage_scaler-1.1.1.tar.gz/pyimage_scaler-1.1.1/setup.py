5aac49047145e1151c4657f18bd3ecb7d22f7766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:30 +0000

    Add package pyimage-scaler/1.1.1/pyimage_scaler-1.1.1.tar.gz

---
+++ packages/pyimage-scaler/pyimage_scaler-1.1.1.tar.gz/pyimage_scaler-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyimage_scaler",
+    version="1.1.1",
+    description="A unique Python package to resize images (JPEG, PNG, BMP, GIF)",
+    author="codingmaster24",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
