d55179266103b5bb2749c925ac2f6f09cbdd8e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:31 +0000

    Add package linux-api/1.2.2/linux_api-1.2.2.tar.gz

---
+++ packages/linux-api/linux_api-1.2.2.tar.gz/linux_api-1.2.2/linux_api/start.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+
+def main():
+    print(os.getcwd())
+    script = "./start.sh"
+
+    if not os.path.exists(script):
+        print(f"Error: {script} not found.")
+        sys.exit(1)
+
+    os.chmod(script, 0o755)
+
+    try:
+        subprocess.run(["./" + script], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing {script}: {e}")
