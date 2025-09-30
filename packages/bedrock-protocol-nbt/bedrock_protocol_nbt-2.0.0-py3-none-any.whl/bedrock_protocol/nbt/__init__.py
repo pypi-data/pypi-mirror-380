5db48839e261172d8b395c6271619cc9c2247a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:39 +0000

    Add package bedrock-protocol-nbt/2.0.0/bedrock_protocol_nbt-2.0.0-py3-none-any.whl

---
+++ packages/bedrock-protocol-nbt/bedrock_protocol_nbt-2.0.0-py3-none-any.whl/bedrock_protocol/nbt/__init__.py
@@ -0,0 +1,44 @@
+# Copyright © 2025 GlacieTeam. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from rapidnbt import (
+    TagType,
+    SnbtFormat,
+    EndTag,
+    ByteTag,
+    ShortTag,
+    IntTag,
+    Int64Tag,
+    FloatTag,
+    DoubleTag,
+    ByteArrayTag,
+    StringTag,
+    ListTag,
+    CompoundTag,
+    IntArrayTag,
+    LongArrayTag,
+    CompoundTagVariant,
+)
+
+__all__ = [
+    "TagType",
+    "SnbtFormat",
+    "EndTag",
+    "ByteTag",
+    "ShortTag",
+    "IntTag",
+    "Int64Tag",
+    "FloatTag",
+    "DoubleTag",
+    "ByteArrayTag",
+    "StringTag",
+    "ListTag",
+    "CompoundTag",
+    "IntArrayTag",
+    "LongArrayTag",
+    "CompoundTagVariant",
+]
