2fe36d39d9f7d93c0e41ce58d928864f09d71c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:28 +0000

    Add package newsflash/0.1.2/newsflash-0.1.2.tar.gz

---
+++ packages/newsflash/newsflash-0.1.2.tar.gz/newsflash-0.1.2/src/newsflash/widgets/__init__.py
@@ -0,0 +1,27 @@
+from .text import Title, Paragraph, Notifications
+from .layout import Rows, Columns, FlexRows, FlexColumns
+from .control import Button, EnumSelect, ListSelect
+# from .chart import BarChart, LineChart
+
+
+__all__ = [
+    # Text
+    "Title",
+    "Paragraph",
+    "Notifications",
+    
+    # Layout
+    "Rows",
+    "Columns",
+    "FlexRows",
+    "FlexColumns",
+
+    # Control
+    "Button",
+    "EnumSelect",
+    "ListSelect",
+
+    # Charts
+    # "BarChart",
+    # "LineChart",
+]
---
+++ packages/newsflash/newsflash-0.1.2.tar.gz/newsflash-0.1.2/src/newsflash/widgets/layout/__init__.py
@@ -0,0 +1,11 @@
+from .columns import Columns
+from .rows import Rows
+from .flex import FlexColumns, FlexRows
+
+
+__all__ = [
+    "Columns",
+    "Rows",
+    "FlexColumns",
+    "FlexRows",
+]
