fab4c653f5e92c4579fbf314b7149459a2f63208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:21 +0000

    Add package donnes-des-sous-mathis/0.1.0/donnes_des_sous_mathis-0.1.0-py3-none-any.whl

---
+++ packages/donnes-des-sous-mathis/donnes_des_sous_mathis-0.1.0-py3-none-any.whl/donnes_des_sous_mathis/banque.py
@@ -0,0 +1,15 @@
+def donnes_des_sous_mathis(a: int, b:int) -> int:
+    """Fonction qui additionne deux entiers.
+
+    Args:
+        a (int): Le premier entier.
+        b (int): Le second entier.
+
+    Returns:
+        int: La somme des deux entiers.
+    """
+
+    if(a>200):
+        return a+b+20
+
+    return a + b
