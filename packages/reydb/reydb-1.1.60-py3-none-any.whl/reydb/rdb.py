5c19052469ec49e5140de730a1c778ee204900a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:15:54 +0000

    Add package reydb/1.1.60/reydb-1.1.60-py3-none-any.whl

---
+++ packages/reydb/reydb-1.1.60-py3-none-any.whl/reydb/rdb.py
@@ -0,0 +1,535 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2022-12-05 14:10:02
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Database methods.
+"""
+
+
+from typing import TypeVar, Generic
+from urllib.parse import quote as urllib_quote
+from pymysql.constants.CLIENT import MULTI_STATEMENTS
+from sqlalchemy import Engine, create_engine as sqlalchemy_create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine as sqlalchemy_create_async_engine
+from reykit.rtext import join_data_text
+
+from . import rbase, rbuild, rconfig, rconn, rerror, rexec, rfile, rinfo, rorm, rparam
+
+
+__all__ = (
+    'DatabaseSuper',
+    'Database',
+    'DatabaseAsync'
+)
+
+
+DatabaseConnectionT = TypeVar('DatabaseConnectionT', 'rconn.DatabaseConnection', 'rconn.DatabaseConnectionAsync')
+DatabaseExecuteT = TypeVar('DatabaseExecuteT', 'rexec.DatabaseExecute', 'rexec.DatabaseExecuteAsync')
+DatabaseSchemaT = TypeVar('DatabaseSchemaT', 'rparam.DatabaseSchema', 'rparam.DatabaseSchemaAsync')
+DatabaseORMT = TypeVar('DatabaseORMT', 'rorm.DatabaseORM', 'rorm.DatabaseORMAsync')
+DatabaseBuildT = TypeVar('DatabaseBuildT')
+
+
+class DatabaseSuper(
+    rbase.DatabaseBase,
+    Generic[
+        rbase.EngineT,
+        DatabaseConnectionT,
+        DatabaseExecuteT,
+        DatabaseSchemaT,
+        DatabaseORMT,
+        DatabaseBuildT
+    ]
+):
+    """
+    Database super type, based `MySQL`.
+    """
+
+
+    def __init__(
+        self,
+        host: str,
+        port: int | str,
+        username: str,
+        password: str,
+        database: str,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+        pool_timeout: float = 30.0,
+        pool_recycle: int | None = 3600,
+        report: bool = False,
+        **query: str
+    ) -> None:
+        """
+        Build instance attributes.
+
+        Parameters
+        ----------
+        host : Remote server database host.
+        port : Remote server database port.
+        username : Remote server database username.
+        password : Remote server database password.
+        database : Remote server database name.
+        pool_size : Number of connections `keep open`.
+        max_overflow : Number of connections `allowed overflow`.
+        pool_timeout : Number of seconds `wait create` connection.
+        pool_recycle : Number of seconds `recycle` connection.
+            - `None | Literal[-1]`: No recycle.
+            - `int`: Use this value.
+        report : Whether report SQL execute information, not include ORM execute.
+        query : Remote server database parameters.
+        """
+
+        # Handle parameter.
+        if type(port) == str:
+            port = int(port)
+
+        # Build.
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port: int | None = port
+        self.database = database
+        self.pool_size = pool_size
+        self.max_overflow = max_overflow
+        self.pool_timeout = pool_timeout
+        if pool_recycle is None:
+            self.pool_recycle = -1
+        else:
+            self.pool_recycle = pool_recycle
+        self.report = report
+        self.query = query
+
+        ## Create engine.
+        self.engine = self.__create_engine()
+
+
+    def __str__(self) -> str:
+        """
+        Return connection information text.
+        """
+
+        # Generate.
+        filter_key = (
+            'engine',
+            'connection',
+            'rdatabase',
+            'begin'
+        )
+        info = {
+            key: value
+            for key, value in self.__dict__.items()
+            if key not in filter_key
+        }
+        info['conn_count'] = self.conn_count
+        text = join_data_text(info)
+
+        return text
+
+
+    @property
+    def backend(self) -> str:
+        """
+        Database backend name.
+
+        Returns
+        -------
+        Name.
+        """
+
+        # Get.
+        url_params = rbase.extract_url(self.url)
+        backend = url_params['backend']
+
+        return backend
+
+
+    @property
+    def driver(self) -> str:
+        """
+        Database driver name.
+
+        Returns
+        -------
+        Name.
+        """
+
+        # Get.
+        url_params = rbase.extract_url(self.url)
+        driver = url_params['driver']
+
+        return driver
+
+
+    @property
+    def url(self) -> str:
+        """
+        Generate server URL.
+
+        Returns
+        -------
+        Server URL.
+        """
+
+        # Generate URL.
+        password = urllib_quote(self.password)
+        match self:
+            case Database():
+                url_ = f'mysql+pymysql://{self.username}:{password}@{self.host}:{self.port}/{self.database}'
+            case DatabaseAsync():
+                url_ = f'mysql+aiomysql://{self.username}:{password}@{self.host}:{self.port}/{self.database}'
+
+        # Add Server parameter.
+        if self.query != {}:
+            query = '&'.join(
+                [
+                    f'{key}={value}'
+                    for key, value in self.query.items()
+                ]
+            )
+            url_ = f'{url_}?{query}'
+
+        return url_
+
+
+    def __create_engine(self) -> rbase.EngineT:
+        """
+        Create database `Engine` object.
+
+        Returns
+        -------
+        Engine object.
+        """
+
+        # Handle parameter.
+        engine_params = {
+            'url': self.url,
+            'pool_size': self.pool_size,
+            'max_overflow': self.max_overflow,
+            'pool_timeout': self.pool_timeout,
+            'pool_recycle': self.pool_recycle,
+            'connect_args': {'client_flag': MULTI_STATEMENTS}
+        }
+
+        # Create Engine.
+        match self:
+            case Database():
+                engine = sqlalchemy_create_engine(**engine_params)
+            case DatabaseAsync():
+                engine = sqlalchemy_create_async_engine(**engine_params)
+
+        return engine
+
+
+    @property
+    def conn_count(self) -> tuple[int, int]:
+        """
+        Count number of keep open and allowed overflow connection.
+
+        Returns
+        -------
+        Number of keep open and allowed overflow connection.
+        """
+
+        # Count.
+        _overflow: int = self.engine.pool._overflow
+        if _overflow < 0:
+            keep_n = self.pool_size + _overflow
+            overflow_n = 0
+        else:
+            keep_n = self.pool_size
+            overflow_n = _overflow
+
+        return keep_n, overflow_n
+
+
+    def connect(self, autocommit: bool = False) -> DatabaseConnectionT:
+        """
+        Build database connection instance.
+
+        Parameters
+        ----------
+        autocommit: Whether automatic commit execute.
+
+        Returns
+        -------
+        Database connection instance.
+        """
+
+        # Build.
+        match self:
+            case Database():
+                conn = rconn.DatabaseConnection(self, autocommit)
+            case DatabaseAsync():
+                conn = rconn.DatabaseConnectionAsync(self, autocommit)
+
+        return conn
+
+
+    @property
+    def execute(self) -> DatabaseExecuteT:
+        """
+        Build database execute instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        conn = self.connect(True)
+        exec = conn.execute
+
+        return exec
+
+
+    @property
+    def orm(self) -> DatabaseORMT:
+        """
+        Build database ORM instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        match self:
+            case Database():
+                orm = rorm.DatabaseORM(self)
+            case DatabaseAsync():
+                orm = rorm.DatabaseORMAsync(self)
+
+        return orm
+
+
+    @property
+    def build(self) -> DatabaseBuildT:
+        """
+        Build database build instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        match self:
+            case Database():
+                build = rbuild.DatabaseBuild(self)
+            case DatabaseAsync():
+                build = rbuild.DatabaseBuildAsync(self)
+
+        return build
+
+
+    @property
+    def file(self):
+        """
+        Build database file instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        dbfile = rfile.DatabaseFile(self)
+
+        return dbfile
+
+
+    @property
+    def error(self):
+        """
+        Build database error instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        dbfile = rerror.DatabaseError(self)
+
+        return dbfile
+
+
+    @property
+    def config(self):
+        """
+        Build database config instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        dbconfig = rconfig.DatabaseConfig(self)
+
+        return dbconfig
+
+
+    @property
+    def info(self):
+        """
+        Build database information schema instance.
+
+        Returns
+        -------
+        Instance.
+
+        Examples
+        --------
+        Get databases information of server.
+        >>> databases_info = DatabaseInformationSchema()
+
+        Get tables information of database.
+        >>> tables_info = DatabaseInformationSchema.database()
+
+        Get columns information of table.
+        >>> columns_info = DatabaseInformationSchema.database.table()
+
+        Get database attribute.
+        >>> database_attr = DatabaseInformationSchema.database['attribute']
+
+        Get table attribute.
+        >>> database_attr = DatabaseInformationSchema.database.table['attribute']
+
+        Get column attribute.
+        >>> database_attr = DatabaseInformationSchema.database.table.column['attribute']
+        """
+
+        # Build.
+        dbischema = rinfo.DatabaseInformationSchema(self)
+
+        return dbischema
+
+
+    @property
+    def schema(self) -> DatabaseSchemaT:
+        """
+        Build database schema instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        match self:
+            case Database():
+                schema = rparam.DatabaseSchema(self)
+            case DatabaseAsync():
+                schema = rparam.DatabaseSchemaAsync(self)
+
+        return schema
+
+
+    @property
+    def status(self):
+        """
+        Build database parameters status instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        dbps = rparam.DatabaseParametersStatus(self, False)
+
+        return dbps
+
+
+    @property
+    def status_global(self):
+        """
+        Build global database parameters status instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        dbps = rparam.DatabaseParametersStatus(self, True)
+
+        return dbps
+
+
+    @property
+    def variables(self):
+        """
+        Build database parameters variable instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+        dbpv = rparam.DatabaseParametersVariable(self, False)
+
+        return dbpv
+
+
+    @property
+    def variables_global(self):
+        """
+        Build global database parameters variable instance.
+
+        Returns
+        -------
+        Instance.
+        """
+
+        # Build.
+
+        ## SQLite.
+        dbpv = rparam.DatabaseParametersVariable(self, True)
+
+        return dbpv
+
+
+class Database(
+    DatabaseSuper[
+        Engine,
+        'rconn.DatabaseConnection',
+        'rexec.DatabaseExecute',
+        'rparam.DatabaseSchema',
+        'rorm.DatabaseORM',
+        'rbuild.DatabaseBuild'
+    ]
+):
+    """
+    Database type, based `MySQL`.
+    """
+
+
+class DatabaseAsync(
+    DatabaseSuper[
+        AsyncEngine,
+        'rconn.DatabaseConnectionAsync',
+        'rexec.DatabaseExecuteAsync',
+        'rparam.DatabaseSchemaAsync',
+        'rorm.DatabaseORMAsync',
+        'rbuild.DatabaseBuildAsync'
+    ]
+):
+    """
+    Asynchronous database type, based `MySQL`.
+    """
+
+
+    async def dispose(self) -> None:
+        """
+        Dispose asynchronous connections.
+        """
+
+        # Dispose.
+        await self.engine.dispose()
---
+++ packages/reydb/reydb-1.1.60-py3-none-any.whl/reydb/rparam.py
@@ -0,0 +1,408 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2022-12-05 14:10:02
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Database parameter methods.
+"""
+
+
+from typing import TypeVar, Generic, overload
+
+from . import rdb
+from .rbase import DatabaseBase
+from .rexec import Result
+
+
+__all__ = (
+    'DatabaseSchema',
+    'DatabaseParameters',
+    'DatabaseParametersStatus',
+    'DatabaseParametersVariable'
+)
+
+
+DatabaseT = TypeVar('DatabaseT', 'rdb.Database', 'rdb.DatabaseAsync')
+
+
+class DatabaseSchemaSuper(DatabaseBase, Generic[DatabaseT]):
+    """
+    Database schema super type.
+    """
+
+
+    def __init__(self, db: DatabaseT) -> None:
+        """
+        Build instance attributes.
+
+        Parameters
+        ----------
+        db: Database instance.
+        """
+
+        # Set parameter.
+        self.db = db
+
+
+    def _call__before(self, filter_default: bool = True) -> tuple[str, tuple[str, ...]]:
+        """
+        Before handle of call method.
+
+        Parameters
+        ----------
+        filter_default : Whether filter default database.
+
+        Returns
+        -------
+        Parameter `sql` and `filter_db`.
+        """
+
+        # Handle parameter.
+        filter_db = (
+            'information_schema',
+            'performance_schema',
+            'mysql',
+            'sys'
+        )
+        if filter_default:
+            where_database = 'WHERE `SCHEMA_NAME` NOT IN :filter_db\n'
+            where_column = '    WHERE `TABLE_SCHEMA` NOT IN :filter_db\n'
+        else:
+            where_database = where_column = ''
+
+        # Select.
+        sql = (
+            'SELECT GROUP_CONCAT(`SCHEMA_NAME`) AS `TABLE_SCHEMA`, NULL AS `TABLE_NAME`, NULL AS `COLUMN_NAME`\n'
+            'FROM `information_schema`.`SCHEMATA`\n'
+            f'{where_database}'
+            'UNION ALL (\n'
+            '    SELECT `TABLE_SCHEMA`, `TABLE_NAME`, `COLUMN_NAME`\n'
+            '    FROM `information_schema`.`COLUMNS`\n'
+            f'{where_column}'
+            '    ORDER BY `TABLE_SCHEMA`, `TABLE_NAME`, `ORDINAL_POSITION`\n'
+            ')'
+        )
+
+        return sql, filter_db
+
+
+    def _call__after(self, result: Result) -> dict[str, dict[str, list[str]]]:
+        """
+        After handle of call method.
+
+        Parameters
+        ----------
+        result : Database select result.
+
+        Returns
+        -------
+        Parameter `schema_dict`.
+        """
+
+        # Convert.
+        database_names, *_ = result.fetchone()
+        database_names: list[str] = database_names.split(',')
+        schema_dict = {}
+        for database, table, column in result:
+            if database in database_names:
+                database_names.remove(database)
+
+            ## Index database.
+            if database not in schema_dict:
+                schema_dict[database] = {table: [column]}
+                continue
+            table_dict: dict = schema_dict[database]
+
+            ## Index table. 
+            if table not in table_dict:
+                table_dict[table] = [column]
+                continue
+            column_list: list = table_dict[table]
+
+            ## Add column.
+            column_list.append(column)
+
+        ## Add empty database.
+        for name in database_names:
+            schema_dict[name] = None
+
+        return schema_dict
+
+
+class DatabaseSchema(DatabaseSchemaSuper['rdb.Database']):
+    """
+    Database schema type.
+    """
+
+
+    def __call__(self, filter_default: bool = True) -> dict[str, dict[str, list[str]]]:
+        """
+        Get schemata of databases and tables and columns.
+
+        Parameters
+        ----------
+        filter_default : Whether filter default database.
+
+        Returns
+        -------
+        Schemata of databases and tables and columns.
+        """
+
+        # Get.
+        sql, filter_db = self._call__before(filter_default)
+        result = self.db.execute(sql, filter_db=filter_db)
+        schema_dict = self._call__after(result)
+
+        return schema_dict
+
+
+class DatabaseSchemaAsync(DatabaseSchemaSuper['rdb.DatabaseAsync']):
+    """
+    Asynchronous database schema type.
+    """
+
+
+    async def __call__(self, filter_default: bool = True) -> dict[str, dict[str, list[str]]]:
+        """
+        Asynchronous get schemata of databases and tables and columns.
+
+        Parameters
+        ----------
+        filter_default : Whether filter default database.
+
+        Returns
+        -------
+        Schemata of databases and tables and columns.
+        """
+
+        # Get.
+        sql, filter_db = self._call__before(filter_default)
+        result = await self.db.execute(sql, filter_db=filter_db)
+        schema_dict = self._call__after(result)
+
+        return schema_dict
+
+
+class DatabaseParameters(DatabaseBase):
+    """
+    Database parameters type.
+    """
+
+
+    def __init__(
+        self,
+        db: 'rdb.Database',
+        global_: bool
+    ) -> None:
+        """
+        Build instance attributes.
+
+        Parameters
+        ----------
+        db: Database instance.
+        global\\_ : Whether base global.
+        """
+
+        # Set parameter.
+        self.db = db
+        self.global_ = global_
+
+
+    def __getitem__(self, key: str) -> str | None:
+        """
+        Get item of parameter dictionary.
+
+        Parameters
+        ----------
+        key : Parameter key.
+
+        Returns
+        -------
+        Parameter value.
+        """
+
+        # Get.
+        value = self.get(key)
+
+        return value
+
+
+    def __setitem__(self, key: str, value: str | float) -> None:
+        """
+        Set item of parameter dictionary.
+
+        Parameters
+        ----------
+        key : Parameter key.
+        value : Parameter value.
+        """
+
+        # Set.
+        params = {key: value}
+
+        # Update.
+        self.update(params)
+
+
+class DatabaseParametersStatus(DatabaseParameters):
+    """
+    Database parameters status type.
+    """
+
+
+    @overload
+    def get(self) -> dict[str, str]: ...
+
+    @overload
+    def get(self, key: str) -> str | None: ...
+
+    def get(self, key: str | None = None) -> dict[str, str] | str | None:
+        """
+        Get parameter.
+
+        Parameters
+        ----------
+        key : Parameter key.
+            - `None`: Return dictionary of all parameters.
+            - `str`: Return value of parameter.
+
+        Returns
+        -------
+        Status of database.
+        """
+
+        # Generate SQL.
+
+        ## Global.
+        if self.global_:
+            sql = 'SHOW GLOBAL STATUS'
+
+        ## Not global.
+        else:
+            sql = 'SHOW STATUS'
+
+        # Execute SQL.
+
+        ## Dictionary.
+        if key is None:
+            result = self.db.execute(sql, key=key)
+            status = result.to_dict(val_field=1)
+
+        ## Value.
+        else:
+            sql += ' LIKE :key'
+            result = self.db.execute(sql, key=key)
+            row = result.first()
+            if row is None:
+                status = None
+            else:
+                status = row['Value']
+
+        return status
+
+
+    def update(self, params: dict[str, str | float]) -> None:
+        """
+        Update parameter.
+
+        Parameters
+        ----------
+        params : Update parameter key value pairs.
+        """
+
+        # Throw exception.
+        raise AssertionError('database status not update')
+
+
+class DatabaseParametersVariable(DatabaseParameters):
+    """
+    Database parameters variable type.
+    """
+
+
+    @overload
+    def get(self) -> dict[str, str]: ...
+
+    @overload
+    def get(self, key: str) -> str | None: ...
+
+    def get(self, key: str | None = None) -> dict[str, str] | str | None:
+        """
+        Get parameter.
+
+        Parameters
+        ----------
+        key : Parameter key.
+            - `None`: Return dictionary of all parameters.
+            - `str`: Return value of parameter.
+
+        Returns
+        -------
+        Variables of database.
+        """
+
+        # Generate SQL.
+
+        ## Global.
+        if self.global_:
+            sql = 'SHOW GLOBAL VARIABLES'
+
+        ## Not global.
+        else:
+            sql = 'SHOW VARIABLES'
+
+        # Execute SQL.
+
+        ## Dictionary.
+        if key is None:
+            result = self.db.execute(sql, key=key)
+            variables = result.to_dict(val_field=1)
+
+        ## Value.
+        else:
+            sql += ' LIKE :key'
+            result = self.db.execute(sql, key=key)
+            row = result.first()
+            if row is None:
+                variables = None
+            else:
+                variables = row['Value']
+
+        return variables
+
+
+    def update(self, params: dict[str, str | float]) -> None:
+        """
+        Update parameter.
+
+        Parameters
+        ----------
+        params : Update parameter key value pairs.
+        """
+
+        # Generate SQL.
+        sql_set_list = [
+            '%s = %s' % (
+                key,
+                (
+                    value
+                    if type(value) in (int, float)
+                    else "'%s'" % value
+                )
+            )
+            for key, value in params.items()
+        ]
+        sql_set = ',\n    '.join(sql_set_list)
+
+        ## Global.
+        if self.global_:
+            sql = f'SET GLOBAL {sql_set}'
+
+        ## Not global.
+        else:
+            sql = f'SET {sql_set}'
+
+        # Execute SQL.
+        self.db.execute(sql)
