47457dd395e124327850db9b22e4d1a418c0b233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:41 +0000

    Add package pysp-xcw/7.4.1/pysp_xcw-7.4.1.tar.gz

---
+++ packages/pysp-xcw/pysp_xcw-7.4.1.tar.gz/pysp_xcw-7.4.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pysp-xcw",  # 包名
+    version="7.4.1",      # 版本号
+    author="Xiong Chengwen",   # 作者
+    author_email="xcw1824@outlook.com",  # 作者邮箱
+    description="Various classic and modern signal analysis and processing algorithms are implemented.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xcw1129/PySP",  # 项目主页
+    packages=find_packages(),  # 自动发现包
+    package_data={
+        "PySP.Assist_Module": ["times+simsun.ttf"],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.21.0",
+        "scipy>=1.7.0",
+        "matplotlib>=3.4.0",
+        "pandas>=1.3.0",
+    ],
+    keywords=["signal processing", "spectral analysis", "time-frequency analysis", "feature extraction", "signal decomposition"],
+    project_urls={
+        "Bug Tracker": "https://github.com/xcw1129/PySP/issues",
+        "Source Code": "https://github.com/xcw1129/PySP",
+    },
+)
