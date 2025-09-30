9ff3871371a183fd115dfd9eb3624d16cf5e42be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:09 +0000

    Add package donnes-des-sous-kyllian/0.1.0/donnes_des_sous_kyllian-0.1.0-py3-none-any.whl

---
+++ packages/donnes-des-sous-kyllian/donnes_des_sous_kyllian-0.1.0-py3-none-any.whl/donnes_des_sous_kyllian/__init__.py
@@ -0,0 +1,6 @@
+from .banque import donnes_des_sous_kyllian
+
+def main() -> None:
+    print("Hello from donnes-des-sous-kyllian!")
+    result = donnes_des_sous_kyllian(250, 100)
+    print(f"Result: {result}")
