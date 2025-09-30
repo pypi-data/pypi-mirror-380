2815f6bb24de8b31be88a0cd4de9e8d5fe9f09c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:04:58 +0000

    Add package kaqing/2.0.48/kaqing-2.0.48-py3-none-any.whl

---
+++ packages/kaqing/kaqing-2.0.48-py3-none-any.whl/adam/commands/postgres/pg_completions.py
@@ -0,0 +1,17 @@
+from adam.commands.postgres.pg_table_completer import PgTableCompleter
+
+def pg_completions(ns: str, pg_path: str):
+    table = PgTableCompleter(ns, pg_path)
+    return {
+        '\h': None,
+        '\d': None,
+        '\dt': None,
+        '\du': None,
+        'delete': {'from': table.nested({'where': {'id': {'=': {"'id'": None}}}})},
+        'insert': {'into': table.nested({'values(': None})},
+        'select': {'*': {'from': table.nested({
+            'limit': {'1': {'where': {'id': {'=': {"'id'": None}}}}},
+            'where': {'id': {'=': {"'id'": {'limit': {'1': None}}}}}
+        })}},
+        'update': table.nested({'set': {'column': {'=': None}}}),
+    }
