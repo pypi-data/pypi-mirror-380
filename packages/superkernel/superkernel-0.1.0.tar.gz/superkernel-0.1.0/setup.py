def612ea2fcb566951664dd4bd26678fa1d8e38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:24 +0000

    Add package superkernel/0.1.0/superkernel-0.1.0.tar.gz

---
+++ packages/superkernel/superkernel-0.1.0.tar.gz/superkernel-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="superkernel",
+    version="0.1.0",
+    packages=find_packages(),
+    author="graph-infra-community",
+    author_email="joozhan@163.com",
+    description="A minimal Python package that prints Hello World",
+    long_description="A super kernel simple package that demonstrates how to create and publish a Python package to PyPI.",
+    long_description_content_type="text/plain",
+    url="https://gitcode.com/cann/graph-infra",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
