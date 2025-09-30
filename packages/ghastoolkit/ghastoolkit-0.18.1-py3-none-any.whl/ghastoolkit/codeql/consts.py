c4f6694f0d0967b1214cdd5168773a28dfd5afed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:47 +0000

    Add package ghastoolkit/0.18.1/ghastoolkit-0.18.1-py3-none-any.whl

---
+++ packages/ghastoolkit/ghastoolkit-0.18.1-py3-none-any.whl/ghastoolkit/codeql/consts.py
@@ -0,0 +1,16 @@
+# Dict of CodeQL supported Languages
+CODEQL_LANGUAGES = {
+    "actions": "actions",
+    "c": "cpp",
+    "cpp": "cpp",
+    "csharp": "csharp",
+    "java": "java",
+    "kotlin": "java",
+    "javascript": "javascript",
+    "typescript": "javascript",
+    "go": "go",
+    "python": "python",
+    "rust": "rust",
+    "swift": "swift",
+    "ruby": "ruby",
+}
