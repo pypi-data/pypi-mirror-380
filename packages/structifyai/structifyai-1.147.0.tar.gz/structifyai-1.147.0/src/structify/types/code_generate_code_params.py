5300cd6edf25ac9613df2675104c31f4ee1744c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:05:26 +0000

    Add package structifyai/1.147.0/structifyai-1.147.0.tar.gz

---
+++ packages/structifyai/structifyai-1.147.0.tar.gz/structifyai-1.147.0/src/structify/types/code_generate_code_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["CodeGenerateCodeParams"]
+
+
+class CodeGenerateCodeParams(TypedDict, total=False):
+    chat_session_id: Required[Annotated[str, PropertyInfo(alias="chatSessionId")]]
+
+    prompt: Required[str]
+
+    assistant_message_id: Annotated[Optional[str], PropertyInfo(alias="assistantMessageId")]
+
+    user_message_id: Annotated[Optional[str], PropertyInfo(alias="userMessageId")]
