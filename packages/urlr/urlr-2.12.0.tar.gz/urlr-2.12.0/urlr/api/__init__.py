d100c437cf7cacdd4ab7f7905abff6e9200acd7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:57 +0000

    Add package urlr/2.12.0/urlr-2.12.0.tar.gz

---
+++ packages/urlr/urlr-2.12.0.tar.gz/urlr-2.12.0/urlr/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from urlr.api.access_tokens_api import AccessTokensApi
+from urlr.api.domains_api import DomainsApi
+from urlr.api.folders_api import FoldersApi
+from urlr.api.links_api import LinksApi
+from urlr.api.qr_codes_api import QRCodesApi
+from urlr.api.statistics_api import StatisticsApi
+from urlr.api.workspaces_api import WorkspacesApi
+
