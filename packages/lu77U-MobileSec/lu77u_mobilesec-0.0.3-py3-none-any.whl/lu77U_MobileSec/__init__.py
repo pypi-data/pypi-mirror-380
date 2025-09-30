0ababa5dd4c8dc2ed49cb73a183eb0d433332d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:39 +0000

    Add package lu77U-MobileSec/0.0.3/lu77u_mobilesec-0.0.3-py3-none-any.whl

---
+++ packages/lu77U-MobileSec/lu77u_mobilesec-0.0.3-py3-none-any.whl/lu77U_MobileSec/__init__.py
@@ -0,0 +1,9 @@
+"""lu77U-MobileSec - The Only Mobile Security Tool Which You Need"""
+
+__version__ = "0.0.3"
+__author__ = "Sam MG Harish"
+__email__ = "sammgharish@gmail.com"
+
+from .core.app import MobileSecApp
+
+__all__ = ["MobileSecApp"]
---
+++ packages/lu77U-MobileSec/lu77u_mobilesec-0.0.3-py3-none-any.whl/lu77U_MobileSec/ui/colors.py
@@ -0,0 +1,38 @@
+"""Color constants and utilities"""
+
+import platform
+
+if platform.system() == "Windows":
+    try:
+        import colorama
+        colorama.init(autoreset=True, convert=True, strip=False, wrap=True)
+    except ImportError:
+        pass
+else:
+    try:
+        import colorama
+        colorama.init(autoreset=False)
+    except ImportError:
+        pass
+
+class Colors:
+    """ANSI color codes for terminal output"""
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    MAGENTA = '\033[35m'
+    
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    
+    SUCCESS = GREEN
+    ERROR = RED
+    WARNING = PURPLE  
+    INFO = CYAN
+    HEADER = PURPLE
+    VERBOSE = YELLOW
