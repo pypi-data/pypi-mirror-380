48ab5b9079437fad9d4c4640c6d1bd2fe2257166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:13:28 +0000

    Add package foundationallm/0.9.7rc418/foundationallm-0.9.7rc418-py3-none-any.whl

---
+++ packages/foundationallm/foundationallm-0.9.7rc418-py3-none-any.whl/foundationallm/models/language_models/language_model_provider.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class LanguageModelProvider(str, Enum):
+    """Enumerator of the Language Model providers."""
+    AZUREAI = "azureai"
+    MICROSOFT = "microsoft"
+    OPENAI = "openai"
+    BEDROCK = "bedrock"
+    VERTEXAI = "vertexai"
