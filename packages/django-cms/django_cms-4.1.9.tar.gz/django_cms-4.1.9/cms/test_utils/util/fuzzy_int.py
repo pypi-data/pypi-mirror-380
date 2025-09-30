6cc484941be67939f45b0e52adcaa7bb5a965b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:56 +0000

    Add package django-cms/4.1.9/django_cms-4.1.9.tar.gz

---
+++ packages/django-cms/django_cms-4.1.9.tar.gz/django_cms-4.1.9/cms/test_utils/util/fuzzy_int.py
@@ -0,0 +1,17 @@
+
+
+class FuzzyInt(int):
+    def __new__(cls, lowest, highest):
+        obj = super().__new__(cls, highest)
+        obj.lowest = lowest
+        obj.highest = highest
+        return obj
+
+    def __eq__(self, other):
+        return other >= self.lowest and other <= self.highest
+
+    def __repr__(self):
+        return "[%d..%d]" % (self.lowest, self.highest)
+
+    def __hash__(self):
+        return hash((self.lowest, self.highest))
