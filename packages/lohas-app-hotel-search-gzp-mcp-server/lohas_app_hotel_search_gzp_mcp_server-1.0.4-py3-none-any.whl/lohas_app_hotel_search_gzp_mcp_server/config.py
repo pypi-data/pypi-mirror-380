2f832e366f633ff163a5b3285bf3ab1268dfabed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:16:02 +0000

    Add package lohas-app-hotel-search-gzp-mcp-server/1.0.4/lohas_app_hotel_search_gzp_mcp_server-1.0.4-py3-none-any.whl

---
+++ packages/lohas-app-hotel-search-gzp-mcp-server/lohas_app_hotel_search_gzp_mcp_server-1.0.4-py3-none-any.whl/lohas_app_hotel_search_gzp_mcp_server/config.py
@@ -0,0 +1,11 @@
+# config.py
+# 数据库配置
+# 数据库配置（请替换为您的）
+DB_CONFIG = {
+   "host": "rm-bp1un2iccg5796p08to.mysql.rds.aliyuncs.com",
+    "port": 3306,
+    "user": "lohas",
+    "password": "Lohas123",
+    "database": "hotel_bidder",
+    "charset": "utf8mb4"
+}
