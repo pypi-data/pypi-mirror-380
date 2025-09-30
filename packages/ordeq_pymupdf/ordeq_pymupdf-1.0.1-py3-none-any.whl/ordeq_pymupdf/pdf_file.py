8cc615d4742084eaad273686cce10e8e0e7afe96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:06:02 +0000

    Add package ordeq_pymupdf/1.0.1/ordeq_pymupdf-1.0.1-py3-none-any.whl

---
+++ packages/ordeq_pymupdf/ordeq_pymupdf-1.0.1-py3-none-any.whl/ordeq_pymupdf/pdf_file.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import pymupdf
+from ordeq import IO
+
+
+@dataclass(frozen=True, kw_only=True)
+class PymupdfFile(IO[pymupdf.Document]):
+    """IO to load from and save PDF files using Pymupdf.
+
+    Example usage:
+
+    ```python
+    >>> from pathlib import Path
+    >>> from ordeq_pymupdf import PymupdfFile
+    >>> MyPDF = PymupdfFile(
+    ...     path=Path("path/to.index")
+    ... )
+
+    ```
+
+    """
+
+    path: Path
+
+    def load(self) -> pymupdf.Document:
+        """Read a PDF file and open the pymypdf Document.
+
+        Returns:
+            `pymupdf` Document
+        """
+        return pymupdf.open(self.path)
+
+    def save(self, document: pymupdf.Document) -> None:
+        """Save a pymypdf Document to a file.
+
+        Args:
+            document: document to save
+        """
+        document.save(self.path)
