eb2c3eee2803443cb9fbe201a3304d6daa5c94e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:53 +0000

    Add package v440/2.0.0.dev65/v440-2.0.0.dev65-py3-none-any.whl

---
+++ packages/v440/v440-2.0.0.dev65-py3-none-any.whl/v440/_utils/SlotList.py
@@ -0,0 +1,32 @@
+from functools import partial
+from typing import *
+
+import setdoc
+from datarepr import datarepr
+
+from v440._utils.BaseList import BaseList
+
+__all__ = ["SlotList"]
+
+
+class SlotList(BaseList):
+    __slots__ = ()
+
+    string: str
+
+    @setdoc.basic
+    def __len__(self: Self) -> int:
+        return len(type(self).__slots__)
+
+    @setdoc.basic
+    def __repr__(self: Self) -> str:
+        return datarepr(type(self).__name__, **self._todict())
+
+    def _cmp(self: Self) -> tuple:
+        return tuple(map(partial(getattr, self), type(self).__slots__))
+
+    def _set(self: Self, value: Any) -> None:
+        if value is None:
+            self.string = ""
+        else:
+            self.string = value
