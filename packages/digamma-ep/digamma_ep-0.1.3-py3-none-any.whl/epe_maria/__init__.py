02825c88dad2024e8931691e04d3fab426f8dd69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:06:14 +0000

    Add package digamma-ep/0.1.3/digamma_ep-0.1.3-py3-none-any.whl

---
+++ packages/digamma-ep/digamma_ep-0.1.3-py3-none-any.whl/epe_maria/__init__.py
@@ -0,0 +1,3 @@
+from .metrics import phi, delta_phi, phi_star
+from .monitor import drift, curvature  # Uncomment when defined
+from .benchmark import fairness_score, ks_compare  # Uncomment when defined
