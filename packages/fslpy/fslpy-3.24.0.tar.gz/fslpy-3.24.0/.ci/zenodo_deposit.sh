4840ef6aaec2358df4a7c77a1c15c79b96c1b3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:43 +0000

    Add package fslpy/3.24.0/fslpy-3.24.0.tar.gz

---
+++ packages/fslpy/fslpy-3.24.0.tar.gz/fslpy-3.24.0/.ci/zenodo_deposit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+tmp=`dirname $0`
+pushd $tmp > /dev/null
+thisdir=`pwd`
+popd > /dev/null
+
+zenodo_url=$1
+zenodo_tkn=$2
+zenodo_depid=$3
+
+version=$(cat fsl/version.py      |
+          egrep '^__version__ +=' |
+          cut -d "=" -f 2         |
+          tr -d "'"               |
+          tr -d ' ')
+upfile=$(pwd)/dist/fslpy-"$version".tar.gz
+metafile=$(pwd)/.ci/zenodo_meta.json.jinja2
+date=$(date +"%Y-%m-%d")
+
+pip install --retries 10 requests jinja2
+
+python "$thisdir"/zenodo.py \
+       "$zenodo_url" \
+       "$zenodo_tkn" \
+       "$zenodo_depid" \
+       "$upfile" \
+       "$metafile" \
+       "$version" \
+       "$date"
