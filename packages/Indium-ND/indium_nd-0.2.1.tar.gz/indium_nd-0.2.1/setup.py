813fc3f5fb304ef8165951f5fb435f3e02007c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:06:33 +0000

    Add package Indium-ND/0.2.1/indium_nd-0.2.1.tar.gz

---
+++ packages/Indium-ND/indium_nd-0.2.1.tar.gz/indium_nd-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Indium-ND",                 # ⚠ must be unique on PyPI
+    version="0.2.1",
+    author="Sayon",
+    author_email="codewithsayon@gmail.com",
+    description="Utility package with file operations and ASCII headings",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pyfiglet",
+        "colorama",
+        "pyautogui",
+        "yaspin",
+        "alive-progress",
+        "rich",
+        "InquirerPy",
+        "tabulate",
+        "keyboard"
+    ],
+    python_requires=">=3.7",
+)
