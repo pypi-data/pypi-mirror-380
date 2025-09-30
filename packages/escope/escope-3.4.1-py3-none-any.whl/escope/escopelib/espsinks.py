7008ee24eb67ec68f48847a995273856016b3708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:06:18 +0000

    Add package escope/3.4.1/escope-3.4.1-py3-none-any.whl

---
+++ packages/escope/escope-3.4.1-py3-none-any.whl/escope/escopelib/espsinks.py
@@ -0,0 +1,30 @@
+# espsinks.py - This file is part of EScope/ESpark
+# (C) 2024  Daniel A. Wagenaar
+#
+# EScope and ESpark are free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# EScope and ESpark are distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software. If not, see <http://www.gnu.org/licenses/>.
+
+
+# espsinks.py
+
+from . import espdatasink
+from . import espdsnidaq
+
+def makeDataSink(cfg):
+    if cfg.hw.adapter[0]=='nidaq':
+        return espdsnidaq.ESPDS_Nidaq(cfg)
+    elif cfg.hw.adapter[0]=='dummy':
+        return espdatasink.ESPDS_Dummy(cfg)
+    else:
+        raise RuntimeError('Unknown adapter type: ' + cfg.hw.adapter[0])
+    
