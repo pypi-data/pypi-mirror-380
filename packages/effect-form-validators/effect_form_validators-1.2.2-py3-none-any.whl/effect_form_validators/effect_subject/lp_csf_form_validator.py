5f94a735b3532b0379e73ae11b65b251866b43e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:05:09 +0000

    Add package effect-form-validators/1.2.2/effect_form_validators-1.2.2-py3-none-any.whl

---
+++ packages/effect-form-validators/effect_form_validators-1.2.2-py3-none-any.whl/effect_form_validators/effect_subject/lp_csf_form_validator.py
@@ -0,0 +1,44 @@
+from typing import Any
+
+from edc_constants.constants import NO, YES
+from edc_crf.crf_form_validator import CrfFormValidator
+from edc_csf.form_validators import (
+    LpFormValidatorMixin,
+    QuantitativeCsfFormValidatorMixin,
+)
+from edc_lab.form_validators import CrfRequisitionFormValidatorMixin
+
+
+class LpCsfFormValidator(
+    CrfRequisitionFormValidatorMixin,
+    LpFormValidatorMixin,
+    QuantitativeCsfFormValidatorMixin,
+    CrfFormValidator,
+):
+    csf_culture_panel = None
+
+    def clean(self):
+        self.validate_lp()
+        self.validate_csf_assessment()
+        self.validate_csf_culture("csf_requisition")
+
+    def validate_csf_assessment(self: Any):
+        for fld in [
+            "india_ink",
+            "csf_crag_lfa",
+            "sq_crag",
+            "sq_crag_pos",
+            "crf_crag_titre_done",
+        ]:
+            self.applicable_if(YES, NO, field="csf_positive", field_applicable=fld)
+
+        self.required_if(YES, field="crf_crag_titre_done", field_required="crf_crag_titre")
+
+    def validate_csf_culture(self: Any, requisition: str):
+        self.require_together(
+            field=requisition,
+            field_required="csf_culture_assay_datetime",
+        )
+        self.validate_requisition(
+            requisition, "csf_culture_assay_datetime", self.csf_culture_panel
+        )
