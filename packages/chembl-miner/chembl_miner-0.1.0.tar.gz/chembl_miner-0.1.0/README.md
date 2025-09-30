3a369c9c1d7b7ed727875e5fffd49b6ae2bf3e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:55 +0000

    Add package chembl-miner/0.1.0/chembl_miner-0.1.0.tar.gz

---
+++ packages/chembl-miner/chembl_miner-0.1.0.tar.gz/chembl_miner-0.1.0/README.md
@@ -0,0 +1,5 @@
+ROADMAP:
+-implementation on generic algorithm for hyperparameter optimization
+-implementation of additional outlier detection methods
+-implementation of additional feature selection methods
+-implementation of classifier pipeline
---
+++ packages/chembl-miner/chembl_miner-0.1.0.tar.gz/chembl_miner-0.1.0/src/chembl_miner/__init__.py
@@ -0,0 +1,8 @@
+from .analysis import DataExplainer, ModelExplainer
+from .data_preprocessing import preprocess_data
+from .data_retrieval import get_activity_data, review_assays, filter_by_assay
+from .datasets import DatasetWrapper, DeployDatasetWrapper
+from .feature_engineering import calculate_fingerprint
+from .machine_learning import MLWrapper
+from .utils import set_verbosity
+
