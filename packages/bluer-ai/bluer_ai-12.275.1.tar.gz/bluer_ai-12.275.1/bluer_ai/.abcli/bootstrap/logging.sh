4a11e2d04f5ebfe8baeef7959399cb45c29c0592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:11 +0000

    Add package bluer-ai/12.275.1/bluer_ai-12.275.1.tar.gz

---
+++ packages/bluer-ai/bluer_ai-12.275.1.tar.gz/bluer_ai-12.275.1/bluer_ai/.abcli/bootstrap/logging.sh
@@ -0,0 +1,74 @@
+#! /usr/bin/env bash
+
+function bluer_ai_log() {
+    local task=$1
+
+    if [[ "$task" == "rm" ]]; then
+        rm -v $bluer_ai_log_filename
+        return
+    fi
+
+    if [[ "$task" == "verbose" ]]; then
+        local what=${2:-on}
+
+        if [ "$what" == "on" ]; then
+            touch $abcli_path_git/verbose
+            bluer_ai_set_log_verbosity
+        elif [ "$what" == "off" ]; then
+            rm $abcli_path_git/verbose
+            bluer_ai_set_log_verbosity
+        else
+            bluer_ai_log_error "@log: verbose: $what: command not found."
+            return 1
+        fi
+
+        return
+    fi
+
+    if [[ "$task" == "watch" ]]; then
+        bluer_ai_watch \
+            seconds=5,$2 \
+            cat \
+            $bluer_ai_log_filename
+
+        return
+    fi
+
+    bluer_ai_log_local "$@"
+
+    bluer_ai_log_remote "$@"
+}
+
+function bluer_ai_log_error() {
+    local message="$@"
+
+    printf "❗️ ${RED}$message$NC\n"
+
+    echo "error: $message" >>$bluer_ai_log_filename
+}
+
+function bluer_ai_log_remote() {
+    echo "$@" >>$bluer_ai_log_filename
+}
+
+function bluer_ai_log_warning() {
+    local message="$@"
+
+    printf "$YELLOW$message$NC\n"
+
+    echo "warning: $message" >>$bluer_ai_log_filename
+}
+
+function bluer_ai_set_log_verbosity() {
+    if [[ -f $abcli_path_git/verbose ]]; then
+        set -x
+    else
+        set +x
+    fi
+}
+
+bluer_ai_set_log_verbosity
+
+if [ -z "$bluer_ai_log_filename" ]; then
+    export bluer_ai_log_filename=$abcli_path_git/bluer_ai.log
+fi
