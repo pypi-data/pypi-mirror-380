c91ce2c93b7096c70a48e45dd039b2050e157304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:00 +0000

    Add package scikit-play/0.1.2/scikit_play-0.1.2-py3-none-any.whl

---
+++ packages/scikit-play/scikit_play-0.1.2-py3-none-any.whl/skplay/transformer_functions.py
@@ -0,0 +1,24 @@
+import polars as pl
+import narwhals as nw
+
+
+def datetime_feats(dataf, column, kind="days"):
+    nw_df = nw.from_native(dataf)
+    if kind == "days":
+        nw_out = nw_df.with_columns(day_of_year=nw.col(column).dt.ordinal_day()).select(
+            "_dt_feat"
+        )
+    if kind == "hours":
+        nw_out = nw_df.with_columns(hour_of_day=nw.col(column).dt.hour()).select(
+            "_dt_feat"
+        )
+    if kind == "minutes":
+        nw_out = nw_df.with_columns(minute_of_hour=nw.col(column).dt.hour()).select(
+            "_dt_feat"
+        )
+
+    return nw.to_native(nw_out)
+
+
+def column_pluck(dataf, column):
+    return pl.DataFrame(dataf)[column].to_list()
