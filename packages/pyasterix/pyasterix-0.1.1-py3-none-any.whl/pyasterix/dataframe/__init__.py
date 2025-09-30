62a1319edeca605ba2accb3eb5abe483b4bc2bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:15:15 +0000

    Add package pyasterix/0.1.1/pyasterix-0.1.1-py3-none-any.whl

---
+++ packages/pyasterix/pyasterix-0.1.1-py3-none-any.whl/pyasterix/dataframe/__init__.py
@@ -0,0 +1,4 @@
+from .base import AsterixDataFrame
+from .attribute import AsterixAttribute, AsterixPredicate
+
+__all__ = ['AsterixDataFrame', 'AsterixAttribute', 'AsterixPredicate', 'AsterixQueryBuilder']
