f31f0c342b114d89493f40d26d803f0b0e8bd0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:10 +0000

    Add package sa-tenders/0.0.9/sa_tenders-0.0.9-py3-none-any.whl

---
+++ packages/sa-tenders/sa_tenders-0.0.9-py3-none-any.whl/sa_tenders/__init__.py
@@ -0,0 +1,12 @@
+'''
+Programmer: Ntuthuko Hlela
+Goal: Deploying the sa_tenders package
+'''
+
+from .main import sa_tenders
+
+'''
+import data_cleaning
+import dict_to_dataframe
+import adding_variables
+'''
