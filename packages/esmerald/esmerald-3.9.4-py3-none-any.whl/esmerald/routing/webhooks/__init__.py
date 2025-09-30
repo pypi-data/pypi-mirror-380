24042c019216151be179e566cf5684f1d7024267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:06:07 +0000

    Add package esmerald/3.9.4/esmerald-3.9.4-py3-none-any.whl

---
+++ packages/esmerald/esmerald-3.9.4-py3-none-any.whl/esmerald/routing/webhooks/__init__.py
@@ -0,0 +1,13 @@
+from .handlers import whdelete, whget, whhead, whoptions, whpatch, whpost, whput, whroute, whtrace
+
+__all__ = [
+    "whdelete",
+    "whhead",
+    "whget",
+    "whoptions",
+    "whpatch",
+    "whpost",
+    "whput",
+    "whroute",
+    "whtrace",
+]
