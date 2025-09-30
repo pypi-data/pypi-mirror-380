a1e48ec9c26732de8b5e2ff966df4b0d2ac1cf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:06:02 +0000

    Add package ordeq_requests/1.0.1/ordeq_requests-1.0.1-py3-none-any.whl

---
+++ packages/ordeq_requests/ordeq_requests-1.0.1-py3-none-any.whl/ordeq_requests/io/__init__.py
@@ -0,0 +1,8 @@
+from ordeq_requests.io.response import (
+    Response,
+    ResponseContent,
+    ResponseJSON,
+    ResponseText,
+)
+
+__all__ = ("Response", "ResponseContent", "ResponseJSON", "ResponseText")
