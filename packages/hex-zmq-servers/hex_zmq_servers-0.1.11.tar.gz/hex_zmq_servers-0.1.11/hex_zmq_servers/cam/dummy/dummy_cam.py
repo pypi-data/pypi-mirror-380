2403282c065b8439dd6179d74c5a5de2215c5798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:47 +0000

    Add package hex-zmq-servers/0.1.11/hex_zmq_servers-0.1.11.tar.gz

---
+++ packages/hex-zmq-servers/hex_zmq_servers-0.1.11.tar.gz/hex_zmq_servers-0.1.11/hex_zmq_servers/cam/dummy/dummy_cam.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+################################################################
+# Copyright 2025 Dong Zhaorui. All rights reserved.
+# Author: Dong Zhaorui 847235539@qq.com
+# Date  : 2025-09-12
+################################################################
+
+import numpy as np
+
+from ..cam_base import HexCamBase
+from ...zmq_base import (
+    MAX_SEQ_NUM,
+    hex_zmq_ts_now,
+    HexRate,
+    HexSafeValue,
+)
+
+
+class HexDummyCam(HexCamBase):
+
+    def __init__(self, params_config: dict = {}):
+        HexCamBase.__init__(self)
+        self._working.set()
+
+    def __del__(self):
+        HexCamBase.__del__(self)
+
+    def work_loop(self, hex_values: list[HexSafeValue]):
+        rgb_value = hex_values[0]
+        depth_value = hex_values[1]
+
+        rgb_count = 0
+        depth_count = 0
+        rate = HexRate(60)
+        while self._working.is_set():
+            # rgb
+            rgb_img = np.random.randint(
+                0,
+                255,
+                (480, 640, 3),
+                dtype=np.uint8,
+            )
+            rgb_value.set((hex_zmq_ts_now(), rgb_count, rgb_img))
+            rgb_count = (rgb_count + 1) % MAX_SEQ_NUM
+
+            # depth
+            depth_img = np.random.randint(
+                0,
+                65535,
+                (480, 640),
+                dtype=np.uint16,
+            )
+            depth_value.set((hex_zmq_ts_now(), depth_count, depth_img))
+            depth_count = (depth_count + 1) % MAX_SEQ_NUM
+
+            # sleep
+            rate.sleep()
+
+    def close(self):
+        self._working.clear()
