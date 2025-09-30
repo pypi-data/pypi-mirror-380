ab2cbe0860d3d480fecc3033f1e42153c6acb8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:39 +0000

    Add package edc-microscopy/1.2.0/edc_microscopy-1.2.0.tar.gz

---
+++ packages/edc-microscopy/edc_microscopy-1.2.0.tar.gz/edc_microscopy-1.2.0/src/edc_microscopy/choices.py
@@ -0,0 +1,7 @@
+from edc_constants.constants import MICROSCOPY, NOT_APPLICABLE, RAPID_TEST
+
+MALARIA_TEST_CHOICES = (
+    (RAPID_TEST, "Rapid test"),
+    (MICROSCOPY, "Microscopy"),
+    (NOT_APPLICABLE, "Not applicable"),
+)
---
+++ packages/edc-microscopy/edc_microscopy-1.2.0.tar.gz/edc_microscopy-1.2.0/src/edc_microscopy/form_validators/malaria_test_form_validator.py
@@ -0,0 +1,11 @@
+from edc_constants.constants import NO, YES
+from edc_crf.crf_form_validator import CrfFormValidator
+
+
+class MalariaTestFormValidator(CrfFormValidator):
+    def clean(self):
+        self.applicable_if(YES, field="performed", field_applicable="diagnostic_type")
+
+        self.required_if(NO, field="performed", field_required="not_performed_reason")
+
+        self.applicable_if(YES, field="performed", field_applicable="result")
