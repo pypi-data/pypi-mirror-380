0e3aed9d19e82ab407a86457042f04c2b8cfc777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:22 +0000

    Add package datacleanerx/0.2.1/datacleanerx-0.2.1.tar.gz

---
+++ packages/datacleanerx/datacleanerx-0.2.1.tar.gz/datacleanerx-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="datacleanerx",
+    version="0.2.1",
+    author="Satyam Singh",
+    author_email="satyamsingh7734@example.com",
+    description="Automated dataset cleaner for machine learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SatyamSingh8306/datacleanerx",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pandas>=1.3.0",
+        "numpy>=1.20.0",
+        "scikit-learn>=1.0.0",
+    ],
+)
