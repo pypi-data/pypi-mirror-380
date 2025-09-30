84141a7952ea4c32889c13d8241ed6e6ba4bf65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:05:22 +0000

    Add package omlish/0.0.0.dev451/omlish-0.0.0.dev451-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev451-py3-none-any.whl/omlish/marshal/polymorphism/standard.py
@@ -0,0 +1,20 @@
+from ..base.types import MarshalerFactory
+from ..base.types import UnmarshalerFactory
+from .marshal import PolymorphismMarshalerFactory
+from .metadata import Polymorphism
+from .metadata import TypeTagging
+from .metadata import WrapperTypeTagging
+from .unmarshal import PolymorphismUnmarshalerFactory
+
+
+##
+
+
+def standard_polymorphism_factories(
+        poly: Polymorphism,
+        tt: TypeTagging = WrapperTypeTagging(),
+) -> tuple[MarshalerFactory, UnmarshalerFactory]:
+    return (
+        PolymorphismMarshalerFactory(poly, tt),
+        PolymorphismUnmarshalerFactory(poly, tt),
+    )
