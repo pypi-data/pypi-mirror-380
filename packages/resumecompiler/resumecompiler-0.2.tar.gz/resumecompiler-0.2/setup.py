bdfcf470543a1560e476be5db8a739051794cf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:14:01 +0000

    Add package resumecompiler/0.2/resumecompiler-0.2.tar.gz

---
+++ packages/resumecompiler/resumecompiler-0.2.tar.gz/resumecompiler-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='resumecompiler',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'markdown',
+        'beautifulsoup4',
+    ],
+    entry_points={
+        'console_scripts': [
+            # 'resume-compiler=resumecompiler.resumecompiler:main',  # if applicable
+        ],
+    },
+    author='Raphael Li',
+    description='A compiler that compiles Markdown files into a resume PDF file, formatted in LaTeX.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
