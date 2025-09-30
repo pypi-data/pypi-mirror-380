fbd68b23e2daf96ee5ef0f9ff0f282c48a8f96cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:53 +0000

    Add package clinicedc/2.0.26/clinicedc-2.0.26-py3-none-any.whl

---
+++ packages/clinicedc/clinicedc-2.0.26-py3-none-any.whl/edc_auth/admin/group_admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
+from edc_auth.admin_site import edc_auth_admin
+from edc_model_admin.mixins import TemplatesModelAdminMixin
+
+admin.site.unregister(Group)
+
+
+@admin.register(Group, site=edc_auth_admin)
+class GroupAdmin(TemplatesModelAdminMixin, admin.ModelAdmin):
+    ordering = ("name",)
+
+    list_display_links = ("name", "codenames")
+
+    list_display = ("name", "codenames")
+
+    list_filter = (
+        "name",
+        "permissions__codename",
+    )
+
+    search_fields = (
+        "name",
+        "permissions__codename",
+    )
+
+    @staticmethod
+    def codenames(obj=None) -> str:
+        codenames = []
+        for permission in obj.permissions.all():
+            codenames.append(permission.codename)
+        codenames.sort()
+        return format_html("{}", mark_safe("<BR>".join(codenames)))  # nosec B703 B308
