a138ac0029651592ff970901a7f7bf85ea77b18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 16:09:21 +0000

    Add package sparams/4.0.0/sparams-4.0.0.tar.gz

---
+++ packages/sparams/sparams-4.0.0.tar.gz/sparams-4.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sparams',
+    version='4.0.0',
+    description='A simple config watcher ',
+    author='PAT',
+    packages=find_packages(),
+    install_requires=[
+        'watchdog',
+        'pyyaml',
+        'numpy',  
+    ],
+    python_requires='>=3.8',
+    long_description = open("README", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
