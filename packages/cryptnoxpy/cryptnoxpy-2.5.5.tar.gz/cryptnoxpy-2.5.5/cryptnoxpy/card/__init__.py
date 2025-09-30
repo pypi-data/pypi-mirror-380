7e012bf3483c58e7b2b17fe49418cb41f6d5715f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 04:05:22 +0000

    Add package cryptnoxpy/2.5.5/cryptnoxpy-2.5.5.tar.gz

---
+++ packages/cryptnoxpy/cryptnoxpy-2.5.5.tar.gz/cryptnoxpy-2.5.5/cryptnoxpy/card/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Module that contains classes for various cryptnox cards.
+"""
+from .base import Base
+from .basic_g0 import BasicG0
+from .basic_g1 import BasicG1
+from .nft import Nft
---
+++ packages/cryptnoxpy/cryptnoxpy-2.5.5.tar.gz/cryptnoxpy-2.5.5/setup.py
@@ -0,0 +1,25 @@
+"""
+Configuration for setting up CryptnoxPy as external library
+"""
+import pathlib
+import sys
+
+from setuptools import setup
+
+
+def read(file):
+    return (pathlib.Path(__file__).parent / file).read_text("utf-8").strip()
+
+
+PYSCARD = "pyscard"
+if sys.platform.startswith("win"):
+    PYSCARD += "==2.1.1"
+
+setup(
+    install_requires=[
+        "aiohttp",
+        "cryptography",
+        PYSCARD
+    ],
+    long_description="\n\n".join((read("README.rst"), read("CHANGELOG.rst"))),
+)
