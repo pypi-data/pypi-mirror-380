f930250eb6dbb20c93fd55d99316a59966a7bd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 18:05:57 +0000

    Add package machineconfig/5.13/machineconfig-5.13.tar.gz

---
+++ packages/machineconfig/machineconfig-5.13.tar.gz/machineconfig-5.13/src/machineconfig/scripts/python/count_lines_frontend.py
@@ -0,0 +1,16 @@
+
+import typer
+
+
+def analyze_repo_development(repo_path: str = typer.Argument(..., help="Path to the git repository")):
+    from machineconfig.scripts.python import count_lines
+    from pathlib import Path
+    count_lines_path = Path(count_lines.__file__).resolve().parent.joinpath("count_lines.py")
+    # --project $HOME/code/machineconfig 
+    cmd = f"""uv run --python 3.13 --with machineconfig --group plot {count_lines_path} analyze-over-time {repo_path}"""
+    from machineconfig.utils.code import run_script
+    run_script(cmd)
+
+
+if __name__ == "__main__":
+    pass
