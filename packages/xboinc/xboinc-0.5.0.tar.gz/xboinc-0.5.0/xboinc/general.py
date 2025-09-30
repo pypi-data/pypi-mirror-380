9d615a87b345868f9ed45776552759d0e1bbae62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:14:39 +0000

    Add package xboinc/0.5.0/xboinc-0.5.0.tar.gz

---
+++ packages/xboinc/xboinc-0.5.0.tar.gz/xboinc-0.5.0/xboinc/general.py
@@ -0,0 +1,30 @@
+# copyright ############################### #
+# This file is part of the Xboinc Package.  #
+# Copyright (c) CERN, 2025.                 #
+# ######################################### #
+
+from pathlib import Path
+
+_pkg_root = Path(__file__).parent.absolute()
+
+
+# ==============================================================================
+# Do not change
+# ==============================================================================
+
+__version__ = '0.5.0'
+
+# These are the xsuite modules that are used by boinc and the versions they are
+# tied to. This will be automatically updated from the active environment when
+# making a minor release. If a new package needs to be pinned, add it here with
+# a random version number, and similarily in the pyproject.toml
+__xsuite__versions__ = {
+    'xobjects' : '0.5.0',
+    'xdeps'    : '0.10.5',
+    'xpart'    : '0.23.0',
+    'xtrack'   : '0.84.7',
+    'xfields'  : '0.24.0',
+    'xcoll'    : '0.6.1',
+    'xaux'     : '0.3.5',
+}
+# ==============================================================================
