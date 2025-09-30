4d6586298f4ec4cf0e84215dda24e6df51deab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:14:57 +0000

    Add package eodc-connect/0.0.9/eodc_connect-0.0.9-py3-none-any.whl

---
+++ packages/eodc-connect/eodc_connect-0.0.9-py3-none-any.whl/eodc_connect/dask.py
@@ -0,0 +1,51 @@
+import logging
+from typing import Optional
+
+from dask_gateway import Gateway, GatewayCluster
+from eodc_connect import auth, settings
+
+logger = logging.getLogger(__name__)
+
+
+class EODCDaskGateway(Gateway):
+    def __init__(self, username: str = None) -> None:
+        self.authenticator = auth.DaskOIDC(username=username)
+        super().__init__(
+            address=settings.DASK_URL,
+            proxy_address=settings.DASK_URL_TCP,
+            auth=self.authenticator,
+        )
+
+    @classmethod
+    def from_token(cls, token: str, auth_extra: str = None):
+        instance = cls.__new__(cls)
+        instance.auth = auth.DaskOIDC.from_token(token, auth_extra)
+        instance.address = settings.DASK_URL
+        instance.proxy_address = settings.DASK_URL_TCP
+        instance._public_address = instance.address
+        instance._asynchronous = False
+        instance._loop_runner.loop = None
+        return instance
+
+    def __new__(cls, *args, **kwargs):
+        """There should only ever be one Gateway object instantiated -> singleton."""
+        if not hasattr(cls, "instance"):
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+
+class EODCCluster(GatewayCluster):
+    def __init__(self, image: Optional[str] = None):
+        self.gateway = EODCDaskGateway()
+
+        cluster_options = self.gateway.cluster_options(use_local_defaults=False)
+        if image is not None:
+            cluster_options.image = image
+        logger.info(f"Provisioning Dask cluster from {self.gateway.address}")
+
+        super().__init__(
+            address=self.gateway.address,
+            cluster_options=cluster_options,
+            shutdown_on_close=True,
+        )
+        logger.info(f"Initialised Dask Cluster at {self.scheduler_address}")
