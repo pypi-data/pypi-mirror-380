786db65b8b9de1a1909d06509459598bbe72f775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:05:54 +0000

    Add package gamengine2d/1.0.8/gamengine2d-1.0.8-py3-none-any.whl

---
+++ packages/gamengine2d/gamengine2d-1.0.8-py3-none-any.whl/gamengine2d/__init__.py
@@ -0,0 +1,12 @@
+from .engine import Engine, SettingsWindow
+from .objects import (
+    GameObject,
+    Rectangle,
+    RectangleOutline,
+    Circle,
+    Line,
+    Polygon,
+    Ellipse
+)
+
+from .helper import Script, Color, vector2d, Camera
