1cf3542cf711a422d325985f68cb4367565ad09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 07:05:36 +0000

    Add package django-tree/0.6.2/django_tree-0.6.2.tar.gz

---
+++ packages/django-tree/django_tree-0.6.2.tar.gz/django_tree-0.6.2/tree/query.py
@@ -0,0 +1,66 @@
+import operator
+from functools import reduce
+
+from django.core.exceptions import FieldDoesNotExist
+from django.db.models import QuerySet, Q
+from django.db.models.manager import Manager
+
+from .fields import PathField
+
+
+def _get_path_fields(model, name=None):
+    if name is None:
+        return [f for f in model._meta.fields if isinstance(f, PathField)]
+    return [model._meta.get_field(name)]
+
+
+def _get_path_field(model, name):
+    path_fields = _get_path_fields(model, name)
+    n = len(path_fields)
+    if n == 0:
+        raise FieldDoesNotExist(
+            'A `PathField` needs to be defined in order to use `TreeModelMixin`.'
+        )
+    if n == 1:
+        return path_fields[0]
+    raise ValueError(
+        'You need to specify which `PathField` to use for this query '
+        'among these values: %s' % [f.name for f in path_fields]
+    )
+
+
+# TODO: Implement a faster `QuerySet.delete` and add it to the benchmark.
+class TreeQuerySetMixin:
+    def _get_path_field_attname(self, name):
+        return _get_path_field(self.model, name).attname
+
+    def filter_roots(self, path_field=None):
+        attname = self._get_path_field_attname(path_field)
+        return self.filter(**{f'{attname}__level': 1})
+
+    def get_descendants(self, include_self=False, path_field=None):
+        attname = self._get_path_field_attname(path_field)
+        # TODO: Avoid doing an extra query.
+        ancestor_paths = list(self.values_list(attname, flat=True))
+        queryset = self.model.objects.all()
+        if not ancestor_paths:
+            return queryset.none()
+        if not include_self:
+            queryset = queryset.exclude(**{attname + '__in': ancestor_paths})
+        return queryset.filter(
+            reduce(
+                operator.or_,
+                [
+                    Q(**{attname + '__descendant_of': ancestor_path})
+                    for ancestor_path in ancestor_paths
+                ],
+            )
+        )
+
+
+class TreeQuerySet(TreeQuerySetMixin, QuerySet):
+    pass
+
+
+class TreeManager(Manager.from_queryset(TreeQuerySet)):
+    pass
---
+++ packages/django-tree/django_tree-0.6.2.tar.gz/django_tree-0.6.2/tree/types.py
@@ -0,0 +1,220 @@
+from importlib.util import find_spec
+from django.db.models import QuerySet
+
+
+class Path:
+    def __init__(self, field, value):
+        self.field = field
+        self.attname = getattr(self.field, 'attname', None)
+        self.field_bound = self.attname is not None
+        self.qs = (
+            self.field.model._default_manager.all() if self.field_bound else QuerySet()
+        )
+        self.value = value
+
+    def __repr__(self):
+        if self.field_bound:
+            return '<Path %s %s>' % (self.field, self.value)
+        return '<Path %s>' % self.value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        if isinstance(other, Path):
+            other = other.value
+        return self.value == other
+
+    def __ne__(self, other):
+        if isinstance(other, Path):
+            other = other.value
+        return self.value != other
+
+    def __lt__(self, other):
+        # We simulate the effects of a NULLS LAST.
+        if not self.value:
+            return False
+        if isinstance(other, Path):
+            other = other.value
+        if not other:
+            return True
+        return self.value < other
+
+    def __le__(self, other):
+        # We simulate the effects of a NULLS LAST.
+        if not self.value:
+            return False
+        if isinstance(other, Path):
+            other = other.value
+        if not other:
+            return True
+        return self.value <= other
+
+    def __gt__(self, other):
+        # We simulate the effects of a NULLS LAST.
+        if not self.value:
+            return True
+        if isinstance(other, Path):
+            other = other.value
+        if not other:
+            return False
+        return self.value > other
+
+    def __ge__(self, other):
+        # We simulate the effects of a NULLS LAST.
+        if not self.value:
+            return True
+        if isinstance(other, Path):
+            other = other.value
+        if not other:
+            return False
+        return self.value >= other
+
+    def __iter__(self):
+        return iter(self.value)
+
+    def get_children(self):
+        if not self.value:
+            return self.qs.none()
+        return self.qs.filter(
+            **{
+                f'{self.attname}__child_of': self.value,
+            }
+        )
+
+    def get_ancestors(self, include_self=False):
+        if not self.value or (self.is_root() and not include_self):
+            return self.qs.none()
+        path = self.value
+        if not include_self:
+            path = path[:-1]
+        # Using the lookup `ancestor_of` here is slower,
+        # so we explicitly specify the ancestors’ paths.
+        return self.qs.filter(
+            **{self.attname + '__in': [path[:i] for i in range(1, len(path) + 1)]}
+        )
+
+    def get_descendants(self, include_self=False):
+        if not self.value:
+            return self.qs.none()
+        # Using the lookup `descendant_of` here is slower,
+        # so we explicitly specify the path slicing comparison.
+        qs = self.qs.filter(
+            **{
+                f'{self.attname}__0_{len(self.value)}': self.value,
+            }
+        )
+        if include_self:
+            return qs
+        return qs.filter(**{f'{self.attname}__level__gt': len(self.value)})
+
+    def get_siblings(self, include_self=False, queryset=None):
+        if not self.value:
+            return self.qs.none()
+
+        qs = self.qs if queryset is None else queryset
+        qs = qs.filter(
+            **{
+                self.attname + '__sibling_of': self.value,
+            }
+        )
+        if include_self:
+            return qs
+        return qs.exclude(**{self.attname: self.value})
+
+    def get_prev_siblings(self, include_self=False, queryset=None):
+        if not self.value:
+            return self.qs.none()
+        siblings = self.get_siblings(include_self=include_self, queryset=queryset)
+        lookup = '__lte' if include_self else '__lt'
+        return siblings.filter(**{self.attname + lookup: self.value}).order_by(
+            '-' + self.attname
+        )
+
+    def get_next_siblings(self, include_self=False, queryset=None):
+        if not self.value:
+            return self.qs.none()
+        siblings = self.get_siblings(include_self=include_self, queryset=queryset)
+        lookup = '__gte' if include_self else '__gt'
+        return siblings.filter(**{self.attname + lookup: self.value}).order_by(
+            self.attname
+        )
+
+    def get_prev_sibling(self, queryset=None):
+        if not self.value:
+            return
+
+        return self.get_prev_siblings(queryset=queryset).first()
+
+    def get_next_sibling(self, queryset=None):
+        if not self.value:
+            return None
+
+        return self.get_next_siblings(queryset=queryset).first()
+
+    def get_level(self):
+        if self.value:
+            return len(self.value)
+
+    def is_root(self):
+        if self.value:
+            return len(self.value) == 1
+
+    def is_leaf(self):
+        if self.value:
+            return not self.get_children().exists()
+
+    def is_ancestor_of(self, other, include_self=False):
+        if not self.value:
+            return False
+        if isinstance(other, Path):
+            other = other.value
+        if not other:
+            return False
+        if not isinstance(other, list):
+            raise TypeError('`other` must be a `Path` instance or a list of decimals.')
+        if not include_self and self.value == other:
+            return False
+        return other[: len(self.value)] == self.value
+
+    def is_descendant_of(self, other, include_self=False):
+        if not self.value:
+            return False
+        if isinstance(other, Path):
+            other = other.value
+        if not other:
+            return False
+        if not isinstance(other, list):
+            raise TypeError('`other` must be a `Path` instance or a list of decimals.')
+        if not include_self and self.value == other:
+            return False
+        return self.value[: len(other)] == other
+
+    @staticmethod
+    def register_psycopg2():
+        from psycopg2.extensions import register_adapter, adapt
+
+        def adapt_path(path):
+            return adapt(path.value)
+
+        register_adapter(Path, adapt_path)
+
+    @staticmethod
+    def register_psycopg3():
+        import psycopg
+        from psycopg.types.string import StrDumper
+
+        class PathDumper(StrDumper):
+            def quote(self, obj: Path):
+                return psycopg.sql.quote(obj.value).encode()
+
+        psycopg.adapters.register_dumper(Path, PathDumper)
+
+    @classmethod
+    def register_psycopg(cls):
+        # Tells psycopg how to prepare a Path object for the database,
+        # in case it doesn't go through the ORM.
+        if find_spec('psycopg') is not None:
+            return cls.register_psycopg3()
+        if find_spec('psycopg2') is not None:
+            return cls.register_psycopg2()
