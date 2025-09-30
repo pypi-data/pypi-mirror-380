55b6b6f327993c66bde03679cfbec402e31f3508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:06:34 +0000

    Add package reqman4/0.5.7/reqman4-0.5.7.tar.gz

---
+++ packages/reqman4/reqman4-0.5.7.tar.gz/reqman4-0.5.7/src/reqman4/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2025 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/reqman4
+# #############################################################################
+from . import main
+main.command()
