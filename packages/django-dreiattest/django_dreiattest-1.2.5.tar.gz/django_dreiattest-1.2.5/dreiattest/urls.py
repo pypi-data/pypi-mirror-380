9455c5f2a20f4e1cb98dd4a6e23acf925a29366d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:06:33 +0000

    Add package django-dreiattest/1.2.5/django_dreiattest-1.2.5.tar.gz

---
+++ packages/django-dreiattest/django_dreiattest-1.2.5.tar.gz/django_dreiattest-1.2.5/dreiattest/urls.py
@@ -0,0 +1,17 @@
+from . import settings as dreiattest_settings, views
+
+from django.urls import re_path
+
+app_name = "dreiattest"
+urlpatterns = [
+    re_path(
+        dreiattest_settings.DREIATTEST_BASE_URL + "nonce",
+        views.nonce,
+        name="dreiattest.nonce",
+    ),
+    re_path(
+        dreiattest_settings.DREIATTEST_BASE_URL + "key",
+        views.key,
+        name="dreiattest.key",
+    ),
+]
