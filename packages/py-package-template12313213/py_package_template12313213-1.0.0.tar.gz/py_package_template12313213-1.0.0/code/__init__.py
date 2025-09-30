6c78fb807d76297fae1ba1ab7198f4c65d8f2935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:06:28 +0000

    Add package py-package-template12313213/1.0.0/py_package_template12313213-1.0.0.tar.gz

---
+++ packages/py-package-template12313213/py_package_template12313213-1.0.0.tar.gz/py_package_template12313213-1.0.0/code/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2022 Thinh Vu @ GitHub
+# See LICENSE for details.
+
+__author__ = "Vu @thinh-vu in GitHub"
+__version__ = "1.0.0"
+
+from .gadget import *
+from .datetime_intel import *
