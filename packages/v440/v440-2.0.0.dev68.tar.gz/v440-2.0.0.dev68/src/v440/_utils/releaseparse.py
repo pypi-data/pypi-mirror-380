9f5933395e32182ca5663154ea89fcbaad2b756d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:05:10 +0000

    Add package v440/2.0.0.dev68/v440-2.0.0.dev68.tar.gz

---
+++ packages/v440/v440-2.0.0.dev68.tar.gz/v440-2.0.0.dev68/src/v440/_utils/releaseparse.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import string
+from typing import *
+
+from v440._utils.Digest import Digest
+from v440.core.VersionError import VersionError
+
+
+def numeral(value: Any, /) -> int:
+    v: Any = segment(value)
+    if type(v) is int:
+        return v
+    e: str = "%r is not a valid numeral segment"
+    e %= v
+    raise VersionError(e)
+
+
+def segment(value: Any, /) -> Any:
+    try:
+        return _segment(value)
+    except:
+        e: str = "%r is not a valid segment"
+        e = VersionError(e % value)
+        raise e from None
+
+
+_segment: Digest = Digest("_segment")
+
+
+@_segment.overload()
+def _segment():
+    return
+
+
+@_segment.overload(int)
+def _segment(value: int, /) -> Any:
+    if value < 0:
+        raise ValueError
+    return value
+
+
+@_segment.overload(str)
+def _segment(value: Any, /) -> int | str:
+    if value.strip(string.ascii_lowercase + string.digits):
+        raise ValueError(value)
+    if value.strip(string.digits):
+        return value
+    elif value == "":
+        return 0
+    else:
+        return int(value)
