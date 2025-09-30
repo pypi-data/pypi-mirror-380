9cb667cfd8e4214767e05e9072311bcf1044df1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 05:13:40 +0000

    Add package paar-dev/1.0.0/paar_dev-1.0.0.tar.gz

---
+++ packages/paar-dev/paar_dev-1.0.0.tar.gz/paar_dev-1.0.0/README.md
@@ -0,0 +1,7 @@
+# paar_dev
+
+Biblioteca Python de exemplo chamada **paar_dev**.
+
+## Instalação
+```bash
+pip install paar-dev
---
+++ packages/paar-dev/paar_dev-1.0.0.tar.gz/paar_dev-1.0.0/src/paar_dev/__init__.py
@@ -0,0 +1,25 @@
+from .core import (
+    gerar_df_phoenix,
+    abrir_planilha,
+    tratar_colunas_df,
+    gsheet_to_df,
+    plotar_tabela_aggrid_colher_selecao,
+    tratar_colunas_df_para_insercao_gsheet,
+    salvar_dados_bigquery,
+    carregar_dados_bigquery,
+    grafico_linhas,
+    identificar_valor_minimo_maximo,
+)
+
+__all__ = [
+    "gerar_df_phoenix",
+    "abrir_planilha",
+    "tratar_colunas_df",
+    "gsheet_to_df",
+    "plotar_tabela_aggrid_colher_selecao",
+    "tratar_colunas_df_para_insercao_gsheet",
+    "salvar_dados_bigquery",
+    "carregar_dados_bigquery",
+    "grafico_linhas",
+    "identificar_valor_minimo_maximo",
+]
