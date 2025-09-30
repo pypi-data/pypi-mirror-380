b2d9b67ff171f363306bad12727dfe69352af6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 16:06:56 +0000

    Add package mesomath/1.1.0/mesomath-1.1.0.tar.gz

---
+++ packages/mesomath/mesomath-1.1.0.tar.gz/mesomath-1.1.0/mesomath/babcalc.py
@@ -0,0 +1,20 @@
+from mesomath.babn import BabN as bn
+from mesomath.npvs import Bbri as bb
+from mesomath.npvs import Bcap as bc
+from mesomath.npvs import Blen as bl
+from mesomath.npvs import Bsur as bs
+from mesomath.npvs import BsyG as bG
+from mesomath.npvs import BsyS as bS
+from mesomath.npvs import Bvol as bv
+from mesomath.npvs import Bwei as bw
+
+message = """\nWelcome to Babylonian Calculator v1.1.0
+    ...the calculator that every scribe should have!
+
+Use: bn(number) for sexagesimal calculations
+Metrological classes: bl, bs, bv, bc, bw, bG, bS and bb loaded.
+
+jccsvq fecit, 2025."""
+
+
+print(message)
