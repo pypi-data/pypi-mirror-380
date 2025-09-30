3b2af5107508bda973ac118f1d5d68bbb75b205a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:15:10 +0000

    Add package django-tabbed-changeform-admin/0.1.8/django_tabbed_changeform_admin-0.1.8-py3-none-any.whl

---
+++ packages/django-tabbed-changeform-admin/django_tabbed_changeform_admin-0.1.8-py3-none-any.whl/django_tabbed_changeform_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoTabbedChangeformAdminConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_tabbed_changeform_admin"
