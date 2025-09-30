4dee6cdc5541dc16d6238a4da1ca4b6d1f9e9f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:05:00 +0000

    Add package meta-edc/1.1.18/meta_edc-1.1.18-py3-none-any.whl

---
+++ packages/meta-edc/meta_edc-1.1.18-py3-none-any.whl/meta_rando/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from edc_constants.choices import GENDER
+from edc_model import models as edc_models
+from edc_randomization.model_mixins import RandomizationListModelMixin
+
+
+class RandomizationList(RandomizationListModelMixin, edc_models.BaseUuidModel):
+    gender = models.CharField(max_length=25, choices=GENDER)
+
+    class Meta(RandomizationListModelMixin.Meta, edc_models.BaseUuidModel.Meta):
+        verbose_name = "Randomization List (Phase Three)"
+        verbose_name_plural = "Randomization List (Phase Three)"
