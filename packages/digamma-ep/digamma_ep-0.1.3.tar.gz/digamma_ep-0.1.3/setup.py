3cb284972fa47896aefe8f1119cc1eba9b5ed7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 14:06:14 +0000

    Add package digamma-ep/0.1.3/digamma_ep-0.1.3.tar.gz

---
+++ packages/digamma-ep/digamma_ep-0.1.3.tar.gz/digamma_ep-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digamma-ep',
+    version = "0.1.3",
+    author='Cerene Rúbio',
+    author_email='teu@email.com',
+    description='Symbolic audit framework for model divergence and integrity',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.8',
+)
