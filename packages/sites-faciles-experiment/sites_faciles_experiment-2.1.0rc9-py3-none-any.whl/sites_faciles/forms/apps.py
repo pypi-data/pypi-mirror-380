8b88b97b3d0fbd313a9a555956cf87898f5f4151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 03:15:42 +0000

    Add package sites-faciles-experiment/2.1.0rc9/sites_faciles_experiment-2.1.0rc9-py3-none-any.whl

---
+++ packages/sites-faciles-experiment/sites_faciles_experiment-2.1.0rc9-py3-none-any.whl/sites_faciles/forms/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FormsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"  # type: ignore
+    name = "sites_faciles.forms"
+    label = "sites_faciles_forms"
