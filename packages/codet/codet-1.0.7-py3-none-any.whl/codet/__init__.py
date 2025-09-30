8accceb2a653a7b84c774a1ba94dca598bf82712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 17:05:57 +0000

    Add package codet/1.0.7/codet-1.0.7-py3-none-any.whl

---
+++ packages/codet/codet-1.0.7-py3-none-any.whl/codet/__init__.py
@@ -0,0 +1,14 @@
+"""
+# Codet
+
+codet is a command-line tool for analyzing Git commit history. It helps users profile project change hotspots, analyze code changes, and leverage AI for deeper insights.
+In particular, the generated git diff files can be conveniently integrated with Cursor for collaborative development.
+
+- Analyze recent commit records, defaulting to the past 7 days.
+- Search for keywords in commit diffs.
+- Search for author email in commit diffs.
+- Support for optional AI analysis via tools.
+- Code hotspot analysis to identify frequently modified areas.
+"""
+
+__version__ = "1.0.7" 
