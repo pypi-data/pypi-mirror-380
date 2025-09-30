bc13ecea2aa928d0c8a9579b8a876b1026e6c336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-30 06:05:24 +0000

    Add package similaripy/0.2.3/similaripy-0.2.3.tar.gz

---
+++ packages/similaripy/similaripy-0.2.3.tar.gz/similaripy-0.2.3/similaripy/__init__.py
@@ -0,0 +1,12 @@
+from .normalization import normalize, bm25, bm25plus, tfidf
+from .similarity import (
+    dot_product,
+    cosine,
+    asymmetric_cosine,
+    jaccard,
+    dice,
+    tversky,
+    p3alpha,
+    rp3beta,
+    s_plus,
+) 
