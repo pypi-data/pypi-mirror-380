1fa6424277eb19abdd79338cbef102cb074c75ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:07:25 +0000

    Add package pythonic-fp/3.2.1/pythonic_fp-3.2.1.tar.gz

---
+++ packages/pythonic-fp/pythonic_fp-3.2.1.tar.gz/pythonic_fp-3.2.1/src/pythonic_fp/homepage/top_level_releases.py
@@ -0,0 +1,37 @@
+"""
+Pythonic FP Project Release Dates
+=================================
+
+
+============================ =========================== ========= =============
+PyPI and GitHub Name         Python Module               Version   Date Released
+============================ =========================== ========= =============
+pythonic-fp-booleans         pythonic_fp.booleans        2.0.0     2025-09-27
+pythonic-fp-circulararray    pythonic_fp.circulararray   6.0.0     2025-09-26
+pythonic-fp-containers       pythonic_fp.containers      4.0.0     2025-09-28
+pythonic-fp-fptools          pythonic_fp.fptools         5.1.2     2025-09-28
+pythonic-fp-gadgets          pythonic_fp.gadgets         3.1.0     2025-09-26
+pythonic-fp-iterables        pythonic_fp.iterables       5.1.2     2025-09-28
+pythonic-fp-queues           pythonic_fp.queues          5.1.0     2025-09-26
+pythonic-fp-sentinels [#]_   pythonic_fp.sentinels       2.1.3     2025-08-02
+pythonic-fp-singletons [#]_  pythonic_fp.singletons      1.0.0     2025-09-25
+pythonic-fp-splitends        pythonic_fp.splitends       2.0.0     2025-09-28
+============================ =========================== ========= =============
+
+
+Footnotes
+---------
+
+.. [#]
+
+    Project **pythonic-fp-sentinels** was **DEPRECATED**. Content was moved
+    to pythonic-fp-gadgets.
+
+.. [#]
+
+    Project **pythonic-fp-singletons** was **DEPRECATED**. Its GitHub repo was
+    repurposed for ``pythonic-fp-sentinels`` and the Boolean content was moved
+    to a new PyPI project, ``pythonic-fp-booleans``. Naming a module after an
+    implementation detail turned out not to scale well.
+
+"""
