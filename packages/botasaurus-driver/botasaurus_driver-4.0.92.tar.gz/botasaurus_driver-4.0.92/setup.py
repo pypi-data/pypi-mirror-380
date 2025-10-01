dd79d0ba6a77cf694a73d0e47a7a9ce3d20019ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:11 +0000

    Add package botasaurus-driver/4.0.92/botasaurus_driver-4.0.92.tar.gz

---
+++ packages/botasaurus-driver/botasaurus_driver-4.0.92.tar.gz/botasaurus_driver-4.0.92/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+def get_description():
+    try:
+        with open("README.md", encoding="utf-8") as readme_file:
+            long_description = readme_file.read()
+        return long_description
+    except:
+        return None
+
+setup(
+    name="botasaurus_driver",
+    version='4.0.92',
+    description="Super Fast, Super Anti-Detect, and Super Intuitive Web Driver",
+    long_description_content_type="text/markdown",
+    long_description=get_description(),
+    author="Chetan",
+    author_email="53407137+Chetan11-dev@users.noreply.github.com",
+    maintainer="Chetan",
+    maintainer_email="53407137+Chetan11-dev@users.noreply.github.com",
+    license="MIT",
+    python_requires=">=3.5",
+    keywords=[
+        "webdriver", "browser", "captcha", "web-scraping",
+        "selenium", "navigator", "python3", "cloudflare",
+        "anti-delect", "anti-bot", "bot-detection",
+        "cloudflare-bypass", "distil", "anti-detection"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: Browsers",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    install_requires=[
+        "requests",
+        "deprecated",
+        "psutil",
+        "botasaurus-proxy-authentication",
+        "websocket-client>=1.8.0",
+        "pyvirtualdisplay"
+    ],
+    url="https://github.com/omkarcloud/botasaurus-driver",
+    project_urls={
+        "Homepage": "https://github.com/omkarcloud/botasaurus-driver",
+        "Bug Reports": "https://github.com/omkarcloud/botasaurus-driver/issues",
+        "Source": "https://github.com/omkarcloud/botasaurus-driver"
+    },
+    packages=find_packages(include=["botasaurus_driver"]),
+    include_package_data=True,
+)
