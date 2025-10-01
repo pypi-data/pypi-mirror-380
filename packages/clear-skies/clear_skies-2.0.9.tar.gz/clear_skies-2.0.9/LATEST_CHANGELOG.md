94faba2dec5955229c88167a7c965f36d6b0ff37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:46 +0000

    Add package clear-skies/2.0.9/clear_skies-2.0.9.tar.gz

---
+++ packages/clear-skies/clear_skies-2.0.9.tar.gz/clear_skies-2.0.9/LATEST_CHANGELOG.md
@@ -0,0 +1,10 @@
+## [2.0.9] - 2025-10-01
+
+### Added
+- Add email configs by @tnijboer in [#21](https://github.com/clearskies-py/clearskies/pull/21)
+- Add regex to string_list/string_list_callable and reuse them
+- Add email configs
+
+### Removed
+- Remove extra call, token is already in credentials json in [#20](https://github.com/clearskies-py/clearskies/pull/20)
+
---
+++ packages/clear-skies/clear_skies-2.0.9.tar.gz/clear_skies-2.0.9/src/clearskies/input_outputs/wsgi.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import json
+from typing import Callable
+
+from .input_output import InputOutput
+
+
+class Wsgi(InputOutput):
+    _environment: dict[str, str] = {}
+    _start_response: Callable = None  # type: ignore
+    _request_headers: dict[str, str] = {}
+    _cached_body: str | None = None
+
+    def __init__(self, environment, start_response):
+        self._environment = environment
+        self._start_response = start_response
+        self._request_headers = {}
+        for key, value in self._environment.items():
+            if key.upper()[0:5] == "HTTP_":
+                self._request_headers[key[5:].lower()] = value
+        super().__init__()
+
+    def _from_environment(self, key):
+        return self._environment[key] if key in self._environment else ""
+
+    def respond(self, body, status_code=200):
+        if "content-type" not in self.response_headers:
+            self.response_headers.content_type = "application/json; charset=UTF-8"
+
+        self._start_response(f"{status_code} Ok", [header for header in self.response_headers.items()])  # type: ignore
+        if type(body) == bytes:
+            final_body = body
+        elif type(body) == str:
+            final_body = body.encode("utf-8")
+        else:
+            final_body = json.dumps(body).encode("utf-8")
+        return [final_body]
+
+    def has_body(self):
+        return bool(self._from_environment("CONTENT_LENGTH"))
+
+    def get_body(self):
+        if self._cached_body is None:
+            self._cached_body = (
+                self._from_environment("wsgi.input").read(int(self._from_environment("CONTENT_LENGTH"))).decode("utf-8")
+                if self._from_environment("CONTENT_LENGTH")
+                else ""
+            )
+        return self._cached_body
+
+    def get_request_method(self):
+        return self._from_environment("REQUEST_METHOD").upper()
+
+    def get_script_name(self):
+        return self._from_environment("SCRIPT_NAME")
+
+    def get_path_info(self):
+        return self._from_environment("PATH_INFO")
+
+    def get_query_string(self):
+        return self._from_environment("QUERY_STRING")
+
+    def get_content_type(self):
+        return self._from_environment("CONTENT_TYPE")
+
+    def get_protocol(self):
+        return self._from_environment("wsgi.url_scheme").lower()
+
+    def context_specifics(self):
+        return {"wsgi_environment": self._environment}
+
+    def get_client_ip(self):
+        return self._environment.get("REMOTE_ADDR")
+
+    def get_request_headers(self):
+        return self._request_headers
