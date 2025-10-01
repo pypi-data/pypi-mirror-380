312643259c8a5ff1dbb3fdd0762caf5e7dd5e727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:06 +0000

    Add package odoo-addon-cooperator-somconnexio/16.0.1.0.5/odoo_addon_cooperator_somconnexio-16.0.1.0.5.tar.gz

---
+++ packages/odoo-addon-cooperator-somconnexio/odoo_addon_cooperator_somconnexio-16.0.1.0.5.tar.gz/odoo_addon_cooperator_somconnexio-16.0.1.0.5/odoo/addons/cooperator_somconnexio/CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+## [16.0.1.0.5] - 2025-09-30
+### Fixed
+- [#1619](https://git.coopdevs.org/coopdevs/som-connexio/odoo-somconnexio/-/merge_requests/1619): Fix available categories from create lead wizard with cooperators
+
+## [16.0.1.0.4] - 2025-09-19
+### Fixed
+- [#1612](https://git.coopdevs.org/coopdevs/som-connexio/odoo/odoo-somconnexio/-/merge_requests/1612) Return recorset in _get_available_categories for partner.create.lead.wizard cooperator_somconnexio module
+
+## [16.0.1.0.3] - 2025-09-09
+### Changed
+- [#1598](https://git.coopdevs.org/coopdevs/som-connexio/odoo-somconnexio/-/merge_requests/1598) Phone required in SR creation view
+
+## [16.0.1.0.2] - 2025-09-03
+### Added
+- [#1583](https://git.coopdevs.org/coopdevs/som-connexio/odoo-somconnexio/-/merge_requests/1583) add source param o crm-lead and create lead wizard
+
+## [16.0.1.0.1] - 2025-08-26
+### Fixed
+- [#1569](https://git.coopdevs.org/coopdevs/som-connexio/odoo-somconnexio/-/merge_requests/1569) Fix mail compose _onchange_template_id
+
+## [16.0.1.0.0] - 2025-05-26
+### Added
+- Migrate cooperator_somconnexio module to ODOO v16
---
+++ packages/odoo-addon-cooperator-somconnexio/odoo_addon_cooperator_somconnexio-16.0.1.0.5.tar.gz/odoo_addon_cooperator_somconnexio-16.0.1.0.5/odoo/addons/cooperator_somconnexio/wizards/create_subscription_from_partner/create_subscription_from_partner.py
@@ -0,0 +1,67 @@
+from odoo import models, fields
+
+
+class PartnerCreateSubscription(models.TransientModel):
+    _inherit = "partner.create.subscription"
+    bank_id = fields.Many2one("res.partner.bank", "Bank Account", required=True)
+    payment_type = fields.Selection(
+        [("single", "One single payment"), ("split", "Ten payments")], required=True
+    )
+
+    def create_subscription(self):
+        sub_req = self.env["subscription.request"]
+
+        cooperator = self.cooperator
+        vals = {
+            "partner_id": cooperator.id,
+            "vat": cooperator.vat,
+            "share_product_id": self.share_product.id,
+            "ordered_parts": self.share_qty,
+            "user_id": self.env.uid,
+            "email": self.email,
+            "source": "crm",
+            "firstname": cooperator.firstname,
+            "lastname": cooperator.lastname,
+            "address": cooperator.street,
+            "zip_code": cooperator.zip,
+            "city": cooperator.city,
+            "country_id": cooperator.country_id.id,
+            "lang": cooperator.lang,
+            "is_company": self.is_company,
+        }
+        if self.is_company:
+            vals.update(
+                {
+                    "company_name": cooperator.name,
+                    "company_email": cooperator.email,
+                    "firstname": self.representative_firstname,
+                    "lastname": self.representative_lastname,
+                }
+            )
+
+        coop_vals = {}
+        if not self._get_email():
+            coop_vals["email"] = self.email
+
+        vals["iban"] = self.bank_id.acc_number
+        vals["payment_type"] = self.payment_type
+
+        if coop_vals:
+            cooperator.write(coop_vals)
+
+        new_sub_req = sub_req.create(vals)
+        context = {
+            "__last_update": {},
+            "active_model": "subscription.request",
+            "active_id": new_sub_req.id,
+            "active_ids": [new_sub_req.id],
+        }
+        return {
+            "type": "ir.actions.act_window",
+            "view_type": "form, tree",
+            "view_mode": "form",
+            "res_model": "subscription.request",
+            "res_id": new_sub_req.id,
+            "target": "current",
+            "context": context,
+        }
