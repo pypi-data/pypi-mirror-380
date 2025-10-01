ee3e28f1d175b680913aefd44a30856be9acf3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:19 +0000

    Add package spamfilter/2.2.0/spamfilter-2.2.0-py3-none-any.whl

---
+++ packages/spamfilter/spamfilter-2.2.0-py3-none-any.whl/spamfilter/filters/email.py
@@ -0,0 +1,30 @@
+"""
+Module for the email information filter.
+
+More information about this filter can be found in its Class docstring.
+"""
+
+from .regex import Regex
+
+POSSIBLE_MODES = ["normal", "censor"]
+STD_EMAIL_REGEX = r"([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)"
+
+
+class Email(Regex):
+    """
+    Check if a string contains an email address.
+
+    - `Email.mode`: how to handle a failing string.
+        - `normal`: fail the string.
+        - `censor`: censor the information.
+    - `Email.regex`: the regex used to check for email addresses.
+    - `Email.replacement`: what regex to replace email addresses with.
+    """
+
+    def __init__(
+        self,
+        regex: str = STD_EMAIL_REGEX,
+        mode: str = "normal",
+        replacement: str = r"***",
+    ):
+        super().__init__(regex, mode, replacement)
---
+++ packages/spamfilter/spamfilter-2.2.0-py3-none-any.whl/spamfilter/filters/regex.py
@@ -0,0 +1,49 @@
+"""
+Base module for the Regex base filter class.
+"""
+
+import re
+
+from ._check_modes import perform_mode_check
+from .filter import Filter
+
+
+POSSIBLE_MODES = ["normal", "censor"]
+
+
+class Regex(Filter):
+    """
+    Check if a string matches a given regular expression.
+
+    - `Regex.mode`: how to handle a failing string.
+        - `normal`: fail the string.
+        - `censor`: censor the match.
+    - `Regex.regex`: the regex used to check for matches.
+    - `Regex.replacement`: what regex to replace matches with.
+
+    _Was called "PersonalInformation" prior to v2.0.0, which was a breaking
+    change._
+    """
+
+    def __init__(
+        self, regex: str, mode: str = "normal", replacement: str = r"***"
+    ):
+        perform_mode_check(mode, POSSIBLE_MODES)
+
+        self.regex = regex
+        self.mode = mode
+        self.replacement = replacement
+
+        self._regex_compiled = re.compile(self.regex)
+
+    def check(self, string: str):
+        passed = not self._regex_compiled.search(string)
+
+        return (
+            (True if self.mode == "censor" else passed),
+            (
+                re.sub(self.regex, self.replacement, string)
+                if self.mode == "censor"
+                else string
+            ),
+        )
