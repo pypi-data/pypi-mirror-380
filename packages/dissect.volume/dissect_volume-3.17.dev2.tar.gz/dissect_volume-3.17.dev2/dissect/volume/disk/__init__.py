4c643659e154bf16bf76a8bf0ee5e85161446fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:25:01 +0000

    Add package dissect.volume/3.17.dev2/dissect_volume-3.17.dev2.tar.gz

---
+++ packages/dissect.volume/dissect_volume-3.17.dev2.tar.gz/dissect_volume-3.17.dev2/dissect/volume/disk/__init__.py
@@ -0,0 +1,9 @@
+from dissect.volume.disk.disk import Disk
+from dissect.volume.disk.partition import Partition
+from dissect.volume.exceptions import DiskError
+
+__all__ = [
+    "Disk",
+    "DiskError",
+    "Partition",
+]
