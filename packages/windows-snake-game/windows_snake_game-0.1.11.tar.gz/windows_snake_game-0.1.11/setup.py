8671d5b191441c96300c98f21170baf0e59dbd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 04:06:28 +0000

    Add package windows-snake-game/0.1.11/windows_snake_game-0.1.11.tar.gz

---
+++ packages/windows-snake-game/windows_snake_game-0.1.11.tar.gz/windows_snake_game-0.1.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="windows_snake_game",
+    version="0.1.11",
+    description="A Windows snake game project.It is a private project but you can use it if you trust me.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="ramimK0bir",
+    author_email="kobirbiddut81@gmail.com" ,  
+    packages=find_packages() ,
+    python_requires='>=3.7',
+
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+    ]
+
+)
