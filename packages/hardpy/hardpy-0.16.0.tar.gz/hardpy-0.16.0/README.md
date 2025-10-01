d17deb6d499bfbfda6194dbc6c90ccaa4b2a9d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:20 +0000

    Add package hardpy/0.16.0/hardpy-0.16.0.tar.gz

---
+++ packages/hardpy/hardpy-0.16.0.tar.gz/hardpy-0.16.0/README.md
@@ -0,0 +1,60 @@
+<h1 align="center">
+    <img src="https://raw.githubusercontent.com/everypinio/hardpy/main/docs/img/logo256.png" alt="HardPy" style="width:150px;">
+</h1>
+
+<p align="center">
+HardPy is a python library for creating a test bench for devices.
+</p>
+
+<div align="center">
+
+[![PyPI version](https://img.shields.io/pypi/v/hardpy)](https://pypi.org/project/hardpy/)
+![python versions](https://img.shields.io/pypi/pyversions/hardpy.svg)
+[![pytest versions](https://img.shields.io/badge/pytest-%3E%3D7.0%20%3C9.0-blue)](https://docs.pytest.org/en/latest/)
+[![Documentation](https://img.shields.io/badge/Documentation%20-Overview%20-%20%23007ec6)](https://everypinio.github.io/hardpy/)
+
+</div>
+
+---
+
+## Overview
+
+HardPy allows you to:
+
+* Create test benches for devices using [pytest](https://docs.pytest.org/);
+* Use a browser to view, start, stop, and interact with tests;
+* Store test results in the [CouchDB](https://couchdb.apache.org/) database.
+
+<h1 align="center">
+    <img src="https://raw.githubusercontent.com/everypinio/hardpy/main/docs/img/hardpy_panel.gif" alt="hardpy panel" style="width:550px;">
+</h1>
+
+## To Install
+
+```bash
+pip install hardpy
+```
+
+## Getting Started
+
+1. Create your first test bench.
+```bash
+hardpy init
+```
+2. Launch [CouchDB](https://couchdb.apache.org/) database via [docker compose](https://docs.docker.com/compose/) in the background.
+```bash
+cd tests
+docker compose up -d
+```
+3. Launch HardPy operator panel.
+```bash
+hardpy run
+```
+4. View operator panel in browser: http://localhost:8000/
+5. View the latest test report: http://localhost:5984/_utils
+
+    Login and password: **dev**, database - **runstore**.
+
+## Examples
+
+For more examples of using **HardPy**, see the [examples](https://github.com/everypinio/hardpy/tree/main/examples) folder and the [documentation](https://everypinio.github.io/hardpy/examples/).
---
+++ packages/hardpy/hardpy-0.16.0.tar.gz/hardpy-0.16.0/hardpy/hardpy_panel/api.py
@@ -0,0 +1,174 @@
+# Copyright (c) 2024 Everypin
+# GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import annotations
+
+import os
+import re
+from enum import Enum
+from pathlib import Path
+from typing import Annotated
+from urllib.parse import unquote
+
+from fastapi import FastAPI, Query
+from fastapi.staticfiles import StaticFiles
+
+from hardpy.common.config import ConfigManager
+from hardpy.pytest_hardpy.pytest_wrapper import PyTestWrapper
+
+app = FastAPI()
+app.state.pytest_wrp = PyTestWrapper()
+
+
+class Status(str, Enum):
+    """HardPy status.
+
+    Statuses, that can be returned by HardPy API.
+    """
+
+    STOPPED = "stopped"
+    STARTED = "started"
+    COLLECTED = "collected"
+    BUSY = "busy"
+    READY = "ready"
+    ERROR = "error"
+
+
+@app.get("/api/hardpy_config")
+def hardpy_config() -> dict:
+    """Get config of HardPy.
+
+    Returns:
+        dict: HardPy config
+    """
+    return app.state.pytest_wrp.get_config()
+
+
+@app.get("/api/start")
+def start_pytest(args: Annotated[list[str] | None, Query()] = None) -> dict:
+    """Start pytest subprocess.
+
+    Args:
+        args: List of arguments in key=value format
+
+    Returns:
+        dict[str, RunStatus]: run status
+    """
+    if args is None:
+        args_dict = []
+    else:
+        args_dict = dict(arg.split("=", 1) for arg in args if "=" in arg)
+
+    if app.state.pytest_wrp.start(start_args=args_dict):
+        return {"status": Status.STARTED}
+    return {"status": Status.BUSY}
+
+
+@app.get("/api/stop")
+def stop_pytest() -> dict:
+    """Stop pytest subprocess.
+
+    Returns:
+        dict[str, RunStatus]: run status
+    """
+    if app.state.pytest_wrp.stop():
+        return {"status": Status.STOPPED}
+    return {"status": Status.READY}
+
+
+@app.get("/api/collect")
+def collect_pytest() -> dict:
+    """Collect pytest subprocess.
+
+    Returns:
+        dict[str, RunStatus]: run status
+
+    """
+    if app.state.pytest_wrp.collect():
+        return {"status": Status.COLLECTED}
+    return {"status": Status.BUSY}
+
+
+@app.get("/api/status")
+def status() -> dict:
+    """Get pytest subprocess status.
+
+    Returns:
+        dict[str, RunStatus]: run status
+    """
+    is_running = app.state.pytest_wrp.is_running()
+    status = Status.BUSY if is_running else Status.READY
+    return {"status": status}
+
+
+@app.get("/api/couch")
+def couch_connection() -> dict:
+    """Get couchdb connection string.
+
+    Returns:
+        dict[str, str]: couchdb connection string
+    """
+    config_manager = ConfigManager()
+
+    return {
+        "connection_str": config_manager.config.database.url,
+    }
+
+
+@app.get("/api/database_document_id")
+def database_document_id() -> dict:
+    """Get couchdb syncronized document name.
+
+    Returns:
+        dict[str, str]: couchdb connection string
+    """
+    config_manager = ConfigManager()
+    return {"document_id": config_manager.config.database.doc_id}
+
+
+@app.post("/api/confirm_dialog_box/{dialog_box_output}")
+def confirm_dialog_box(dialog_box_output: str) -> dict:
+    """Confirm dialog box.
+
+    Args:
+        dialog_box_output (str): output data from dialog box.
+
+    Returns:
+        dict[str, RunStatus]: run status
+    """
+    hex_base = 16
+    unquoted_string = unquote(dialog_box_output)
+    decoded_string = re.sub(
+        "%([0-9a-fA-F]{2})",
+        lambda match: chr(int(match.group(1), hex_base)),
+        unquoted_string,
+    )
+
+    if app.state.pytest_wrp.send_data(str(decoded_string)):
+        return {"status": Status.BUSY}
+    return {"status": Status.ERROR}
+
+
+@app.post("/api/confirm_operator_msg/{is_msg_visible}")
+def confirm_operator_msg(is_msg_visible: str) -> dict:
+    """Confirm operator msg.
+
+    Args:
+        is_msg_visible (bool): is operator message is visible
+
+    Returns:
+        dict[str, RunStatus]: run status
+    """
+    if app.state.pytest_wrp.send_data(str(is_msg_visible)):
+        return {"status": Status.BUSY}
+    return {"status": Status.ERROR}
+
+
+if "DEBUG_FRONTEND" not in os.environ:
+    app.mount(
+        "/",
+        StaticFiles(
+            directory=Path(__file__).parent / "frontend/dist",
+            html=True,
+        ),
+        name="static",
+    )
