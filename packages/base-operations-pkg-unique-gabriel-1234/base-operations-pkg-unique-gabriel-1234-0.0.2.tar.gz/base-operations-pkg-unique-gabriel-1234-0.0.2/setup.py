98afd48208386e38339a05607e288b151ac6c9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 07:06:09 +0000

    Add package base-operations-pkg-unique-gabriel-1234/0.0.2/base-operations-pkg-unique-gabriel-1234-0.0.2.tar.gz

---
+++ packages/base-operations-pkg-unique-gabriel-1234/base-operations-pkg-unique-gabriel-1234-0.0.2.tar.gz/base-operations-pkg-unique-gabriel-1234-0.0.2/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='base-operations-pkg-unique-gabriel-1234', 
+    version='0.0.2',
+    author='თქვენი სახელი',
+    author_email='თქვენი-მეილი@example.com',
+    description='მარტივი კლასი ძირითადი მათემატიკური ოპერაციებისთვის.',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
