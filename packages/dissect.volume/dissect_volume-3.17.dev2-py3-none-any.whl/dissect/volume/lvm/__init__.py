6eaffd12c6a7d2f1bbad59941a71883c7599f49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:25:01 +0000

    Add package dissect.volume/3.17.dev2/dissect_volume-3.17.dev2-py3-none-any.whl

---
+++ packages/dissect.volume/dissect_volume-3.17.dev2-py3-none-any.whl/dissect/volume/lvm/__init__.py
@@ -0,0 +1,7 @@
+from dissect.volume.lvm.lvm2 import LVM2
+from dissect.volume.lvm.physical import LVM2Device
+
+__all__ = [
+    "LVM2",
+    "LVM2Device",
+]
---
+++ packages/dissect.volume/dissect_volume-3.17.dev2-py3-none-any.whl/dissect/volume/md/__init__.py
@@ -0,0 +1,6 @@
+from dissect.volume.md.md import MD, MDPhysicalDisk
+
+__all__ = [
+    "MD",
+    "MDPhysicalDisk",
+]
