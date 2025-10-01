91d77b7ec213586ae65d4decee9bdab028294049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 06:05:53 +0000

    Add package iden3math/0.1.1/iden3math-0.1.1-cp38-cp38-musllinux_1_2_x86_64.whl

---
+++ packages/iden3math/iden3math-0.1.1-cp38-cp38-musllinux_1_2_x86_64.whl/iden3math/prime.pyi
@@ -0,0 +1,11 @@
+"""
+This module provides type annotations for the prime module.
+"""
+
+def bn254() -> int:
+    """
+    Get the BN254 prime number.
+
+    :return: The BN254 prime number.
+    """
+    ...
