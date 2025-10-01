47d964586680f18985788d3c3aae55360065b44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 05:07:03 +0000

    Add package shudaodao/0.1.30/shudaodao-0.1.30-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/shudaodao/shudaodao-0.1.30-cp311-cp311-macosx_11_0_arm64.whl/shudaodao_core/portal_auth/entity_table/auth_rule.pyi
@@ -0,0 +1,16 @@
+from .. import auth_registry as auth_registry, get_schema_name as get_schema_name
+from _typeshed import Incomplete
+
+Base: Incomplete
+
+class AuthRule(Base):
+    __tablename__: str
+    __table_args__: Incomplete
+    id: Incomplete
+    ptype: Incomplete
+    v0: Incomplete
+    v1: Incomplete
+    v2: Incomplete
+    v3: Incomplete
+    v4: Incomplete
+    v5: Incomplete
