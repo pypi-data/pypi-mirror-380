df1b3a8b19e9c89e2d29ce3294c32ba439519e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 08:11:27 +0000

    Add package ctfbridge/0.4.3/ctfbridge-0.4.3.tar.gz

---
+++ packages/ctfbridge/ctfbridge-0.4.3.tar.gz/ctfbridge-0.4.3/ctfbridge/platforms/berg/http/endpoints.py
@@ -0,0 +1,9 @@
+API_BASE = "/api/v2"
+
+
+class Endpoints:
+    class Challenges:
+        LIST = f"{API_BASE}/challenges"
+
+    class Misc:
+        METADATA = f"{API_BASE}/metadata"
---
+++ packages/ctfbridge/ctfbridge-0.4.3.tar.gz/ctfbridge-0.4.3/ctfbridge/platforms/ctfd/utils/csrf.py
@@ -0,0 +1,25 @@
+import logging
+import re
+from typing import Optional, Any
+from ctfbridge.platforms.ctfd.http.endpoints import Endpoints
+
+from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+
+
+async def get_csrf_nonce(client: Any) -> Optional[str]:
+    response = await client.get(Endpoints.Misc.BASE_PAGE)
+    if nonce := extract_csrf_nonce(response.text):
+        return nonce
+    raise ValueError("Missing CSRF token")
+
+
+def extract_csrf_nonce(html: str) -> Optional[str]:
+    soup = BeautifulSoup(html, "html.parser")
+    for script in soup.find_all("script"):
+        if script.string and "csrfNonce" in script.string:
+            match = re.search(r"'csrfNonce':\s*\"([0-9a-f]{64})\"", script.string)
+            if match:
+                return match.group(1)
+    return None
