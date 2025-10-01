d8ae9064754302c0b460389f9b152e4c26f0d02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:50 +0000

    Add package pytrms/0.9.7/pytrms-0.9.7-py3-none-any.whl

---
+++ packages/pytrms/pytrms-0.9.7-py3-none-any.whl/pytrms/clients/db_api.py
@@ -0,0 +1,213 @@
+import os
+import json
+
+import requests
+
+from . import _logging
+from .ssevent import SSEventListener
+from .._base import _IoniClientBase
+
+log = _logging.getLogger(__name__)
+
+
+class IoniConnect(_IoniClientBase):
+
+    @property
+    def is_connected(self):
+        '''Returns `True` if connection to IoniTOF could be established.'''
+        try:
+            self.get("/api/status")
+            return True
+        except:
+            return False
+
+    @property
+    def is_running(self):
+        '''Returns `True` if IoniTOF is currently acquiring data.'''
+        # TODO :: /api/meas/curretn {isRunning ?}
+        raise NotImplementedError("is_running")
+
+    def connect(self, timeout_s):
+        # TODO :: create session ?! (see __init__ ...)
+        pass
+
+    def disconnect(self):
+        # TODO :: del session ?!
+        pass
+
+    def start_measurement(self, path=None):
+        '''Start a new measurement and block until the change is confirmed.
+
+        If 'path' is not None, write to the given .h5 file.
+        '''
+        # TODO :: POST /api/measurement {recipeDirectory}  / path = ?
+        pass
+
+    def stop_measurement(self, future_cycle=None):
+        '''Stop the current measurement and block until the change is confirmed.
+
+        If 'future_cycle' is not None and in the future, schedule the stop command.
+        '''
+        # TODO :: PUT /api/meas/current {isRunning = False}
+        pass
+
+    def __init__(self, host='127.0.0.1', port=5066, session=None):
+        super().__init__(host, port)
+        self.url = f"http://{self.host}:{self.port}"
+        if session is None:
+            session = requests.sessions.Session()
+        self.session = session
+        # ??
+        self.current_avg_endpoint = None
+        self.comp_dict = dict()
+
+    def get(self, endpoint, **kwargs):
+        return self._get_object(endpoint, **kwargs).json()
+
+    def post(self, endpoint, data, **kwargs):
+        return self._create_object(endpoint, data, 'post', **kwargs).headers.get('Location')
+
+    def put(self, endpoint, data, **kwargs):
+        return self._create_object(endpoint, data, 'put', **kwargs).headers.get('Location')
+
+    def upload(self, endpoint, filename):
+        if not endpoint.startswith('/'):
+            endpoint = '/' + endpoint
+        with open(filename) as f:
+            # Note (important!): this is a "form-data" entry, where the server
+            #  expects the "name" to be 'file' and rejects it otherwise:
+            name = 'file'
+            r = self.session.post(self.url + endpoint, files=[(name, (filename, f, ''))])
+            r.raise_for_status()
+
+        return r
+
+    def _get_object(self, endpoint, **kwargs):
+        if not endpoint.startswith('/'):
+            endpoint = '/' + endpoint
+        if 'headers' not in kwargs:
+            kwargs['headers'] = {'content-type': 'application/hal+json'}
+        elif 'content-type' not in (k.lower() for k in kwargs['headers']):
+            kwargs['headers'].update({'content-type': 'application/hal+json'})
+        if 'timeout' not in kwargs:
+            # https://requests.readthedocs.io/en/latest/user/advanced/#timeouts
+            kwargs['timeout'] = (6.06, 27)
+        r = self.session.request('get', self.url + endpoint, **kwargs)
+        r.raise_for_status()
+        
+        return r
+
+    def _create_object(self, endpoint, data, method='post', **kwargs):
+        if not endpoint.startswith('/'):
+            endpoint = '/' + endpoint
+        if not isinstance(data, str):
+            data = json.dumps(data, ensure_ascii=False)  # default is `True`, escapes Umlaute!
+        if 'headers' not in kwargs:
+            kwargs['headers'] = {'content-type': 'application/hal+json'}
+        elif 'content-type' not in (k.lower() for k in kwargs['headers']):
+            kwargs['headers'].update({'content-type': 'application/hal+json'})
+        if 'timeout' not in kwargs:
+            # https://requests.readthedocs.io/en/latest/user/advanced/#timeouts
+            kwargs['timeout'] = (6.06, 27)
+        r = self.session.request(method, self.url + endpoint, data=data, **kwargs)
+        if not r.ok:
+            log.error(f"POST {endpoint}\n{data}\n\nreturned [{r.status_code}]: {r.content}")
+            r.raise_for_status()
+
+        return r
+
+    def sync(self, peaktable):
+        """Compare and upload any differences in `peaktable` to the database."""
+        from pytrms.peaktable import Peak, PeakTable
+        from operator import attrgetter
+
+        # Note: the DB-API distinguishes between peaks with 
+        #  different center *and* name, so this is our key:
+        make_key = lambda peak: (peak['center'], peak['name'])
+
+        if isinstance(peaktable, str):
+            log.info(f"loading peaktable '{peaktable}'...")
+            peaktable = PeakTable.from_file(peaktable)
+
+        # get the PyTRMS- and IoniConnect-peaks on the same page:
+        conv = {
+            'name':   attrgetter('label'),
+            'center': attrgetter('center'),
+            'kRate':  attrgetter('k_rate'),
+            'low':    lambda p: p.borders[0],
+            'high':   lambda p: p.borders[1],
+            'shift':  attrgetter('shift'),
+            'multiplier': attrgetter('multiplier'),
+            'resolution': attrgetter('resolution'),
+        }
+        # normalize the input argument and create a hashable set:
+        updates = dict()
+        for peak in peaktable:
+            payload = {k: conv[k](peak) for k in conv}
+            updates[make_key(payload)] = {'payload': payload}
+
+        log.info(f"fetching current peaktable from the server...")
+        # create a comparable collection of peaks already on the database by
+        # reducing the keys in the response to what we actually want to update:
+        db_peaks = {make_key(p): {
+                    'payload': {k: p[k] for k in conv.keys()},
+                    'self':   p['_links']['self'],
+                    'parent': p['_links'].get('parent'),
+                    } for p in self.get('/api/peaks')['_embedded']['peaks']}
+
+        to_update = updates.keys() & db_peaks.keys()
+        to_upload = updates.keys() - db_peaks.keys()
+        updated = 0
+        for key in sorted(to_update):
+            # check if an existing peak needs an update
+            if db_peaks[key]['payload'] == updates[key]['payload']:
+                # nothing to do..
+                log.debug(f"up-to-date: {key}")
+                continue
+
+            self.put(db_peaks[key]['self']['href'], updates[key]['payload'])
+            log.info(f"updated:    {key}")
+            updated += 1
+
+        if len(to_upload):
+            # Note: POSTing the embedded-collection is *miles faster*
+            #  than doing separate requests for each peak!
+            payload = {'_embedded': {'peaks': [updates[key]['payload'] for key in sorted(to_upload)]}}
+            self.post('/api/peaks', payload)
+            for key in sorted(to_upload):
+                log.info(f"added new:  {key}")
+
+        if len(peaktable.fitted):
+            # Note: until now, we disregarded the peak-parent-relationship, so
+            # make another request to the updated peak-table from the server...
+            peak2href = {Peak(center=p["center"], label=p["name"]): p["_links"]["self"]["href"]
+                            for p in self.get('/api/peaks')['_embedded']['peaks']}
+
+            for fitted in peaktable.fitted:
+                fitted_href = peak2href[fitted]
+                parent_href = peak2href[fitted.parent]
+                r = self.session.request('link', self.url + parent_href, headers={"location": fitted_href})
+                if not r.ok:
+                    log.error(f"LINK {parent_href} to Location: {fitted_href} failed\n\n[{r.status_code}]: {r.content}")
+                    r.raise_for_status()
+                log.debug(f"linked parent {parent_href} ~> {fitted_href}")
+
+        return {
+                'added': len(to_upload),
+                'updated': updated,
+                'up-to-date': len(to_update) - updated,
+        }
+
+    def iter_events(self, event_re=r".*"):
+        """Follow the server-sent-events (SSE) on the DB-API.
+
+        `event_re`  a regular expression to filter events (default: matches everything)
+
+        Note: This will block until a matching event is received.
+         Especially, it cannot be cancelled by KeyboardInterrupt (due to the `requests`
+         stream-implementation), unless the server sends a keep-alive at regular
+         intervals (as every well-behaved server should be doing)!
+        """
+        yield from SSEventListener(event_re, host_url=self.url, endpoint="/api/events",
+                session=self.session)
+
---
+++ packages/pytrms/pytrms-0.9.7-py3-none-any.whl/pytrms/instrument.py
@@ -0,0 +1,127 @@
+import os.path
+import time
+from abc import abstractmethod, ABC
+
+from .measurement import (
+        RunningMeasurement,
+        FinishedMeasurement,
+)
+
+__all__ = ['Instrument']
+
+
+class Instrument(ABC):
+    '''
+    Class for controlling the PTR instrument remotely.
+
+    This class reflects the states of the actual instrument, which can be currently idle
+    or running a measurement. An idle instrument can start a measurement. A running
+    instrument can be stopped. But trying to start a measurement twice will raise an
+    exception (RuntimeError).
+
+    Note, that for every client PTR instrument there is only one instance of this class.
+    This is to prevent different instances to be in other states than the instrument.
+    '''
+    __instance = None
+
+    def _new_state(self, newstate):
+        # Note: we get ourselves a nifty little state-machine :)
+        self.__class__ = newstate
+
+    def __new__(cls, backend):
+        # Note (reminder): If __new__() does not return an instance of cls,
+        #  then the new instance’s __init__() method will *not* be invoked!
+        #
+        # This aside, we override the __new__ method to make this class a
+        #  singleton that reflects the PTR-instrument state and dispatches
+        #  to one of its subclass implementations.
+        if cls._Instrument__instance is not None:
+            return cls._Instrument__instance
+
+        if backend.is_running:
+            inst = object.__new__(_RunningInstrument)
+        else:
+            inst = object.__new__(_IdleInstrument)
+
+        Instrument._Instrument__instance = inst
+
+        return inst
+
+    def __init__(self, backend):
+        # Note: this will be called *once* per Python process!
+        self.backend = backend
+
+    @property
+    def is_local(self):
+        """Returns True if files are written to the local machine."""
+        host = str(self.backend.host)
+        return 'localhost' in host or '127.0.0.1' in host
+
+    def get(self, varname):
+        """Get the current value of a setting."""
+        # TODO :: this is not an interface implementation...
+        raw = self.backend.get(varname)
+
+        from .clients.mqtt import MqttClient
+        if not isinstance(self.backend, MqttClient):
+            import json
+            jobj = json.loads(raw)
+
+            return jobj[0]['Act']['Real']
+
+        ## ...how it should be: just:
+        return raw
+
+    def set(self, varname, value, unit='-'):
+        """Set a variable to a new value."""
+        return self.backend.set(varname, value, unit='-')
+
+    _current_sourcefile = ''
+
+    def start_measurement(self, filename=''):
+        # this method must be implemented by each state
+        raise RuntimeError("can't start %s" % self.__class__)
+
+    def stop_measurement(self):
+        # this method must be implemented by each state
+        raise RuntimeError("can't stop %s" % self.__class__)
+
+
+class _IdleInstrument(Instrument):
+
+    def start_measurement(self, filename=''):
+        dirname = os.path.dirname(filename)
+        if dirname and self.is_local:
+            # Note: if we send a filepath to the server that does not exist there, the
+            #  server will open a dialog and "hang" (which I'd very much like to avoid).
+            #  the safest way is to not send a path at all and start a 'Quick' measurement.
+            #  but if the server is the local machine, we do our best to verify the path:
+            os.makedirs(dirname, exist_ok=True)
+
+        if filename:
+            basename = os.path.basename(filename)
+            # this may very well be a directory to record a filename into:
+            if not basename:
+                basename = '%Y-%m-%d_%H-%M-%S.h5'
+                filename = os.path.join(dirname, basename)
+            # finally, pass everything through strftime...
+            filename = time.strftime(filename)
+            if os.path.exists(filename):
+                raise RuntimeError(f'filename exists and cannot be overwritten')
+
+        self.backend.start_measurement(filename)
+        self._current_sourcefile = filename
+        self._new_state(RunningInstrument)
+
+        return RunningMeasurement(self)
+
+
+class _RunningInstrument(Instrument):
+
+    def stop_measurement(self):
+        self.backend.stop_measurement()
+        self._new_state(IdleInstrument)
+
+        # TODO :: this catches only one sourcefile.. it'll do for simple cases:
+        return FinishedMeasurement(self._current_sourcefile)
+
