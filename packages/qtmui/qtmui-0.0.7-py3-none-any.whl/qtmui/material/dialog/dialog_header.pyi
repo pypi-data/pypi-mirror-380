87e944efc62f4e011429092f51125a295c4b9594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 06:06:16 +0000

    Add package qtmui/0.0.7/qtmui-0.0.7-py3-none-any.whl

---
+++ packages/qtmui/qtmui-0.0.7-py3-none-any.whl/qtmui/material/dialog/dialog_header.pyi
@@ -0,0 +1,5 @@
+from qtpy.QtWidgets import QHBoxLayout, QFrame, QSpacerItem, QSizePolicy
+from ..typography.typography import Typography
+from ..button import IconButton
+class DialogHeader:
+    def __init__(self, parent, title: str, align: str): ...
