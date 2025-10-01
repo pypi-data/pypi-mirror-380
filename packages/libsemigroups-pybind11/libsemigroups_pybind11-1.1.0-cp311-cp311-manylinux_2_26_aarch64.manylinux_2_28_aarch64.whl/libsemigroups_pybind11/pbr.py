a597717997ab0184b8d5bbe628bcd9a0ab21ea77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 04:06:28 +0000

    Add package libsemigroups-pybind11/1.1.0/libsemigroups_pybind11-1.1.0-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.1.0-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/libsemigroups_pybind11/pbr.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Joseph Edwards
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""
+This page contains the documentation for the ``pbr`` subpackage, that
+contains helper functions for the :any:`PBR` class.
+"""
+
+from _libsemigroups_pybind11 import (  # pylint: disable=no-name-in-module,unused-import
+    pbr_one as one,
+)
