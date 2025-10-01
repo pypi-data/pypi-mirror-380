87e3c29d0eef69a4871344f0cd0a2c15186fc166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:07:31 +0000

    Add package types-gunicorn/23.0.0.20251001/types_gunicorn-23.0.0.20251001.tar.gz

---
+++ packages/types-gunicorn/types_gunicorn-23.0.0.20251001.tar.gz/types_gunicorn-23.0.0.20251001/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'gunicorn-stubs': ['__init__.pyi', '_types.pyi', 'app/__init__.pyi', 'app/base.pyi', 'app/pasterapp.pyi', 'app/wsgiapp.pyi', 'arbiter.pyi', 'config.pyi', 'debug.pyi', 'errors.pyi', 'glogging.pyi', 'http/__init__.pyi', 'http/body.pyi', 'http/errors.pyi', 'http/message.pyi', 'http/parser.pyi', 'http/unreader.pyi', 'http/wsgi.pyi', 'instrument/__init__.pyi', 'instrument/statsd.pyi', 'pidfile.pyi', 'reloader.pyi', 'sock.pyi', 'systemd.pyi', 'util.pyi', 'workers/__init__.pyi', 'workers/base.pyi', 'workers/base_async.pyi', 'workers/geventlet.pyi', 'workers/ggevent.pyi', 'workers/gthread.pyi', 'workers/gtornado.pyi', 'workers/sync.pyi', 'workers/workertmp.pyi', 'METADATA.toml', 'py.typed']})
