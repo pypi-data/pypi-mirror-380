028ec432698ba8c276fc7e7cdb1becaf95b83b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:51 +0000

    Add package protocol-proxy/2.0.0rc1/protocol_proxy-2.0.0rc1-py3-none-any.whl

---
+++ packages/protocol-proxy/protocol_proxy-2.0.0rc1-py3-none-any.whl/protocol_proxy/ipc/__init__.py
@@ -0,0 +1,5 @@
+from .headers import HeadersV1, ProtocolHeaders
+from .decorator import callback
+from .base import (IPCConnector, ProtocolProxyCallback, ProtocolHeaders, ProtocolProxyMessage,
+                   ProtocolProxyPeer, SocketParams)
+
