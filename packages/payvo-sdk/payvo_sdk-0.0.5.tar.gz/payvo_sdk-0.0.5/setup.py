c815933a3c0a996290da33c91110b30da2ec9c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 05:06:54 +0000

    Add package payvo-sdk/0.0.5/payvo_sdk-0.0.5.tar.gz

---
+++ packages/payvo-sdk/payvo_sdk-0.0.5.tar.gz/payvo_sdk-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="payvo_sdk",
+    version="0.0.5",
+    author="erruqie",
+    author_email="clownl3ss@icloud.com",
+    description="Python SDK для работы с Payvo API",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/erruqie/payvo_sdk",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.30.0"
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
