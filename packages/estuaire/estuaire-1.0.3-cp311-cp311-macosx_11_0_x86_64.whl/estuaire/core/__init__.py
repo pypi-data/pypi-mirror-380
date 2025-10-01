5fbc7d05924a1bab539dcbd37df33d36197d89ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:28 +0000

    Add package estuaire/1.0.3/estuaire-1.0.3-cp311-cp311-macosx_11_0_x86_64.whl

---
+++ packages/estuaire/estuaire-1.0.3-cp311-cp311-macosx_11_0_x86_64.whl/estuaire/core/__init__.py
@@ -0,0 +1,11 @@
+#
+# @Author : Jean-Pascal Mercier <jean-pascal.mercier@agsis.com>
+#
+# @Copyright (C) 2010 Jean-Pascal Mercier
+#
+# All rights reserved.
+#
+
+# from .frechet import compute_frechet, compute_sparse_sensitivity
+#
+# __all__ = ["compute_frechet", "compute_sparse_sensitivity"]
