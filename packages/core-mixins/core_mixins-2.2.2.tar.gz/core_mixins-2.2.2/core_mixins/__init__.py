603c894467fd700c226113f8c33764829afa7696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:07:39 +0000

    Add package core-mixins/2.2.2/core_mixins-2.2.2.tar.gz

---
+++ packages/core-mixins/core_mixins-2.2.2.tar.gz/core_mixins-2.2.2/core_mixins/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from enum import StrEnum as _StrEnum
+
+except ImportError:
+    from .compatibility import StrEnum as _StrEnum  # type: ignore[assignment]
+
+from .compatibility import Self
+
+# Type alias that works with both standard library
+# and the custom HTTPStatus...
+StrEnum = _StrEnum
+
+__all__ = [
+    "Self",
+    "StrEnum",
+]
