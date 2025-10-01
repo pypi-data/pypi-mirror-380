d07365a271d401fc588fb13da32b45aba06c30c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:37 +0000

    Add package django-qr-toolkit-core/4.3.1/django_qr_toolkit_core-4.3.1.tar.gz

---
+++ packages/django-qr-toolkit-core/django_qr_toolkit_core-4.3.1.tar.gz/django_qr_toolkit_core-4.3.1/qrtoolkit_core/validators.py
@@ -0,0 +1,15 @@
+from django.core.exceptions import ValidationError
+from rest_framework import permissions
+
+
+def validate_is_api_mode_qrcode(value):
+    """
+
+    :type value: LinkUrl
+    """
+    from .models import QRCode, LinkUrl
+    qrcode = LinkUrl.objects.select_related('code').get(pk=value).code
+    if qrcode.mode != QRCode.REDIRECT_MODE_CHOICES.API_CALL:
+        raise ValidationError('The selected qr code is not in Api call Mode.')
+
+
