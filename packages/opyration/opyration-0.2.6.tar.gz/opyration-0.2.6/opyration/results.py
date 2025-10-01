84e15373f775868a553d353f4043872ef71bda5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:06:53 +0000

    Add package opyration/0.2.6/opyration-0.2.6.tar.gz

---
+++ packages/opyration/opyration-0.2.6.tar.gz/opyration-0.2.6/opyration/results.py
@@ -0,0 +1,63 @@
+from collections import deque
+
+
+class Lookup(dict):
+    def __init__(self, data: dict):
+        super().__init__(data)
+        self._data = data
+
+    def __getattr__(self, key: str):
+        value = self._data.get(key)
+        if isinstance(value, dict):
+            return Lookup(value)
+        return value
+
+    def __getitem__(self, key: str):
+        return self._data.get(key)
+
+    def __setitem__(self, key: str, value):
+        self._data[key] = value
+    
+    def __delitem__(self, key: str):
+        del self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __dict__(self):
+        return self._data
+
+
+class Results(object):
+    def __init__(self, data: list):
+        self.__data = data
+        self.__queue = deque(data or [])
+        self.__count = 0
+
+    def __bool__(self):
+        return bool(self.__queue)
+
+    def data(self):
+        return self.__data
+
+    def next(self) -> Lookup | None:
+        try: return Lookup(self.__queue[self.__count])
+        except: return None
+        finally: self.__count += 1
+
+    def pop(self) -> Lookup | None:
+        try: datum = self.__queue.popleft()
+        except IndexError: return None
+        else: return Lookup(dict(datum))
+
+    def reset(self, index: int = 0):
+        self.__count = index
+
+    def row(self, n: int):
+        if n < 1: n = 1
+        try: return Lookup(dict(self.__queue[n - 1]))
+        except: return None
+
+    def rows(self, n: int) -> list[Lookup]:
+        if n > len(self.__queue): n = len(self.__queue)
+        return [self.pop() for _ in range(n)]
