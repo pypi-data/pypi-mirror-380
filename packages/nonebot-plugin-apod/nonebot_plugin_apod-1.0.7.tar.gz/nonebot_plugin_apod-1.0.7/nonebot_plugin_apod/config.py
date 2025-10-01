52d153f279a2ceb49f5f956f3b24b1c60836a6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:07:12 +0000

    Add package nonebot-plugin-apod/1.0.7/nonebot_plugin_apod-1.0.7.tar.gz

---
+++ packages/nonebot-plugin-apod/nonebot_plugin_apod-1.0.7.tar.gz/nonebot_plugin_apod-1.0.7/nonebot_plugin_apod/config.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from asyncio import Lock
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    apod_api_key: Optional[str] = None
+    apod_default_send_time: str = "13:00"
+    apod_hd_image: bool = False
+    apod_baidu_trans: bool = False
+    apod_baidu_trans_appid: Optional[int] = None
+    apod_baidu_trans_api_key: Optional[str] = None
+    apod_infopuzzle: bool = True
+    apod_infopuzzle_dark_mode: bool = False
+    apod_deepl_trans: bool = False
+    apod_deepl_trans_api_key: Optional[str] = None
+
+
+# 缓存天文一图图片
+cache_image = None
+cache_lock = Lock()
+
+# 获取缓存图片
+def get_cache_image():
+    return cache_image
+
+# 设置缓存图片
+async def set_cache_image(image):
+    global cache_image
+    async with cache_lock:
+        cache_image = image
+
+# 清除缓存图片
+async def clear_cache_image():
+    global cache_image
+    async with cache_lock:
+        cache_image = None
