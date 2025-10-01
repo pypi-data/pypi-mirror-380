da78489d2644af67623694f60d44ece50cad97b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:04 +0000

    Add package jobtty/1.2.8/jobtty-1.2.8-py3-none-any.whl

---
+++ packages/jobtty/jobtty-1.2.8-py3-none-any.whl/jobtty/__init__.py
@@ -0,0 +1,8 @@
+"""
+Jobtty.io - Terminal Job Board
+🚀 Find your next role from the command line
+"""
+
+__version__ = "1.2.8"
+__author__ = "Croscom Software"
+__email__ = "konrad@croscomsoftware.com"
