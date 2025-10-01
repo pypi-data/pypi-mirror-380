1c0ddd551735fa35ea116862d5eede51d4008003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:06:56 +0000

    Add package ngcsdk/4.5.2/ngcsdk-4.5.2-py3-none-any.whl

---
+++ packages/ngcsdk/ngcsdk-4.5.2-py3-none-any.whl/registry/data/search/ResourceTypeEnum.py
@@ -0,0 +1,3 @@
+ResourceTypeEnum=["MODEL","JOB","JOB_TEMPLATE","CONTAINER","DATASET","RESULTSET","WORKSPACE","RECIPE","HELM_CHART","ORGANIZATION","TEAM","USER","COLLECTION","PRODUCT",]
+str(repr(ResourceTypeEnum))  # Prevent optimizer removing enum
+
