eb0cad8af69ef3ae76d5555ad4c89fc1e77564d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:05:57 +0000

    Add package pytrms/0.9.7/pytrms-0.9.7.tar.gz

---
+++ packages/pytrms/pytrms-0.9.7.tar.gz/pytrms-0.9.7/pytrms/__init__.py
@@ -0,0 +1,42 @@
+_version = '0.9.7'
+
+__all__ = ['load', 'connect']
+
+
+def load(path):
+    '''Open a datafile for post-analysis or batch processing.
+
+    `path` may be a glob-expression to collect a whole batch.
+
+    returns a `Measurement` instance.
+    '''
+    import glob
+    from .measurement import FinishedMeasurement
+
+    files = glob.glob(path)
+
+    return FinishedMeasurement(*files)
+
+
+def connect(host='localhost', port=None, method='mqtt'):
+    '''Connect a client to a running measurement server.
+
+    'method' is the preferred connection, either 'mqtt' (default), 'webapi' or 'modbus'.
+
+    returns an `Instrument` if connected successfully.
+    '''
+    from .instrument import Instrument
+
+    if method.lower() == 'mqtt':
+        from .clients.mqtt import MqttClient as _client
+    elif method.lower() == 'webapi':
+        from .clients.ioniclient import IoniClient as _client
+    elif method.lower() == 'modbus':
+        from .clients.modbus import IoniconModbus as _client
+    else:
+        raise NotImplementedError(str(method))
+
+    backend = _client(host, port) if port is not None else _client(host)
+
+    return Instrument(backend)
+
---
+++ packages/pytrms/pytrms-0.9.7.tar.gz/pytrms-0.9.7/pytrms/clients/ssevent.py
@@ -0,0 +1,82 @@
+import re
+from collections import namedtuple
+from collections.abc import Iterable
+
+import requests
+
+from . import _logging
+
+log = _logging.getLogger(__name__)
+
+_event_rv = namedtuple('ssevent', ['event', 'data'])
+
+class SSEventListener(Iterable):
+
+    @staticmethod
+    def _line_stream(response):
+        # Note: using .iter_content() seems to yield results faster than .iter_lines()
+        line = ''
+        for bite in response.iter_content(chunk_size=1, decode_unicode=True):
+            line += bite
+            if bite == '\n':
+                yield line.strip()
+                line = ''
+
+    def __init__(self, event_re=None, host_url='http://127.0.0.1:5066',
+            endpoint='/api/events', session=None):
+        self.uri = host_url + endpoint
+        if session is not None:
+            self._get = session.get
+        else:
+            self._get = requests.get
+        self._connect_response = None
+        self.subscriptions = set()
+        if event_re is not None:
+            self.subscribe(event_re)
+
+    def subscribe(self, event_re):
+        """Listen for events matching the given string or regular expression."""
+        self.subscriptions.add(re.compile(event_re))
+        if self._connect_response is None:
+            r = self._get(self.uri, headers={'accept': 'text/event-stream'}, stream=True)
+            if not r.ok:
+                log.error(f"no connection to {self.uri} (got [{r.status_code}])")
+                r.raise_for_status()
+
+            self._connect_response = r
+
+    def unsubscribe(self, event_re):
+        """Stop listening for certain events."""
+        self.subscriptions.remove(re.compile(event_re))
+        if not len(self.subscriptions):
+            log.debug(f"closing connection to {self.uri}")
+            self._connect_response.close()
+            self._connect_response = None
+
+    def __iter__(self):
+        if self._connect_response is None:
+            raise Exception("call .subscribe() first to listen for events")
+
+        event = msg = ''
+        for line in self._line_stream(self._connect_response):  # blocks...
+            if not line:
+                # an empty line concludes an event
+                if event and any(re.match(sub, event) for sub in self.subscriptions):
+                    yield _event_rv(event, msg)
+
+                # Note: any further empty lines are ignored (may be used as keep-alive),
+                #  but in either case clear event and msg to rearm for the next event:
+                event = msg = ''
+                continue
+
+            key, val = line.split(':', maxsplit=1)
+            if not key:
+                # this is a comment, starting with a colon ':' ...
+                log.log(_logging.TRACE, "sse:" + val)
+            elif key == 'event':
+                event = val.lstrip()
+            elif key == 'data':
+                msg += val.lstrip()
+            else:
+                log.warning(f"unknown SSE-key <{key}> in stream")
+
