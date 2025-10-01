b27d13c4ecfa76ed81263c8760a0bad78359b6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:08:16 +0000

    Add package paraconsistent/0.1.5/paraconsistent-0.1.5-py3-none-any.whl

---
+++ packages/paraconsistent/paraconsistent-0.1.5-py3-none-any.whl/paraconsistent/core/types.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from typing import TypedDict
+
+
+__all__ = ["Complete"]
+
+
+class Complete(TypedDict):
+    # parâmetros
+    FL: float; FtC: float; FD: float
+    VSSC: float; VICC: float; VSSCT: float; VICCT: float
+    VlV: float; VlF: float; L: float
+    # entradas
+    mu: float; lam: float; mu_p: float; lam_p: float
+    # graus e derivados
+    gc: float; gct: float; gct_adj: float
+    d: float; D: float; gcr: float
+    # evidências
+    muE: float; muE_p: float; muECT: float; muER: float; phi: float; phiE: float
