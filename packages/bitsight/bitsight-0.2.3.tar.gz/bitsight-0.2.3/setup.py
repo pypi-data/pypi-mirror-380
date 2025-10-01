341374d7d0b00ce7bd1e9519a20a45b149fafe48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:07:18 +0000

    Add package bitsight/0.2.3/bitsight-0.2.3.tar.gz

---
+++ packages/bitsight/bitsight-0.2.3.tar.gz/bitsight-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+version = '0.2.3'
+
+setup(
+    name='bitsight',
+    version=version,
+    packages=['bitsight', 'bitsight.api_io', 'bitsight.resources'],
+    url='https://github.com/brandon-smith-187/bitsight',
+    license='MIT',
+    author='Brandon Smith',
+    author_email='bcsmith2@gmail.com',
+    description='Unofficial Package for BitSight\'s API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+    install_requires=[
+        'requests>=2.28.1',
+    ],
+)
