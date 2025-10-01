06d9e804844f466e24a0aa08dd758e004a97f038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:23:54 +0000

    Add package amcheck/1.0.2/amcheck-1.0.2-py3-none-any.whl

---
+++ packages/amcheck/amcheck-1.0.2-py3-none-any.whl/amcheck/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.2'
+
+from .amcheck import check_altermagnetism_orbit
+from .amcheck import is_altermagnet
+from .amcheck import label_matrix
+from .amcheck import symmetrized_conductivity_tensor
