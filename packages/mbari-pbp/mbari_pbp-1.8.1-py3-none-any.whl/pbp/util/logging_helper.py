c44dbc04c9c8a5aa0202bafd233efe62865bf5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 07:05:59 +0000

    Add package mbari-pbp/1.8.1/mbari_pbp-1.8.1-py3-none-any.whl

---
+++ packages/mbari-pbp/mbari_pbp-1.8.1-py3-none-any.whl/pbp/util/logging_helper.py
@@ -0,0 +1,49 @@
+import copy
+import sys
+import loguru
+import os
+import pathlib
+
+from typing import Optional, Tuple
+
+
+def create_logger(
+    log_filename_and_level: Optional[Tuple[str, str]] = None,
+    console_level: Optional[str] = None,
+) -> "loguru.Logger":
+    """
+    Create logger instance.
+    This utility was initially introduced for the HMB generation functionality,
+    but later on also used in other places.
+
+    NOTE: The created object should be passed around to all relevant functions.
+    Do not use `from loguru import logger` (or similar) to obtain the logger.
+
+    :param log_filename_and_level:
+        (filename, level) tuple or None to disable file logging.
+    :param console_level:
+        The log level for the console, or None to disable console logging.
+    """
+
+    fmt = "{time} {level} {message}"
+    loguru.logger.remove()
+    log = copy.deepcopy(loguru.logger)
+    if log_filename_and_level is not None:
+        log_filename, log_level = log_filename_and_level
+        # create log_filename's parent directory if needed:
+        parent_dir = pathlib.Path(log_filename).parent
+        pathlib.Path(parent_dir).mkdir(parents=True, exist_ok=True)
+        file_fmt = fmt
+        if os.getenv("EXCLUDE_LOG_TIME", "no") == "yes":
+            # test convenience to facilitate local diffing of log files
+            file_fmt = "{level} {message}"
+        log.add(
+            sink=open(log_filename, "w"), level=log_level, format=file_fmt, enqueue=True
+        )
+
+    if console_level is not None:
+        log.add(
+            sink=sys.stderr, level=console_level, format=fmt, colorize=True, enqueue=True
+        )
+
+    return log
