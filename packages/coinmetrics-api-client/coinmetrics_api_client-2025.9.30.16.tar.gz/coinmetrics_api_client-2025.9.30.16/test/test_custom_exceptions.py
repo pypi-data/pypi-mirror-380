c2a27436552f5136e1b0616cb0b96dd249f2783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 06:06:29 +0000

    Add package coinmetrics-api-client/2025.9.30.16/coinmetrics_api_client-2025.9.30.16.tar.gz

---
+++ packages/coinmetrics-api-client/coinmetrics_api_client-2025.9.30.16.tar.gz/coinmetrics_api_client-2025.9.30.16/test/test_custom_exceptions.py
@@ -0,0 +1,68 @@
+import pytest
+import requests
+from datetime import datetime
+from coinmetrics._exceptions import (
+    CoinMetricsClientQueryParamsException,
+    # CoinMetricsUnauthorizedException,
+)
+from coinmetrics.api_client import CoinMetricsClient
+from coinmetrics.data_exporter import CoinMetricsDataExporter
+import os
+
+client = CoinMetricsClient(str(os.environ.get("CM_API_KEY")))
+cm_api_key_set = os.environ.get("CM_API_KEY") is not None
+REASON_TO_SKIP = "Need to set CM_API_KEY as an env var in order to run this test"
+
+print("CM_API_KEY is set - tests will run") if cm_api_key_set else print(
+    "CM_API_KEY not set, tests will not run"
+)
+
+
+@pytest.mark.skipif(not cm_api_key_set, reason=REASON_TO_SKIP)
+def test_custom_414_error() -> None:
+    """
+    Test for CoinMetricsClientQueryParamsException hitting a 414 error.
+    """
+    markets = [market["market"] for market in client.reference_data_markets()]
+    try:
+        intentional_414 = client.reference_data_markets(markets=markets)
+    except Exception as e:
+        assert type(e) == CoinMetricsClientQueryParamsException
+        print(e)
+
+
+@pytest.mark.skipif(not cm_api_key_set, reason=REASON_TO_SKIP)
+def test_custom_exception_not_raised_for_403() -> None:
+    """
+    Tests that custom exceptions are not raised for unauthorized - just a normal HTTPError, If a different error is
+    is raised it asserts false
+    """
+    unauthorized_client = CoinMetricsClient(api_key="Invalid API Key")
+    try:
+        intentional_401 = unauthorized_client.reference_data_markets()
+    except requests.HTTPError as e:
+        assert e.response is not None
+        assert e.response.status_code == 401
+    except Exception as e:
+        assert False
+
+
+# @pytest.mark.skipif(not cm_api_key_set, reason=REASON_TO_SKIP)
+# def test_403_error_message() -> None:
+#     invalid_api_key = "invalid_key"
+#     data_exporter = CoinMetricsDataExporter(api_key=invalid_api_key)
+#     try:
+#         start_date = datetime(2019, 1, 1)
+#         end_date = datetime(2019, 1, 31)
+#         exchanges = ["coinbase", "gemini"]
+#         data_exporter.export_market_trades_spot_data(
+#             start_date=start_date, end_date=end_date, exchanges=exchanges, threaded=True
+#         )
+#     except CoinMetricsUnauthorizedException as e:
+#         assert e.response is not None
+#         assert e.response.status_code == 403 or e.response.status_code == 401
+#         print(e)
+
+
+if __name__ == "__main__":
+    pytest.main()
