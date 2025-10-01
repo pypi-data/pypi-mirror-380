0d67cd116eab33dba9df19bc726cd5eba3494b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 07:06:08 +0000

    Add package bus-202/1.17.1/bus_202-1.17.1.tar.gz

---
+++ packages/bus-202/bus_202-1.17.1.tar.gz/bus_202-1.17.1/bus_202/__init__.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pathlib import Path
+from .data_visualization.trim import trim
+from .data_visualization.boxplot import boxplot
+from .data_visualization.histogram import histogram
+from .data_visualization.scatter import scatter
+from .data import (
+    sp1500_cross_sectional, sp1500_panel, ceo_comp)
+from .stats.ci import ci
+from .stats.reg import reg
+
+# Add this line to expose the functions at package level
+__all__ = [
+    'trim', 'boxplot', 'histogram', 'ci', 
+    'sp1500_cross_sectional', 'sp1500_panel', 
+    'ceo_comp']
