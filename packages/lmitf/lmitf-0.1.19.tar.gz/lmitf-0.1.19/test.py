1aa5b848023920fe76e45844e09a3aae885880bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:18 +0000

    Add package lmitf/0.1.19/lmitf-0.1.19.tar.gz

---
+++ packages/lmitf/lmitf-0.1.19.tar.gz/lmitf-0.1.19/test.py
@@ -0,0 +1,36 @@
+# %%
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-4.1-mini",
+    input=[{
+        "role": "user",
+        "content": [
+            {"type": "input_text", "text": "what's in this image?"},
+            {
+                "type": "input_image",
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
+        ],
+    }],
+)
+
+print(response.output_text)
+#%%
+from lmitf import BaseLLM
+llm = BaseLLM()
+response = llm.call(
+    messages=[{
+        "role": "user",
+        "content": [
+            {"type": "input_text", "text": "what's in this image?"},
+            {
+                "type": "input_image",
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
+        ],
+    }]
+)
+#%%
