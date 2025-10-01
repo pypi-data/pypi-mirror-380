b75946d793393c4ea56d9ab7aeb9290eb5b4da20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 07:05:58 +0000

    Add package segy/0.5.1.post1/segy-0.5.1.post1-py3-none-any.whl

---
+++ packages/segy/segy-0.5.1.post1-py3-none-any.whl/segy/config.py
@@ -0,0 +1,54 @@
+"""SEG-Y parser configuration."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+from segy.schema import Endianness  # noqa: TCH001
+
+
+class SegyBaseSettings(BaseSettings):
+    """Base class for settings."""
+
+    model_config = SettingsConfigDict(extra="ignore", case_sensitive=False)
+
+
+class BinaryHeaderSettings(SegyBaseSettings):
+    """SEG-Y binary header parsing overrides.
+
+    Any value that is set to an integer will override what is parsed from
+    the binary header in the actual file.
+    """
+
+    ext_text_header: int | None = Field(
+        default=None, description="Override extended text headers."
+    )
+    revision: int | float | None = Field(
+        default=None, description="SEG-Y revision of the file."
+    )
+
+
+class SegySettings(SegyBaseSettings):
+    """SEG-Y file parsing settings."""
+
+    binary: BinaryHeaderSettings = Field(
+        default_factory=BinaryHeaderSettings,
+        description="Overrides for binary file header settings.",
+    )
+    endianness: Endianness | None = Field(
+        default=None,
+        description="Override the inferred endianness of the file.",
+    )
+    storage_options: dict[str, Any] = Field(
+        default_factory=dict,
+        description="Storage options to pass to the storage backend.",
+    )
+
+    model_config = SettingsConfigDict(
+        env_prefix="SEGY__",
+        env_nested_delimiter="__",
+    )
