f41c667642265d021a3dab4a892589bcc408cfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 05:06:37 +0000

    Add package pycex/0.0.2/pycex-0.0.2.tar.gz

---
+++ packages/pycex/pycex-0.0.2.tar.gz/pycex-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+from setuptools import setup
+
+# Remove build and dist directories if they exist
+for dir_name in ['build', 'dist']:
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+
+setup(
+    name='pycex',
+    version='0.0.2',
+    packages=['cex', 'cex/bnc', 'cex/ob'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dwdwow/pycex',
+)
