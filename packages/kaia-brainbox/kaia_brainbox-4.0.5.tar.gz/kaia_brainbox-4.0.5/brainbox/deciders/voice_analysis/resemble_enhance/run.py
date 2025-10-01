e9f5e99b4b15899d057c8110782e0bcf090358d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:12 +0000

    Add package kaia-brainbox/4.0.5/kaia_brainbox-4.0.5.tar.gz

---
+++ packages/kaia-brainbox/kaia_brainbox-4.0.5.tar.gz/kaia_brainbox-4.0.5/brainbox/deciders/voice_analysis/resemble_enhance/run.py
@@ -0,0 +1,7 @@
+from brainbox.deciders import ResembleEnhance
+
+if __name__ == '__main__':
+    installer = ResembleEnhance.Controller()
+    installer.install()
+    installer.self_test()
+    #installer.run_notebook()
