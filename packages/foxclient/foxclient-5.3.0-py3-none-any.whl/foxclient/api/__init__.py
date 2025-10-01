8a794c1ceb4574bb5db4427c96a23d33883534e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 07:05:43 +0000

    Add package foxclient/5.3.0/foxclient-5.3.0-py3-none-any.whl

---
+++ packages/foxclient/foxclient-5.3.0-py3-none-any.whl/foxclient/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from foxclient.api.device_api import DeviceApi
+from foxclient.api.paste_api import PasteApi
+from foxclient.api.release_api import ReleaseApi
+from foxclient.api.stats_api import StatsApi
+
