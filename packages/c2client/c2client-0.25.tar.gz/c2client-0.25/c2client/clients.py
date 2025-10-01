95ea640172e0e22316357133861c0e950ee3d00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:13 +0000

    Add package c2client/0.25/c2client-0.25.tar.gz

---
+++ packages/c2client/c2client-0.25.tar.gz/c2client-0.25/c2client/clients.py
@@ -0,0 +1,243 @@
+import argparse
+import json
+import re
+import ssl
+from abc import abstractmethod
+from functools import wraps
+from typing import Any, Dict, Optional
+
+import boto3
+import inflection
+
+from c2client.errors import InvalidMethodName
+from c2client.utils import from_dot_notation, get_env_var, convert_args
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def exitcode(func: callable):
+    """Wrapper for logging any caught exception."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            return e
+    return wrapper
+
+
+def parse_arguments():
+    """
+    Parse incoming action and arguments as a dictionary
+    for support AWS API requests format.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", help="The action that you want to perform.")
+    parser.add_argument(
+        "--no-verify-ssl",
+        action="store_false",
+        help="disable verifying ssl certificate",
+        required=False
+    )
+    parser.add_argument(
+        "parameters",
+        nargs="*",
+        help="Any parameters for the action. "
+             "Parameters specified by parameter key and "
+             "parameter value separated by space."
+    )
+    args = parser.parse_args()
+
+    params = args.parameters
+    no_verify_ssl = args.no_verify_ssl
+    parameters = dict(zip(params[::2], params[1::2]))
+
+    return args.action, parameters, no_verify_ssl
+
+
+class BaseClient:
+
+    url_key: str
+    client_name: str
+    use_base_access_key: bool = False
+
+    @classmethod
+    @abstractmethod
+    def make_request(cls, method: str, arguments: Dict[str, str], verify: bool):
+        """Run request."""
+
+        raise NotImplementedError
+
+    @classmethod
+    @exitcode
+    def execute(cls):
+        """Main function for API client."""
+
+        action, arguments, verify = parse_arguments()
+        response = cls.make_request(action, arguments, verify)
+
+        print(response)
+
+
+class C2Client(BaseClient):
+
+    @classmethod
+    def get_client(cls, verify: bool):
+        """Return boto3 client."""
+
+        endpoint = get_env_var(cls.url_key)
+
+        aws_access_key_id = get_env_var("AWS_ACCESS_KEY_ID")
+        base_access_key = get_env_var("BASE_ACCESS_KEY")
+        aws_secret_access_key = get_env_var("AWS_SECRET_ACCESS_KEY")
+
+        return boto3.client(
+            cls.client_name,
+            aws_access_key_id=base_access_key if cls.use_base_access_key else aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            region_name="croc",
+            endpoint_url=endpoint,
+            verify=verify,
+        )
+
+    @classmethod
+    def make_request(cls, method: str, arguments: Optional[Dict], verify: bool) -> str:
+
+        client = cls.get_client(verify)
+
+        if not hasattr(client, inflection.underscore(method)):
+            raise InvalidMethodName(method)
+
+        if arguments:
+            arguments = cls.convert_fields_names(arguments)
+            shape = client.meta.service_model.operation_model(method).input_shape
+            arguments = convert_args(from_dot_notation(arguments), shape)
+
+        result = getattr(client, inflection.underscore(method))(**arguments)
+
+        result.pop("ResponseMetadata", None)
+
+        # default=str is required for serializing Datetime objects
+        return json.dumps(result, indent=4, default=str)
+
+    @staticmethod
+    def convert_fields_names(arguments: dict) -> Dict[str, Any]:
+        """Convert field names as in the documentation."""
+
+        return arguments
+
+
+class EC2Client(C2Client):
+
+    url_key = "EC2_URL"
+    client_name = "ec2"
+
+    @staticmethod
+    def convert_fields_names(arguments: dict) -> Dict[str, Any]:
+        """Convert field names as in the documentation."""
+
+        tag_pattern = r"Filter\.\d+\.Value"
+
+        new_arguments = {}
+        filters = {}
+        for key, value in arguments.items():
+            new_key = key
+            if re.fullmatch(tag_pattern, key):
+                key = ".".join(key.split(".")[:3])
+
+                if key in filters:
+                    filters[key] += 1
+                else:
+                    filters[key] = 1
+
+                new_key = f"{key}.{filters[key]}"
+
+            new_arguments[new_key] = value
+
+        return new_arguments
+
+
+class CWClient(C2Client):
+
+    url_key = "AWS_CLOUDWATCH_URL"
+    client_name = "cloudwatch"
+
+    @staticmethod
+    def convert_fields_names(arguments: dict) -> Dict[str, Any]:
+        """Convert field names as in the documentation."""
+
+        new_arguments = {}
+        for key, value in arguments.items():
+            new_key = key.replace(".member.", ".")
+            new_arguments[new_key] = value
+        return new_arguments
+
+
+class CTClient(C2Client):
+
+    url_key = "AWS_CLOUDTRAIL_URL"
+    client_name = "cloudtrail"
+
+
+class ASClient(C2Client):
+
+    url_key = "AUTO_SCALING_URL"
+    client_name = "autoscaling"
+
+
+class BSClient(C2Client):
+
+    url_key = "BACKUP_URL"
+    client_name = "backup"
+
+
+class EKSClient(C2Client):
+
+    url_key = "EKS_URL"
+    client_name = "eks"
+
+
+class LegacyEKSClient(C2Client):
+
+    url_key = "LEGACY_EKS_URL"
+    client_name = "eks-legacy"
+
+
+class PaasClient(C2Client):
+
+    url_key = "PAAS_URL"
+    client_name = "paas"
+
+
+class ELBClient(C2Client):
+
+    url_key = "ELB_URL"
+    client_name = "elbv2"
+
+
+class Route53Client(C2Client):
+
+    url_key = "ROUTE53_URL"
+    client_name = "route53"
+
+
+class EFSClient(C2Client):
+
+    url_key = "EFS_URL"
+    client_name = "efs"
+
+
+class IAMClient(C2Client):
+
+    url_key = "IAM_URL"
+    client_name = "iam"
+    use_base_access_key = True
+
+
+class DirectConnectClient(C2Client):
+
+    url_key = "DIRECT_CONNECT_URL"
+    client_name = "directconnect"
---
+++ packages/c2client/c2client-0.25.tar.gz/c2client-0.25/setup.py
@@ -0,0 +1,75 @@
+from setuptools import setup, find_packages
+
+import os
+
+from c2client import __version__
+
+
+PACKAGE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_description():
+    with open(os.path.join(PACKAGE_PATH, "README.rst")) as readme:
+        return readme.read()
+
+
+install_requires = [
+    "boto3",
+    "botocore",
+    "inflection==0.3.1",
+]
+
+entrypoints = [
+    ("c2-as", "ASClient"),
+    ("c2-bs", "BSClient"),
+    ("c2-ct", "CTClient"),
+    ("c2-cw", "CWClient"),
+    ("c2-ec2", "EC2Client"),
+    ("c2-efs", "EFSClient"),
+    ("c2-eks", "EKSClient"),
+    ("c2-eks-legacy", "LegacyEKSClient"),
+    ("c2-elb", "ELBClient"),
+    ("c2-iam", "IAMClient"),
+    ("c2-paas", "PaasClient"),
+    ("c2-route53", "Route53Client"),
+    ("c2-dc", "DirectConnectClient"),
+]
+
+setup(
+    name="c2client",
+    version=__version__,
+    description="CROC Cloud Platform - API Client",
+    long_description=get_description(),
+    url="https://github.com/c2devel/c2-client",
+    license="GPL3",
+    author="CROC Cloud Team",
+    author_email="devel@croc.ru",
+    maintainer="Andrey Kulaev",
+    maintainer_email="adkulaev@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    install_requires=install_requires,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            f"{name} = c2client.clients:{client}.execute"
+            for name, client in entrypoints
+        ] + [
+            "c2rc-convert = c2client.c2rc_convert:main",
+        ]
+    },
+)
