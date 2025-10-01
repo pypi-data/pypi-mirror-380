c70ce6241adeb8b8575d5378c0117c1afb89abea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:06:44 +0000

    Add package evalassist/0.2.4/evalassist-0.2.4.tar.gz

---
+++ packages/evalassist/evalassist-0.2.4.tar.gz/evalassist-0.2.4/src/evalassist/judges/__init__.py
@@ -0,0 +1,68 @@
+import asyncio
+
+from .base import (
+    BaseDirectJudge,
+    BaseJudge,
+    BasePairwiseJudge,
+    UnitxtInferenceLangchainRunnable,
+)
+from .const import DEFAULT_JUDGE_INFERENCE_PARAMS
+from .direct_judge import DirectJudge
+from .dummy_judge import DummyDirectJudge, DummyPairwiseJudge
+from .mprometheus_judge import MPrometheusDirectJudge, MPrometheusPairwiseJudge
+from .types import (
+    Criteria,
+    CriteriaOption,
+    DirectInstance,
+    DirectInstanceResult,
+    DirectPositionalBias,
+    Instance,
+    MultiCriteria,
+    MultiCriteriaDirectInstanceResult,
+    MultiCriteriaItem,
+    PairwiseInstance,
+    PairwiseInstanceResult,
+    SingleSystemPairwiseResult,
+)
+from .unitxt_judges import GraniteGuardianJudge, UnitxtDirectJudge, UnitxtPairwiseJudge
+
+
+class SafeEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
+    def get_event_loop(self):
+        try:
+            return super().get_event_loop()
+        except RuntimeError:
+            loop = self.new_event_loop()
+            self.set_event_loop(loop)
+            return loop
+
+
+asyncio.set_event_loop_policy(SafeEventLoopPolicy())
+
+__all__: list[str] = [
+    "BaseJudge",
+    "DummyDirectJudge",
+    "DummyPairwiseJudge",
+    "DirectJudge",
+    "UnitxtDirectJudge",
+    "UnitxtPairwiseJudge",
+    "BaseDirectJudge",
+    "BasePairwiseJudge",
+    "Instance",
+    "DirectInstance",
+    "PairwiseInstance",
+    "SingleSystemPairwiseResult",
+    "PairwiseInstanceResult",
+    "DirectPositionalBias",
+    "DirectInstanceResult",
+    "DEFAULT_JUDGE_INFERENCE_PARAMS",
+    "Criteria",
+    "CriteriaOption",
+    "UnitxtInferenceLangchainRunnable",
+    "MPrometheusDirectJudge",
+    "MPrometheusPairwiseJudge",
+    "MultiCriteria",
+    "MultiCriteriaItem",
+    "MultiCriteriaDirectInstanceResult",
+    "GraniteGuardianJudge",
+]
