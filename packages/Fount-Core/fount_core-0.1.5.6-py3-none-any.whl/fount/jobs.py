161dd2aa978114060bb718379329254ef63f0ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 04:06:29 +0000

    Add package Fount-Core/0.1.5.6/fount_core-0.1.5.6-py3-none-any.whl

---
+++ packages/Fount-Core/fount_core-0.1.5.6-py3-none-any.whl/fount/jobs.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from typing import Any
+
+from fount._exceptions import TrainingError
+
+class Job(ABC):
+
+    @abstractmethod
+    def status(self,_transport):
+        ...
+
+    @abstractmethod
+    def metrics(self,_transport):
+        ...
+
+
+
+@dataclass
+class TrainingJob(Job):
+    id: str
+    _transport: Any
+
+    @classmethod
+    def run(cls,_transport,job_type,payload):
+        try:
+            response = _transport.start_job(job_type,payload)
+        except TrainingError:
+            return None
+        return cls(id=response["id"],_transport=_transport)
+
+    def status(self, _transport=None):
+        transport = _transport or self._transport
+        return transport.get_job_status(self.id)
+
+    def model(self,_transport=None):
+        transport = _transport or self._transport
+        return transport.get_model("training")
+
+    def metrics(self, _transport=None) :
+        transport = _transport or self._transport
+        return transport.get_job_metrics(self.id)
+
+
+@dataclass
+class TuningJob(Job):
+    id: str
+    _transport: Any
+
+    @classmethod
+    def run(cls,_transport,job_type,payload):
+        response = _transport.start_job(job_type,payload)
+        return cls(id=response["id"],_transport=_transport)
+
+    def status(self, _transport=None):
+        transport = _transport or self._transport
+        return transport.get_job_status(self.id)
+
+    def model(self,_transport=None):
+        transport = _transport or self._transport
+        return transport.get_model("tuning")
+
+    def metrics(self, _transport=None) :
+        transport = _transport or self._transport
+        return transport.get_job_metrics(self.id)
+
+
+@dataclass
+class InferenceJob(Job):
+    id: str
+    _transport: Any
+
+    @classmethod
+    def run(cls,_transport,job_type,payload):
+        response = _transport.start_job(job_type,payload)
+        return cls(id=response["id"],_transport=_transport)
+
+    def status(self, _transport=None):
+        transport = _transport or self._transport
+        return transport.get_job_status(self.id)
+
+    def metrics(self, _transport=None) :
+        transport = _transport or self._transport
+        return transport.get_job_metrics(self.id)
