89cd43f6df566e0eb549a4c96d72d64fca4d4254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 08:11:26 +0000

    Add package anzar/0.1.9/anzar-0.1.9-py3-none-any.whl

---
+++ packages/anzar/anzar-0.1.9-py3-none-any.whl/anzar/__init__.py
@@ -0,0 +1,49 @@
+import dotenv
+
+from anzar._api.jwt_interceptor import JwtInterceptor
+from anzar._api.session_interceptor import SessionInterceptor
+from anzar._auth.authenticator import AuthManager
+from anzar._models.anzar_config import (
+    AnzarConfig,
+    AuthStrategy,
+    Authentication,
+    Database,
+    DatabaseDriver,
+    EmailAndPassword,
+)
+
+from ._api.client import HttpClient
+
+_ = dotenv.load_dotenv()
+
+
+default_config = AnzarConfig(
+    api_url="http://localhost:3000",
+    database=Database(
+        connection_string="sqlite:memory:",
+        driver=DatabaseDriver.SQLite,
+    ),
+    auth=Authentication(
+        strategy=AuthStrategy.Session,
+    ),
+    emailAndPassword=EmailAndPassword(
+        enable=True,
+    ),
+)
+
+
+def AnzarAuth(config: AnzarConfig = default_config) -> AuthManager:
+    http_interceptor = (
+        SessionInterceptor()
+        if config.auth.strategy == AuthStrategy.Session
+        else JwtInterceptor()
+    )
+    return AuthManager(
+        HttpClient(
+            http_interceptor,
+        ),
+        config,
+    )
+
+
+__all__ = ["AnzarAuth"]
