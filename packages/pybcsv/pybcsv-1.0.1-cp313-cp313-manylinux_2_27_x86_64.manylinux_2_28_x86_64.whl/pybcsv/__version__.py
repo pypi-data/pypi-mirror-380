6ceb4fd84fdf1df0287c1294fd672b76eb5cd9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:47 +0000

    Add package pybcsv/1.0.1/pybcsv-1.0.1-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pybcsv/pybcsv-1.0.1-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pybcsv/__version__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 Tobias Weber <weber.tobias.md@gmail.com>
+# 
+# This file is part of the BCSV library.
+# 
+# Licensed under the MIT License. See LICENSE file in the project root 
+# for full license information.
+
+"""Version information for pybcsv."""
+
+__version__ = "1.0.1"
+__author__ = "Tobias Weber"
+__email__ = "weber.tobias.md@gmail.com"
+__license__ = "MIT"
