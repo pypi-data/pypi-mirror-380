6c0d074e8e8d32c136f3d1f297f3895c480459e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 04:06:23 +0000

    Add package splunk_add_on_ucc_framework/6.0.1/splunk_add_on_ucc_framework-6.0.1.tar.gz

---
+++ packages/splunk_add_on_ucc_framework/splunk_add_on_ucc_framework-6.0.1.tar.gz/splunk_add_on_ucc_framework-6.0.1/splunk_add_on_ucc_framework/package/appserver/static/js/build/Menu.BxkNNSpG.js
@@ -0,0 +1,1 @@
+import{g as r}from"./entry_page.js";import{e as o}from"./ErrorBoundary.xJor3Ppq.js";import{y as e}from"./Search.BNBTqs0I.js";var t=o();const m=r(t);var p=e();const u=r(p);export{m as D,u as M};
---
+++ packages/splunk_add_on_ucc_framework/splunk_add_on_ucc_framework-6.0.1.tar.gz/splunk_add_on_ucc_framework-6.0.1/splunk_add_on_ucc_framework/package/appserver/static/js/build/Search.CUubiSTp.js
@@ -0,0 +1,1 @@
+import{g as r}from"./entry_page.js";import{z as o}from"./Search.BNBTqs0I.js";var a=o();const s=r(a);export{s as S};
