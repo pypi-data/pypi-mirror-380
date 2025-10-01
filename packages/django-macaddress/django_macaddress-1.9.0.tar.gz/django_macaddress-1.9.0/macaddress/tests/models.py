f118be7926abca2b42bd40a59494d36dd8bcaa68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 07:05:55 +0000

    Add package django-macaddress/1.9.0/django_macaddress-1.9.0.tar.gz

---
+++ packages/django-macaddress/django_macaddress-1.9.0.tar.gz/django_macaddress-1.9.0/macaddress/tests/models.py
@@ -0,0 +1,14 @@
+"""
+A model for testing
+"""
+
+from django.db import models
+
+from macaddress.fields import MACAddressField
+
+
+class NetworkThingy(models.Model):
+    mac = MACAddressField()
+
+    def __str__(self):
+        return f"{self.mac}"
