9817d3ba283e9be48f5588cbfc6567d5c9dea118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:06:47 +0000

    Add package vector-mcp/0.1.6/vector_mcp-0.1.6-py3-none-any.whl

---
+++ packages/vector-mcp/vector_mcp-0.1.6-py3-none-any.whl/vector_mcp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+from vector_mcp.vector_mcp import vector_mcp
+
+"""
+vector-mcp
+
+Interchangeably use several vector databases as an MCP Server.
+"""
+
+
+__all__ = ["vector_mcp"]
