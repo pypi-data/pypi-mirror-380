302f47cd818da677b5901d1ed22bd764bb9ce510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:24:24 +0000

    Add package edx-release-notes/0.1.0/edx_release_notes-0.1.0-py2.py3-none-any.whl

---
+++ packages/edx-release-notes/edx_release_notes-0.1.0-py2.py3-none-any.whl/release_notes/urls.py
@@ -0,0 +1,10 @@
+"""
+URLs for release_notes.
+"""
+from django.urls import re_path  # pylint: disable=unused-import
+from django.views.generic import TemplateView  # pylint: disable=unused-import
+
+urlpatterns = [
+    # TODO: Fill in URL patterns and views here.
+    # re_path(r'', TemplateView.as_view(template_name="release_notes/base.html")),
+]
