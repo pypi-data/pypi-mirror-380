a09c77c345e46d75a296d75ec72cdabb2585fd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 04:06:39 +0000

    Add package libsemigroups-pybind11/1.1.0/libsemigroups_pybind11-1.1.0-cp313-cp313-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.1.0-cp313-cp313-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/libsemigroups_pybind11/blocks.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""
+This page contains the documentation for the ``blocks`` subpackage, that
+contains helper functions for the :any:`Blocks` class.
+"""
+
+from _libsemigroups_pybind11 import (  # pylint: disable=no-name-in-module,unused-import
+    blocks_underlying_partition as underlying_partition,
+    Blocks,
+)
