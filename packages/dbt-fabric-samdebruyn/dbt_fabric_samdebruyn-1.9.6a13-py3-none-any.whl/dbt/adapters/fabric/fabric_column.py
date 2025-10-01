5d8cbdf3874eaab1aa31b1995da413fb792a1323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:25:14 +0000

    Add package dbt-fabric-samdebruyn/1.9.6a13/dbt_fabric_samdebruyn-1.9.6a13-py3-none-any.whl

---
+++ packages/dbt-fabric-samdebruyn/dbt_fabric_samdebruyn-1.9.6a13-py3-none-any.whl/dbt/adapters/fabric/fabric_column.py
@@ -0,0 +1,29 @@
+from typing import Any, ClassVar, Dict
+
+from dbt.adapters.base import Column
+
+
+class FabricColumn(Column):
+    TYPE_LABELS: ClassVar[Dict[str, str]] = {
+        "STRING": "VARCHAR(MAX)",
+        "TIMESTAMP": "DATETIME2(6)",
+        "FLOAT": "FLOAT",
+        "INTEGER": "INT",
+        "BOOLEAN": "BIT",
+    }
+
+    @classmethod
+    def string_type(cls, size: int) -> str:
+        return f"varchar({size if size > 0 else 'max'})"
+
+    def literal(self, value: Any) -> str:
+        return "cast('{}' as {})".format(value, self.data_type)
+
+    def is_integer(self):
+        return super().is_integer() or self.dtype.lower() == "int"
+
+    @property
+    def data_type(self) -> str:
+        if self.dtype == "datetime2":
+            return f"datetime2({self.numeric_scale})"
+        return super().data_type
