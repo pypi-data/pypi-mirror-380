69be41b7908fc4e8b0986b317bf658e439f0fa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 08:11:39 +0000

    Add package devopsdriver/0.1.50/devopsdriver-0.1.50.tar.gz

---
+++ packages/devopsdriver/devopsdriver-0.1.50.tar.gz/devopsdriver-0.1.50/devopsdriver/__init__.py
@@ -0,0 +1,12 @@
+"""DevOps tools"""
+
+from .settings import Settings
+from .sendmail import send_email
+from .template import Template
+from .azdo.clients import Azure
+from .github.client import Github
+
+
+__version__ = "0.1.50"
+__author__ = "Marc Page"
+__credits__ = ""
