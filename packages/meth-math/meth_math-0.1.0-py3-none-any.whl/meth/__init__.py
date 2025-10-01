54b679b1fbd07a5aef88c52db411271b6f0bbf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 15:06:33 +0000

    Add package meth-math/0.1.0/meth_math-0.1.0-py3-none-any.whl

---
+++ packages/meth-math/meth_math-0.1.0-py3-none-any.whl/meth/__init__.py
@@ -0,0 +1,11 @@
+"""
+meth - A simple Python library for mathematical functions
+"""
+
+from .math_functions import sine
+
+__version__ = "0.1.0"
+__author__ = "jhon"
+__email__ = "adghb@gmail.com"
+
+__all__ = ["sine"]
