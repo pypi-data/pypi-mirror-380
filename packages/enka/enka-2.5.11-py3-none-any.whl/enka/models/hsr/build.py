f6377a84db41030c45f8ca9dec442184ef96d05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 03:25:01 +0000

    Add package enka/2.5.11/enka-2.5.11-py3-none-any.whl

---
+++ packages/enka/enka-2.5.11-py3-none-any.whl/enka/models/hsr/build.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from decimal import Decimal
+
+from pydantic import BaseModel, Field
+
+from .character import Character
+
+
+class Build(BaseModel):
+    """Represents a HSR build.
+
+    Attributes:
+        id: The build's ID.
+        name: The build's name.
+        order: The build's order.
+        live: Whether the build is live.
+        character_id: The build's character ID.
+        character: The build's character data.
+    """
+
+    id: int
+    name: str
+    order: Decimal
+    live: bool
+    character_id: int = Field(alias="avatar_id")
+    character: Character = Field(alias="avatar_data")
