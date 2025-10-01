c800d635939acb7d8db1bdee076485707c606a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 08:11:26 +0000

    Add package cowayaio/0.2.3.1/cowayaio-0.2.3.1.tar.gz

---
+++ packages/cowayaio/cowayaio-0.2.3.1.tar.gz/cowayaio-0.2.3.1/cowayaio/exceptions.py
@@ -0,0 +1,43 @@
+"""Exceptions for Coway IoCare."""
+
+from typing import Any
+
+
+class CowayError(Exception):
+    """Error from Coway api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+
+class AuthError(Exception):
+    """Authentication issue from Coway api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+
+class PasswordExpired(Exception):
+    """Coway API indicating password has expired."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args) 
+
+
+class ServerMaintenance(Exception):
+    """Coway API indicating servers are undergoing maintenance"""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+
+class RateLimited(Exception):
+    """Coway API indicating account has been rate-limited"""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
