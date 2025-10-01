089f7ff05d07d965b2675d16736ec62688b3ccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 14:07:32 +0000

    Add package pyautd3/36.0.2/pyautd3-36.0.2.tar.gz

---
+++ packages/pyautd3/pyautd3-36.0.2.tar.gz/pyautd3-36.0.2/pyautd3/modulation/audio_file/__init__.py
@@ -0,0 +1,8 @@
+from .csv_ import Csv, CsvOption
+from .wav import Wav
+
+__all__ = [
+    "Csv",
+    "CsvOption",
+    "Wav",
+]
---
+++ packages/pyautd3/pyautd3-36.0.2.tar.gz/pyautd3-36.0.2/pyautd3/native_methods/__init__.py
@@ -0,0 +1,44 @@
+import contextlib
+import os
+import platform
+import sys
+from pathlib import Path
+
+from .autd3capi import NativeMethods as Base
+from .autd3capi_gain_holo import NativeMethods as GainHolo
+from .autd3capi_link_ethercrab import NativeMethods as LinkEtherCrab
+from .autd3capi_link_remote import NativeMethods as LinkRemote
+from .autd3capi_link_simulator import NativeMethods as LinkSimulator
+from .autd3capi_link_twincat import NativeMethods as LinkTwincAT
+from .autd3capi_modulation_audio_file import NativeMethods as ModulationAudioFile
+
+_PLATFORM = platform.system()
+_PREFIX = ""
+_BIN_EXT = ""
+if _PLATFORM == "Windows":
+    _BIN_EXT = ".dll"
+elif _PLATFORM == "Darwin":
+    _PREFIX = "lib"
+    _BIN_EXT = ".dylib"
+elif _PLATFORM == "Linux":
+    _PREFIX = "lib"
+    _BIN_EXT = ".so"
+else:
+    err = "Not supported OS"
+    raise ImportError(err)
+
+_LIB_PATH = Path(__file__).parent.parent / "bin"
+
+Base().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
+GainHolo().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
+ModulationAudioFile().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
+LinkSimulator().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
+LinkRemote().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
+if sys.platform == "win32":
+    with contextlib.suppress(FileNotFoundError):
+        os.add_dll_directory("C:\\TwinCAT\\Common64")
+    with contextlib.suppress(FileNotFoundError):
+        os.add_dll_directory("C:\\Program Files (x86)\\Beckhoff\\TwinCAT\\Common64")
+with contextlib.suppress(Exception):
+    LinkTwincAT().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
+    LinkEtherCrab().init_dll(_LIB_PATH, _PREFIX, _BIN_EXT)
