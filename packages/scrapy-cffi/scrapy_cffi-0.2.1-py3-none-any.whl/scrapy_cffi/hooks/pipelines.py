b8c5f6508b6108f41b52478708f9b0f6c24a2e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-01 16:06:47 +0000

    Add package scrapy-cffi/0.2.1/scrapy_cffi-0.2.1-py3-none-any.whl

---
+++ packages/scrapy-cffi/scrapy_cffi-0.2.1-py3-none-any.whl/scrapy_cffi/hooks/pipelines.py
@@ -0,0 +1,21 @@
+from typing import Protocol, TYPE_CHECKING, Dict
+if TYPE_CHECKING:
+    from ..models.api import SingalInfo
+
+class SignalHooks(Protocol):
+    def send(self, signal: object, data: "SingalInfo") -> None: ...
+
+class _SessionHooks(Protocol):
+    def mark_end(self, session_id: str) -> None: ...
+
+    def get_session_cookies(self, session_id: str) -> Dict: ...
+
+class SessionHooks(Protocol):
+    def get_session_cookies(self, session_id: str) -> Dict: ...
+
+class _PipelinesHooks(Protocol):
+    session: _SessionHooks
+
+class PipelinesHooks(Protocol):
+    session: SessionHooks
+    signals: SignalHooks
