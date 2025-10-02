c0dc74beb1611a421aa645970d48eaa3a9054e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:06:45 +0000

    Add package extrator-xml-fiscal/1.0.1/extrator_xml_fiscal-1.0.1-py3-none-any.whl

---
+++ packages/extrator-xml-fiscal/extrator_xml_fiscal-1.0.1-py3-none-any.whl/extrator_xml_fiscal/__init__.py
@@ -0,0 +1,11 @@
+from .extrator_nfe import ExtratorNFe
+from .extrator_cancelamento import ExtratorCancelamento
+from .extrator_carta_correcao import ExtratorCartaCorrecao
+from .extrator_cte import ExtratorCTe
+
+__all__ = [
+    'ExtratorNFe',
+    'ExtratorCancelamento',
+    'ExtratorCartaCorrecao',
+    'ExtratorCTe',
+]
