2e13d520578f27d27dddf77ebd0c5f241478eb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:15:33 +0000

    Add package mixedbread/0.32.0/mixedbread-0.32.0-py3-none-any.whl

---
+++ packages/mixedbread/mixedbread-0.32.0-py3-none-any.whl/mixedbread/types/scored_video_url_input_chunk.py
@@ -0,0 +1,54 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["ScoredVideoURLInputChunk", "VideoURL"]
+
+
+class VideoURL(BaseModel):
+    url: str
+    """The video URL. Can be either a URL or a Data URI."""
+
+
+class ScoredVideoURLInputChunk(BaseModel):
+    chunk_index: int
+    """position of the chunk in a file"""
+
+    mime_type: Optional[str] = None
+    """mime type of the chunk"""
+
+    generated_metadata: Optional[Dict[str, object]] = None
+    """metadata of the chunk"""
+
+    model: Optional[str] = None
+    """model used for this chunk"""
+
+    score: float
+    """score of the chunk"""
+
+    file_id: str
+    """file id"""
+
+    filename: str
+    """filename"""
+
+    store_id: str
+    """store id"""
+
+    metadata: Optional[object] = None
+    """file metadata"""
+
+    type: Optional[Literal["video_url"]] = None
+    """Input type identifier"""
+
+    transcription: Optional[str] = None
+    """speech recognition (sr) text of the video"""
+
+    summary: Optional[str] = None
+    """summary of the video"""
+
+    video_url: VideoURL
+    """The video input specification."""
