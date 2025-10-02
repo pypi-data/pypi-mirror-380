e8ea22a45df28e60edaa4c2dfd512874792515cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:16:22 +0000

    Add package NearBeach/0.31.99/nearbeach-0.31.99.tar.gz

---
+++ packages/NearBeach/nearbeach-0.31.99.tar.gz/nearbeach-0.31.99/vitest.config.js
@@ -0,0 +1,36 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [
+        vue()
+    ],
+    test: {
+        coverage: {
+            enabled: true,
+            exclude: [
+                `NearBeach/static/**/*`,
+                `venv/**/*`,
+                `node_modules/**/*`
+            ],
+            include: [
+                'src/js/components/**/*.vue',
+                'src/js/vuex/**/*.js'
+            ],
+            provider: "v8",
+            // provider: "istanbul",
+            reporter: ['text','html','json']
+        },
+        environment: "jsdom",
+        exclude: [
+            `./src/js/components.js`,
+            `./src/js/composables/**/*`,
+        ],
+        include: [
+            `./tests/unit/**/*.unit.js`,
+        ],
+        minThreads: 2,
+        maxThreads: 5
+    },
+})
