5742f00ff49199b0648246d9406b6e4c9cfec10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:15:42 +0000

    Add package invenio-subjects-cessda/2.0.0/invenio_subjects_cessda-2.0.0-py3-none-any.whl

---
+++ packages/invenio-subjects-cessda/invenio_subjects_cessda-2.0.0-py3-none-any.whl/invenio_subjects_cessda/convert.py
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2022-2024 KTH Royal Institute of Technology Sweden.
+#
+# invenio-subjects-CESSDA is free software, you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file details.
+
+
+# from typing import Any, Callable, Dict, Iterator, NoReturn
+
+
+from click import secho
+from yaml import dump
+
+from invenio_subjects_cessda.schemas import cessda_schema
+from invenio_subjects_cessda.utils import logger
+
+
+def sort_vocabularies(data):
+    """Sort vocabularies by 'id'."""
+    logger.debug("Sorting ...")
+    return sorted(
+        [(v["name"], s) for v in data for s in v["data"]], key=lambda x: x[1]["id"]
+    )
+
+
+def process_vocabularies(sorted_vocabularies):
+    """Process sorted vocabularies."""
+    logger.debug("Processing schema ...")
+    return [
+        cessda_schema((name, entry))
+        for name, entry in sorted_vocabularies
+        if cessda_schema((name, entry))
+    ]
+
+
+def write_to_file(processed_data, dpath):
+    """Write processed data to a file."""
+    logger.debug("Writing to: '%s'", dpath)
+    with open(dpath, "w", encoding="utf-8") as yaml_f:
+        for data in processed_data:
+            dump(data, yaml_f, allow_unicode=True, sort_keys=False)
+
+
+def log_conversion_success(dpath):
+    """Log success message."""
+    secho("Converted successfully to ", fg="green", nl=False)
+    secho(f" {dpath}", fg="yellow", bold=True)
+
+
+def convert_vocabularies(data, dpath):
+    """Convert vocabularies to yaml."""
+    logger.debug("Convert vocabularies started ...")
+    sorted_vocabularies = sort_vocabularies(data)
+    processed_data = process_vocabularies(sorted_vocabularies)
+    write_to_file(processed_data, dpath)
+    log_conversion_success(dpath)
---
+++ packages/invenio-subjects-cessda/invenio_subjects_cessda-2.0.0-py3-none-any.whl/invenio_subjects_cessda/fetch_voc.py
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2022-2024 KTH Royal Institute of Technology Sweden.
+#
+# invenio-subjects-CESSDA is free software, you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file details.
+
+
+from asyncio import gather, run
+
+from aiohttp import ClientSession
+
+from invenio_subjects_cessda.utils import logger
+
+
+# Data fetching
+async def fetch(session, url_obj):
+    """Fetch API calls."""
+    logger.debug("Making request to '%s'", url_obj["endpoint"])
+    try:
+        async with session.get(url_obj["endpoint"]) as res:
+            data = await res.json()
+            return {"name": url_obj["name"], "data": data}
+    except Exception as e:
+        logger.error("Error fetching %s: %s", url_obj["name"], e)
+        return None
+
+
+async def gather_data(urls):
+    """Gather workers."""
+    async with ClientSession() as session:
+        tasks = [fetch(session, url) for url in urls]
+        results = await gather(*tasks, return_exceptions=True)
+        return [result for result in results if result is not None]
+
+
+def fetch_voc(urls):
+    """Fetch CESSDA voc and return a list."""
+    return run(gather_data(urls))
