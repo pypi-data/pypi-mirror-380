543786b01799f061f040fb256c8173cf3d229da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:07:24 +0000

    Add package react-tk/0.4.0/react_tk-0.4.0.tar.gz

---
+++ packages/react-tk/react_tk-0.4.0.tar.gz/react_tk-0.4.0/react_tk/tk/nodes/tool_tip_label.py
@@ -0,0 +1,22 @@
+import tkinter
+from react_tk.rendering.actions.compute import AnyNode
+from react_tk.rendering.actions.node_reconciler import reconciler
+from react_tk.rendering.actions.reconcile_state import RenderedNode
+from react_tk.tk.nodes.label import Label, LabelReconciler
+
+from react_tk.tk.win32.tweaks import make_clickthrough
+
+
+class ToolTipLabelReconciler(LabelReconciler):
+
+    def _create(
+        self, container: "tkinter.Misc", node: AnyNode
+    ) -> RenderedNode["tkinter.Widget"]:
+        label = super()._create(container, node)
+        make_clickthrough(label.resource)
+        return label
+
+
+@reconciler(ToolTipLabelReconciler)
+class ToolTipLabel(Label):
+    pass
