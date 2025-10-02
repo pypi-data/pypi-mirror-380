5e3d064d4bc4993e083844bd66f14de959575d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:16:26 +0000

    Add package mymathutils1/0.1.0/mymathutils1-0.1.0-py3-none-any.whl

---
+++ packages/mymathutils1/mymathutils1-0.1.0-py3-none-any.whl/mymathutils1/__init__.py
@@ -0,0 +1,5 @@
+from .core import add, multiply, subtract
+
+__all__ = ["add", "multiply", "subtract"]
+
+__version__ = "0.1.0"
---
+++ packages/mymathutils1/mymathutils1-0.1.0-py3-none-any.whl/mymathutils1/core.py
@@ -0,0 +1,12 @@
+#import pandas as pd
+def add(x, y):
+    """Return sum of two numbers."""
+    return x + y
+
+def multiply(x, y):
+    """Return product of two numbers."""
+    return x * y
+
+def subtract(x, y):
+    """Return difference of two numbers."""
+    return x - y
