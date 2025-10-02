aa4cddb6d913f83c8f1c8bc6c22edff0a0a47ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:07:18 +0000

    Add package viperlog/0.2.88/viperlog-0.2.88.tar.gz

---
+++ packages/viperlog/viperlog-0.2.88.tar.gz/viperlog-0.2.88/src/viperlog/formatters/json_string.py
@@ -0,0 +1,20 @@
+
+import json
+from logging import LogRecord
+from typing import Optional
+
+from .base import BaseFormatter
+from .dict_formatter import DictFormatter
+from .template import IMessageTemplate
+
+
+class JsonStringFormatter(BaseFormatter[str]):
+    def __init__(self, template:Optional[str|IMessageTemplate] = None):
+        super().__init__()
+        self._helper = DictFormatter(template)
+
+
+    def format(self, record: LogRecord) -> str:
+        # call dict_formatter to keep creating the props the same for most
+        result = self._helper.format(record)
+        return json.dumps(result)
