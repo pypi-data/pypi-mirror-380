194503cf5ec3542b84a29b44e01f9ab732a8b511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:06:17 +0000

    Add package ms-swift/3.8.3/ms_swift-3.8.3-py3-none-any.whl

---
+++ packages/ms-swift/ms_swift-3.8.3-py3-none-any.whl/swift/megatron/trainers/trainer.py
@@ -0,0 +1,124 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+from functools import partial
+from typing import List, Optional
+
+import torch
+import torch.nn
+from megatron.core import mpu
+from megatron.core.rerun_state_machine import get_rerun_state_machine
+from megatron.training import get_args, get_timers
+from torch.distributed.nn import all_reduce
+
+from swift.utils import get_logger
+from .base import BaseMegatronTrainer
+from .utils import get_batch
+
+logger = get_logger()
+
+
+class MegatronTrainer(BaseMegatronTrainer):
+
+    # Code borrowed from NVIDIA/Megatron-LM
+    def loss_func(self,
+                  output_tensor: torch.Tensor,
+                  *,
+                  labels: torch.Tensor,
+                  loss_scale: Optional[torch.Tensor] = None,
+                  channels: Optional[List[str]] = None,
+                  packed_seq_params=None):
+        args = get_args()
+
+        losses = output_tensor.float()
+        loss_mask = labels != -100
+        if args.enable_dft_loss:
+            losses = losses * torch.exp(-losses.detach())
+        if loss_scale is not None:
+            losses = losses * loss_scale
+        if args.enable_channel_loss and channels is not None:
+            assert losses.shape[0] == 1, 'only support padding_free'
+            mode = 'train' if self.unwrapped_model.training else 'eval'
+            metrics = self.custom_metrics[mode]
+            num_samples = packed_seq_params.num_samples
+            cu_seqlens = packed_seq_params.cu_seqlens_q[:num_samples + 1] // args.context_parallel_size
+            for i in range(cu_seqlens.shape[0] - 1):
+                channel = channels[i]
+                slice_ = slice(cu_seqlens[i], cu_seqlens[i + 1])
+                metrics[f'loss_{channel}'].update(losses[0, slice_][loss_mask[0, slice_]])
+
+        loss = torch.cat([torch.sum(losses * loss_mask).view(1), loss_mask.sum().view(1)])
+
+        if args.context_parallel_size > 1 and not self.megatron_core_013:
+            loss = all_reduce(loss, group=mpu.get_context_parallel_group())
+
+        # Check individual rank losses are not NaN prior to DP all-reduce.
+        rerun_state_machine = get_rerun_state_machine()
+        if args.check_for_nan_in_loss_and_grad:
+            rerun_state_machine.validate_result(
+                result=loss[0],
+                rejection_func=torch.isnan,
+                message='found NaN in local forward loss calculation',
+                tolerance=0.0,  # forward pass calculations are determinisic
+                fatal=True,
+            )
+            rerun_state_machine.validate_result(
+                result=loss[0],
+                rejection_func=torch.isinf,
+                message='found Inf in local forward loss calculation',
+                tolerance=0.0,  # forward pass calculations are determinisic
+                fatal=True,
+            )
+        # Check for spiky loss
+        if args.check_for_spiky_loss:
+            # define spiky loss as a loss that's 10x the max loss observed
+            SPIKY_LOSS_FACTOR = 10
+            rerun_state_machine.validate_result(
+                result=loss[0],
+                rejection_func=partial(
+                    rerun_state_machine.is_unexpectedly_large,
+                    threshold=SPIKY_LOSS_FACTOR,
+                    context='loss',
+                ),
+                message='Spiky loss',
+                tolerance=0.0,  # forward pass calculations are determinisic
+                fatal=False,
+            )
+        # Reduce loss for logging.
+        reporting_loss = loss.clone().detach()
+        lm_loss = loss[0]
+        if not self.megatron_core_013:
+            # fix megatron-lm bug
+            # https://github.com/NVIDIA/Megatron-LM/blob/core_r0.12.0/megatron/core/pipeline_parallel/schedules.py#L291
+            torch.distributed.all_reduce(reporting_loss, group=mpu.get_data_parallel_group())
+            lm_loss = lm_loss / mpu.get_context_parallel_world_size()
+            reporting_loss = (reporting_loss[0], reporting_loss[1])
+        else:
+            lm_loss = lm_loss.clone()
+        local_num_tokens = loss[1].clone().detach().to(torch.int)
+        return (
+            lm_loss,
+            local_num_tokens,
+            {
+                'lm loss': reporting_loss
+            },
+        )
+
+    def forward_step(self, data_iterator, model):
+        timers = get_timers()
+
+        # Get the batch.
+        timers('batch-generator', log_level=2).start()
+        with self.stimer(bdata=True):
+            data = get_batch(data_iterator)
+        timers('batch-generator').stop()
+        loss_scale = data.pop('loss_scale', None)
+        channels = data.pop('channel', None)
+        with self.stimer:
+            output_tensor = model(**data)
+        labels = data.get('labels')
+        packed_seq_params = data.get('packed_seq_params')
+        return output_tensor, partial(
+            self.loss_func,
+            labels=labels,
+            loss_scale=loss_scale,
+            channels=channels,
+            packed_seq_params=packed_seq_params)
---
+++ packages/ms-swift/ms_swift-3.8.3-py3-none-any.whl/swift/megatron/trainers/utils.py
@@ -0,0 +1,153 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+from typing import Any, Dict
+
+import torch
+from megatron.core import mpu
+from megatron.core.packed_seq_params import PackedSeqParams
+from megatron.core.utils import get_batch_on_this_cp_rank as mcore_get_batch_on_this_cp_rank
+from megatron.training import get_args
+
+from swift.llm import get_packed_seq_params as _get_packed_seq_params
+from swift.llm import to_device
+from swift.utils import get_current_device
+
+
+def get_swift_datasets_provider(train_dataset, val_dataset):
+
+    def swift_datasets_provider(train_val_test_num_samples):
+        nonlocal val_dataset
+        args = get_args()
+        data_parallel_size = mpu.get_data_parallel_world_size()
+        step_batch_size = args.micro_batch_size * data_parallel_size
+        # To avoid errors caused by the validation set being insufficient to complete a single step.
+        if val_dataset is not None and hasattr(val_dataset, '__len__') and len(val_dataset) < step_batch_size:
+            val_dataset = None
+        return train_dataset, val_dataset, None
+
+    return swift_datasets_provider
+
+
+# Code borrowed from NVIDIA/Megatron-LM
+def get_batch_on_this_tp_rank(data_iterator):
+    args = get_args()
+
+    data = next(data_iterator)
+    is_finished = data.pop('is_finished', False)
+    data['labels'] = torch.roll(data['labels'], -1, dims=-1)
+    if 'loss_scale' in data:
+        data['loss_scale'] = torch.roll(data['loss_scale'], -1, dims=-1)
+    batch = to_device(data, 'cuda', non_blocking=True)
+    if args.pipeline_model_parallel_size == 1:
+        pass
+    elif mpu.is_pipeline_first_stage():
+        batch['labels'] = None
+        batch['loss_scale'] = None
+    elif mpu.is_pipeline_last_stage():
+        batch['input_ids'] = None
+    else:
+        for key in ('input_ids', 'labels', 'loss_scale'):
+            batch[key] = None
+
+    if is_finished:
+        args.train_iters = args.curr_iteration + 1
+
+    return batch
+
+
+def get_packed_seq_params(position_ids: torch.Tensor) -> PackedSeqParams:
+    params = _get_packed_seq_params(position_ids)
+    return PackedSeqParams(
+        cu_seqlens_q=params['cu_seq_lens_q'],
+        cu_seqlens_kv=params['cu_seq_lens_k'],
+        max_seqlen_q=params['max_length_q'],
+        max_seqlen_kv=params['max_length_k'],
+        qkv_format='thd')
+
+
+def _split_tokens(tokens, cu_seqlens):
+    assert tokens.shape[-2] == 1, f'tokens.shape: {tokens.shape}'  # [..., 1, L]
+    new_tokens = []
+    cp_size = mpu.get_context_parallel_world_size()
+    cp_rank = mpu.get_context_parallel_rank()
+    for i in range(cu_seqlens.shape[0] - 1):
+        val = tokens[..., cu_seqlens[i]:cu_seqlens[i + 1]]
+        val = val.view(
+            *tokens.shape[:-1],
+            2 * cp_size,
+            val.shape[-1] // (2 * cp_size),
+        )
+        index = torch.tensor([cp_rank, (2 * cp_size - cp_rank - 1)], device='cpu',
+                             pin_memory=True).cuda(non_blocking=True)
+        val = val.index_select(-2, index)
+        new_tokens.append(val.view(*tokens.shape[:-1], -1))
+    return torch.cat(new_tokens, dim=-1)
+
+
+def _split_tokens_decoder_input(tokens, cu_seqlens):
+    assert tokens.shape[1] == 1, f'tokens.shape: {tokens.shape}'  # [L, 1, E]
+    new_tokens = []
+    cp_size = mpu.get_context_parallel_world_size()
+    cp_rank = mpu.get_context_parallel_rank()
+    for i in range(cu_seqlens.shape[0] - 1):
+        val = tokens[cu_seqlens[i]:cu_seqlens[i + 1], ...]
+        val = val.view(
+            2 * cp_size,
+            val.shape[0] // (2 * cp_size),
+            *tokens.shape[1:],
+        )
+        index = torch.tensor([cp_rank, (2 * cp_size - cp_rank - 1)], device='cpu',
+                             pin_memory=True).cuda(non_blocking=True)
+        val = val.index_select(0, index)
+        new_tokens.append(val.view(-1, *tokens.shape[1:]))
+    return torch.cat(new_tokens, dim=0)
+
+
+def get_batch_on_this_cp_rank(batch: Dict[str, Any]):
+    """Slice batch input along sequence dimension into multiple chunks,
+    which are parallelized across GPUs in a context parallel group.
+    """
+
+    # With causal masking, each token only attends to its prior tokens. Simply split
+    # sequence into CP chunks can result in severe load imbalance. That's to say, chunks
+    # at the end of sequence have bigger workload than others. To address this issue,
+    # we split sequence into 2*CP ranks. Assuming CP=2, we then get 4 chunks, chunk_0
+    # and chunk_3 are assigned to GPU0, chunk_1 and chunk_2 are assigned to GPU1, so
+    # that we can get balanced workload among GPUs in a context parallel group.
+    cp_size = mpu.get_context_parallel_world_size()
+    if cp_size > 1:
+        args = get_args()
+        keys = ['labels', 'attention_mask', 'position_ids', 'loss_scale']
+        if args.model_meta.is_multimodal:
+            keys.append('decoder_input')
+        else:
+            keys.append('input_ids')
+        packed_seq_params = batch.get('packed_seq_params')
+        if packed_seq_params is None:
+            return mcore_get_batch_on_this_cp_rank(batch)
+        for key, val in batch.items():
+            if key not in keys:
+                continue
+            if val is not None:
+                if key == 'decoder_input':
+                    batch[key] = _split_tokens_decoder_input(val, packed_seq_params.cu_seqlens_q)
+                else:
+                    batch[key] = _split_tokens(val, packed_seq_params.cu_seqlens_q)
+
+    return batch
+
+
+def get_batch(data_iterator):
+    """Generate a batch."""
+    # get batches based on the TP rank you are on
+    batch = get_batch_on_this_tp_rank(data_iterator)
+    args = get_args()
+    num_samples = batch.pop('num_samples')
+    text_position_ids = batch.pop('text_position_ids', None)
+    if text_position_ids is None:
+        text_position_ids = batch.get('position_ids')
+    if args.padding_free and text_position_ids is not None:
+        batch['packed_seq_params'] = get_packed_seq_params(text_position_ids)
+        batch['packed_seq_params'].num_samples = num_samples
+    # slice batch along sequence dimension for context parallelism
+    batch = get_batch_on_this_cp_rank(batch)
+    return batch
