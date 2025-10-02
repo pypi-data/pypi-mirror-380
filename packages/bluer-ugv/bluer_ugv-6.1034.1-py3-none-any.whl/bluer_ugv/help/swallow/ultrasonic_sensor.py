228c2050513ebdf2c40a5f6fe16eaca70dd477ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:15:49 +0000

    Add package bluer-ugv/6.1034.1/bluer_ugv-6.1034.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.1034.1-py3-none-any.whl/bluer_ugv/help/swallow/ultrasonic_sensor.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from bluer_options.terminal import show_usage, xtra
+
+
+def help_review(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = xtra("~download,upload", mono=mono)
+
+    args = [
+        "[--gif 1]",
+        "[--frame_count <100>]",
+    ]
+
+    return show_usage(
+        [
+            "@swallow",
+            "ultrasonic",
+            "review",
+            f"[{options}]",
+            "[.|<object-name>]",
+        ]
+        + args,
+        "review ultrasonic sensor data.",
+        mono=mono,
+    )
+
+
+def help_test(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = xtra("~upload", mono=mono)
+
+    args = [
+        "[--export 0]",
+        "[--log 0]",
+        "[--max_m 0.8]",
+    ]
+
+    return show_usage(
+        [
+            "@swallow",
+            "ultrasonic",
+            "test",
+            f"[{options}]",
+            "[-|<object-name>]",
+        ]
+        + args,
+        "test ultrasonic sensors.",
+        mono=mono,
+    )
+
+
+help_functions = {
+    "review": help_review,
+    "test": help_test,
+}
---
+++ packages/bluer-ugv/bluer_ugv-6.1034.1-py3-none-any.whl/bluer_ugv/swallow/session/classical/ultrasonic_sensor/testing.py
@@ -0,0 +1,65 @@
+# continues -v7
+
+from typing import List
+
+from bluer_objects import file
+from bluer_objects import objects
+
+from bluer_ugv.logger import logger
+from bluer_ugv.swallow.session.classical.ultrasonic_sensor.detection import (
+    Detection,
+)
+from bluer_ugv.swallow.session.classical.ultrasonic_sensor.log import (
+    UltrasonicSensorDetectionLog,
+)
+
+
+def test(
+    object_name: str,
+    max_m: float = 0.8,
+    export: bool = True,
+    export_gif: bool = False,
+    log: bool = True,
+    line_width: int = 80,
+) -> bool:
+    from RPi import GPIO
+
+    from bluer_ugv.swallow.session.classical.ultrasonic_sensor.pack import (
+        UltrasonicSensorPack,
+    )
+
+    ultrasonic_sensor_pack = UltrasonicSensorPack(max_m=max_m)
+    if not ultrasonic_sensor_pack.valid:
+        return False
+
+    detection_log = UltrasonicSensorDetectionLog()
+
+    success = True
+    try:
+        while True:
+            success, detection = ultrasonic_sensor_pack.detect(log=log)
+            if not success:
+                break
+
+            if export:
+                detection_log.append(detection)
+    except KeyboardInterrupt:
+        logger.info("^C detected.")
+    finally:
+        GPIO.cleanup()
+
+    if not export:
+        return success
+
+    if not success:
+        return success
+
+    if not detection_log.export(
+        object_name=object_name,
+        line_width=line_width,
+        export_gif=export_gif,
+        log=log,
+    ):
+        return False
+
+    return detection_log.save(object_name=object_name)
