f1e9b8e37c65fa78f853b5c02ac8efd8fdb4d37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:15:49 +0000

    Add package binarytrees-nv/0.1.4/binarytrees_nv-0.1.4.tar.gz

---
+++ packages/binarytrees-nv/binarytrees_nv-0.1.4.tar.gz/binarytrees_nv-0.1.4/README.md
@@ -0,0 +1,34 @@
+This is a package providing recursive and iterative binary tree generators. Each nodes' values are generated using provided functions. (By default, the leaf values are equal to the root value.)
+Don't take it too seriously. It is a demo package, but still can be used as a library.
+
+# Installation
+```bash
+pip install binarytrees_nv
+```
+
+## Usage
+```python
+from binarytrees_nv import gen_bin_tree_iterative, gen_bin_tree_recursive
+
+height: int = 4
+
+# recursive
+recursive_tree = gen_bin_tree_recursive(height)
+# or iterative
+iterative_tree = gen_bin_tree_iterative(height)
+
+print(recursive_tree)
+print(iterative_tree)
+
+
+# Custom functions for leaf values
+def left_function(x):
+    return x ** 2
+
+def right_function(x):
+    return x - 2
+
+custom_tree = gen_bin_tree_iterative(height, left_function=left_function, right_function=right_function)
+
+print(custom_tree)
+```
---
+++ packages/binarytrees-nv/binarytrees_nv-0.1.4.tar.gz/binarytrees_nv-0.1.4/src/binarytrees_nv/binarytrees_nv.py
@@ -0,0 +1,128 @@
+from collections import deque
+
+from .exceptions import (
+    InvalidTreeFunctions,
+    InvalidTreeHeight,
+    InvalidTreeRoot,
+)
+
+
+def leaf_function(x):
+    """Example function to generate the left/right leaf of a binary tree given a node."""
+    return x**2
+
+
+def gen_bin_tree_recursive(
+    height: int,
+    root: int = 1,
+    left_function=lambda x: x,
+    right_function=lambda x: x,
+):
+    """
+    Generates a binary tree recursively given a height and a root node.
+
+    Args:
+        height (int): The height of the tree.
+        root (int): The root node of the tree.
+        left_function (callable): The function to generate the left leaf of the tree. Default is lambda x: x.
+        right_function (callable): The function to generate the right leaf of the tree. Default is lambda x: x.
+
+    Raises:
+        InvalidTreeHeight: If the height is not an integer.
+        InvalidTreeRoot: If the root is not an integer.
+        InvalidTreeFunctions: If either left_function or right_function is not callable.
+
+    Returns:
+        The generated binary tree. If height is 0 or less, returns None.
+
+    """
+    if not isinstance(height, int):
+        raise InvalidTreeHeight(height)  # Custom exception
+    if not isinstance(root, int):
+        raise InvalidTreeRoot(root)  # Custom exception
+
+    if height <= 0:
+        return None
+
+    if not callable(left_function) or not callable(right_function):
+        raise InvalidTreeFunctions(left_function, right_function)  # Custom exception
+
+    left_leaf: int = left_function(root)
+    right_leaf: int = right_function(root)
+
+    return {
+        str(root): [
+            gen_bin_tree_recursive(
+                height - 1,
+                left_leaf,
+                left_function,
+                right_function,
+            ),
+            gen_bin_tree_recursive(
+                height - 1,
+                right_leaf,
+                left_function,
+                right_function,
+            ),
+        ],
+    }
+
+
+def gen_bin_tree_iterative(
+    height: int,
+    root: int = 1,
+    left_function=lambda x: x,
+    right_function=lambda x: x,
+):
+    """
+    Generates a binary tree iteratively given a height and a root node.
+
+    Args:
+        height (int): The height of the tree. Must be a non-negative integer.
+        root (int): The root node of the tree. Must be an integer.
+        left_function (callable): The function to generate the left leaf of the tree. Default is lambda x: x.
+        right_function (callable): The function to generate the right leaf of the tree. Default is lambda x: x.
+
+    Raises:
+        InvalidTreeHeight: If the height is not an integer.
+        InvalidTreeRoot: If the root is not an integer.
+        InvalidTreeFunctions: If either left_function or right_function is not callable.
+
+    Returns:
+        The generated binary tree in dictionary form. If height is 0 or less, returns None.
+
+    """
+    if not isinstance(height, int):
+        raise InvalidTreeHeight(height)
+    if not isinstance(root, int):
+        raise InvalidTreeRoot(root)
+
+    if height <= 0:
+        return None
+
+    if not callable(left_function) or not callable(right_function):
+        raise InvalidTreeFunctions(left_function, right_function)
+
+    tree = {}
+    stack = deque([(root, height, tree)])
+
+    while stack:
+        node, level, parent = stack.popleft()
+
+        left_leaf: int = left_function(node)
+        right_leaf: int = right_function(node)
+
+        parent[str(node)] = [{} if level > 1 else None, {} if level > 1 else None]
+
+        if level > 1:
+            stack.append((left_leaf, level - 1, parent[str(node)][0]))
+            stack.append((right_leaf, level - 1, parent[str(node)][1]))
+
+    return tree
+
+
+# if __name__ == "__main__":  # for debugging
+# height: int = 6
+
+# print(gen_bin_tree_recursive(height))
+# print(gen_bin_tree_iterative(height))
