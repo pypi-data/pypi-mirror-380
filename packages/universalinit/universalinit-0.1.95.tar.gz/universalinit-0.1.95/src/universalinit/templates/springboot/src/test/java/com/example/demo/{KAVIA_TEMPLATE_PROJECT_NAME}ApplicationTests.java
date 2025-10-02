1333933750af5af944c73e97d2ad64926a65442f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:06:36 +0000

    Add package universalinit/0.1.95/universalinit-0.1.95.tar.gz

---
+++ packages/universalinit/universalinit-0.1.95.tar.gz/universalinit-0.1.95/src/universalinit/templates/springboot/src/test/java/com/example/demo/{KAVIA_TEMPLATE_PROJECT_NAME}ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.{KAVIA_TEMPLATE_PROJECT_NAME};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class {KAVIA_TEMPLATE_PROJECT_NAME}ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
