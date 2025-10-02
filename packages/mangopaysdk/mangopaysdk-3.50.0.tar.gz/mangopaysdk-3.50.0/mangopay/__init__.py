326524d3b98ad683ee53061626b2d8d17f2708b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:06:23 +0000

    Add package mangopaysdk/3.50.0/mangopaysdk-3.50.0.tar.gz

---
+++ packages/mangopaysdk/mangopaysdk-3.50.0.tar.gz/mangopaysdk-3.50.0/mangopay/__init__.py
@@ -0,0 +1,27 @@
+client_id = None
+apikey = None
+api_url = 'https://api.mangopay.com/v2.01/'
+api_sandbox_url = 'https://api.sandbox.mangopay.com/v2.01/'
+temp_dir = None
+api_version = 2.01
+sandbox = True
+uk_header_flag = False
+
+package_version = None
+try:
+    with open('./setup.py', 'r') as f:
+        for line in f:
+            if line.startswith('    version'):
+                package_version = line.split('=')[1].replace("'", "").replace(",", "").replace("\n", "")
+except:
+    None
+
+
+from .api import APIRequest  # noqa
+from .utils import memoize
+
+
+def _get_default_handler():
+    return APIRequest()
+
+get_default_handler = memoize(_get_default_handler, {}, 0)
