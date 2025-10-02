5ceb19e8290f86cd43f83b5b2284bca5125a3bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:06:53 +0000

    Add package tech4354353/1.3/tech4354353-1.3-py3-none-any.whl

---
+++ packages/tech4354353/tech4354353-1.3-py3-none-any.whl/tech4354353/tech4354353.py
@@ -0,0 +1,13 @@
+class Stu:
+    def __init__(self):   #method
+        self.name = input("enter your name:")   #attribute
+        self.grade = input("enter your grade")
+        self.percent = input("enter your percent")
+
+    def stu_details(self):
+        print(f"{self.name} is in class {self.grade} & percent {self.percent}")
+
+
+
+
+        
