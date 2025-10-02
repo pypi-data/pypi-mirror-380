8d720e6593210d1dfc412ac830b8e204f500d4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:06:28 +0000

    Add package bedrock-agentcore/0.1.7/bedrock_agentcore-0.1.7.tar.gz

---
+++ packages/bedrock-agentcore/bedrock_agentcore-0.1.7.tar.gz/bedrock_agentcore-0.1.7/tests_integ/tools/test_browser.py
@@ -0,0 +1,12 @@
+from bedrock_agentcore.tools.browser_client import browser_session
+
+with browser_session("us-west-2") as client:
+    assert client.session_id is not None
+    url, headers = client.generate_ws_headers()
+    assert url.startswith("wss")
+
+    url = client.generate_live_view_url()
+    assert url.startswith("https")
+
+    client.take_control()
+    client.release_control()
