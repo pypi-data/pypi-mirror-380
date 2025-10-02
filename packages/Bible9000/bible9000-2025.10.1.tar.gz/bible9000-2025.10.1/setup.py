6cff027222d3a46768439a24fc9d40c3c43f44c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:07:03 +0000

    Add package Bible9000/2025.10.1/bible9000-2025.10.1.tar.gz

---
+++ packages/Bible9000/bible9000-2025.10.1.tar.gz/bible9000-2025.10.1/setup.py
@@ -0,0 +1,27 @@
+# Works, yet beta.toml over-installs the .gsdict files?
+#
+from __future__ import unicode_literals
+from distutils.core import setup
+from setuptools import find_packages, find_namespace_packages
+
+# ---from glob import glob as glob
+# ---aFiles = glob("Hershey01//GsDict//*.gsdict")
+# defer to README.md:
+aFiles = [
+    "./bible9000/biblia.sqlt3",
+    ]
+
+zFiles = [
+    ('.',aFiles)
+    ]
+
+setup(name='.',
+      author="Randall Nagy",
+      version="2.0.0",
+      description="Stick of Joseph: Ancient Wisdom for Our Times.",
+      author_email="r.a.nagy@gmail.com",
+      url="http://MightyMaxims.com",
+      download_url="https://github.com/DoctorQuote/The-Stick-of-Joseph",
+      platforms="Cross-Platform CLI / TUI",
+      packages=find_namespace_packages(),
+      data_files=zFiles)
